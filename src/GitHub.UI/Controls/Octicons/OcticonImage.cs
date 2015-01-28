680a549452a630dba87c24fbb41d740c7df857b1
Andreia Gaita <shana@github.com>
2015-01-28 15:05:23 +0100

    Update github styles, add image icon

---
+++ src/GitHub.UI/Controls/Octicons/OcticonImage.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GitHub.UI
+{
+    public class OcticonImage : Control
+    {
+        public Octicon Icon
+        {
+            get { return (Octicon)GetValue(OcticonPath.IconProperty); }
+            set { SetValue(OcticonPath.IconProperty, value); }
+        }
+
+        public static DependencyProperty IconProperty =
+            OcticonPath.IconProperty.AddOwner(typeof(OcticonImage));
+    }
+}
