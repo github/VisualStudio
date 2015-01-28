d866f5d756bef8e7bc57e0d1ec3ba420fc7b102f
Andreia Gaita <shana@github.com>
2015-01-28 17:30:55 +0100

    Fleshing out Home and Connect pages
    Set up base classes for Team Explorer navigation pages, sections, items
    and links.
    Home and Connect sections underway.

---
+++ src/GitHub.VisualStudio/Base/TeamExplorerItemBase.cs
@@ -0,0 +1,42 @@
+﻿using NullGuard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub.VisualStudio.Base
+{
+    abstract class TeamExplorerItemBase : TeamExplorerBase, INotifyPropertySource
+    {
+        bool isEnabled;
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set { isEnabled = value; this.RaisePropertyChange(); }
+        }
+
+        bool isVisible;
+        public bool IsVisible
+        {
+            get { return IsVisible; }
+            set { IsVisible = value; this.RaisePropertyChange(); }
+        }
+
+        string text;
+        [AllowNull]
+        public string Text
+        {
+            get { return text; }
+            set { text = value; this.RaisePropertyChange(); }
+        }
+
+        public virtual void Execute()
+        {
+        }
+
+        public virtual void Invalidate()
+        {
+        }
+    }
+}
---
+++ src/GitHub.VisualStudio/TeamExplorerConnect/GitHubConnectSection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.TeamFoundation.Controls;
+
+namespace GitHub.VisualStudio
+{
+    [TeamExplorerSection(GitHubConnectSectionId, TeamExplorerPageIds.Connect, 10)]
+    class GitHubConnectSection : TeamExplorerSectionBase
+    {
+        public const string GitHubConnectSectionId = "45081988-634A-4B8C-A4EA-9187169E567A";
+        
+        public GitHubConnectSection()
+            : base()
+        {
+            Title = "GitHub";
+            IsVisible = true;
+            IsExpanded = true;
+        }
+    }
+}
