08d7d17b002769ebe5b95faa215025d512ad40b6
Andreia Gaita <shana@github.com>
2015-01-28 17:30:55 +0100

    Fleshing out Home and Connect pages
    Set up base classes for Team Explorer navigation pages, sections, items
    and links.
    Home and Connect sections underway.

---
+++ src/GitHub.VisualStudio/Base/TeamExplorerNavigationItemBase.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.TeamFoundation.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using NullGuard;
+using GitHub.VisualStudio.Base;
+
+namespace GitHub.VisualStudio
+{
+    class TeamExplorerNavigationItemBase : TeamExplorerItemBase, ITeamExplorerNavigationItem2, INotifyPropertySource
+    {
+        public TeamExplorerNavigationItemBase(IServiceProvider serviceProvider)
+            : base()
+        {
+            this.ServiceProvider = serviceProvider;
+        }
+
+        int argbColor;
+        public int ArgbColor
+        {
+            get { return argbColor; }
+            set { argbColor = value; this.RaisePropertyChange(); }
+        }
+
+        object icon;
+        [AllowNull]
+        public object Icon
+        {
+            get { return icon; }
+            set { icon = value; this.RaisePropertyChange(); }
+        }
+
+        Image image;
+        [AllowNull]
+        public Image Image
+        {
+            get { return image; }
+            set { image = value; this.RaisePropertyChange(); }
+        }
+
+    }
+}
