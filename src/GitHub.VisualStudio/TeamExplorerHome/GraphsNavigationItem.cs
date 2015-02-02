ddc293ba97ab10b60a1b33e9438e2386c0e9283d
Andreia Gaita <shana@github.com>
2015-02-02 18:38:50 +0100

    Add navigation items and icons

---
+++ src/GitHub.VisualStudio/TeamExplorerHome/GraphsNavigationItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Controls;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Shell;
+
+namespace GitHub.VisualStudio
+{
+    [TeamExplorerNavigationItem(GraphsNavigationItemId, 10, TargetPageId = TeamExplorerPageIds.Home)]
+    class GraphsNavigationItem : TeamExplorerNavigationItemBase
+    {
+        public const string GraphsNavigationItemId = "5245767A-B657-4F8E-BFEE-F04159F1DDA5";
+
+        [ImportingConstructor]
+        public GraphsNavigationItem([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+            Text = "Graphs";
+            IsVisible = true;
+            IsEnabled = true;
+
+            Image = UI.Controls.NavigationIcon.GetImage(ServiceProvider, GitHub.UI.Octicon.graph);
+        }
+    }
+}
---
+++ src/GitHub.VisualStudio/TeamExplorerHome/IssuesNavigationItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Controls;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Shell;
+
+namespace GitHub.VisualStudio
+{
+    [TeamExplorerNavigationItem(IssuesNavigationItemId, 30, TargetPageId = TeamExplorerPageIds.Home)]
+    class IssuesNavigationItem : TeamExplorerNavigationItemBase
+    {
+        public const string IssuesNavigationItemId = "5245767A-B657-4F8E-BFEE-F04159F1DDA4";
+
+        [ImportingConstructor]
+        public IssuesNavigationItem([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+            Text = "Issues";
+            IsVisible = true;
+            IsEnabled = true;
+
+            Image = UI.Controls.NavigationIcon.GetImage(ServiceProvider, GitHub.UI.Octicon.issue_closed);
+        }
+    }
+}
