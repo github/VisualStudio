49afbf88daa5e1987f8a335fd7f22bde9c2fc689
Andreia Gaita <shana@github.com>
2015-02-02 18:38:50 +0100

    Add navigation items and icons

---
+++ src/GitHub.VisualStudio/TeamExplorerHome/PullRequestsNavigationItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Controls;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Shell;
+
+namespace GitHub.VisualStudio
+{
+    [TeamExplorerNavigationItem(PullRequestsNavigationItemId, 10, TargetPageId = TeamExplorerPageIds.Home)]
+    class PullRequestsNavigationItem : TeamExplorerNavigationItemBase
+    {
+        public const string PullRequestsNavigationItemId = "5245767A-B657-4F8E-BFEE-F04159F1DDA3";
+
+        [ImportingConstructor]
+        public PullRequestsNavigationItem([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+            Text = "Pull Requests";
+            IsVisible = true;
+            IsEnabled = true;
+
+            Image = UI.Controls.NavigationIcon.GetImage(ServiceProvider, GitHub.UI.Octicon.git_pull_request);
+        }
+    }
+}
---
+++ src/GitHub.VisualStudio/TeamExplorerHome/WikiNavigationItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Controls;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Shell;
+
+namespace GitHub.VisualStudio
+{
+    [TeamExplorerNavigationItem(WikiNavigationItemId, 10, TargetPageId = TeamExplorerPageIds.Home)]
+    class WikiNavigationItem : TeamExplorerNavigationItemBase
+    {
+        public const string WikiNavigationItemId = "5245767A-B657-4F8E-BFEE-F04159F1DDA1";
+
+        [ImportingConstructor]
+        public WikiNavigationItem([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+            Text = "Wiki";
+            IsVisible = true;
+            IsEnabled = true;
+
+            Image = UI.Controls.NavigationIcon.GetImage(ServiceProvider, GitHub.UI.Octicon.book);
+
+        }
+    }
+}
