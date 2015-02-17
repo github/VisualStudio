28a98145c97f7d931dbb0b56ade4b8277d76b401
Andreia Gaita <shana@spoiledcat.net>
2015-02-17 19:31:33 +0000

    Nitpick Nancy hunting spaces

--- src/GitHub.VisualStudio/Settings.cs
+++ src/GitHub.VisualStudio/Settings.cs
@@ -18,7 +18,7 @@ namespace GitHub.VisualStudio
         public const int PullRequests = TeamExplorerNavigationItemPriority.GitCommits - 1;
         public const int Wiki = TeamExplorerNavigationItemPriority.Settings - 1;
         public const int Pulse = Wiki - 3;
-        public const int Graphs = Wiki- 2;
+        public const int Graphs = Wiki - 2;
         public const int Issues = Wiki - 1;
     }
 }
