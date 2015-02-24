84f309306063da5ee7797855ffc5e15e4d7ca592
Andreia Gaita <shana@github.com>
2015-02-24 21:52:25 +0100

    Add separate lib to hold mef interfaces with reactive dependencies

---
+++ src/GitHub.Exports.Reactive/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GitHub.Exports.Reactive")]
+[assembly: AssemblyDescription("GitHub interfaces for mef exports with reactive dependencies")]
+[assembly: Guid("e4ed0537-d1d9-44b6-9212-3096d7c3f7a1")]
