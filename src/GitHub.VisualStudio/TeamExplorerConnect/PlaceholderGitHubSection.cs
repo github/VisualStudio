0b342ef028ec9fbeba5ad2e790bcad9fcaaf3049
Andreia Gaita <shana@github.com>
2015-02-24 21:56:23 +0100

    Temporary TE section for triggering clone/create
    While we don't have the definitive classes/interfaces
    for the GitHub Connect section in Team Explorer, use
    a mock one that triggers our clone/create dialogs
    (with optional auth)

---
+++ src/GitHub.VisualStudio/TeamExplorerConnect/PlaceholderGitHubSection.cs
@@ -0,0 +1,70 @@
+﻿using GitHub.Exports;
+using GitHub.Extensions;
+using GitHub.Models;
+using GitHub.Services;
+using GitHub.VisualStudio.UI.Views;
+using Microsoft.TeamFoundation.Controls;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub.VisualStudio.TeamExplorerConnect
+{
+    [TeamExplorerSection(PlaceholderGitHubSectionId, TeamExplorerPageIds.Connect, 10)]
+    public class PlaceholderGitHubSection : TeamExplorerSectionBase
+    {
+        public const string PlaceholderGitHubSectionId = "519B47D3-F2A9-4E19-8491-8C9FA25ABE97";
+
+        protected GitHubConnectContent View
+        {
+            get { return this.SectionContent as GitHubConnectContent; }
+            set { this.SectionContent = value; }
+        }
+
+        [ImportingConstructor]
+        public PlaceholderGitHubSection([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
+        {
+
+            this.Title = "GitHub";
+            this.IsVisible = true;
+            this.IsEnabled = true;
+            this.IsExpanded = true;
+            View = new GitHubConnectContent();
+            View.ViewModel = this;
+        }
+
+        public void DoCreate()
+        {
+            // this is done here and not via the constructor so nothing gets loaded
+            // until we get here
+            var ui = ServiceProvider.GetExportedValue<IUIProvider>();
+
+            var factory = ui.GetService<ExportFactoryProvider>();
+            var d = factory.UIControllerFactory.CreateExport();
+            var creation = d.Value.SelectFlow(GitHub.UI.UIControllerFlow.Create);
+            var x = new WindowController(creation);
+            creation.Subscribe(_ => { }, _ => x.Close());
+            x.Show();
+        }
+
+        public void DoClone()
+        {
+            // this is done here and not via the constructor so nothing gets loaded
+            // until we get here
+            var ui = ServiceProvider.GetExportedValue<IUIProvider>();
+
+            var factory = ui.GetService<ExportFactoryProvider>();
+            var d = factory.UIControllerFactory.CreateExport();
+            var creation = d.Value.SelectFlow(GitHub.UI.UIControllerFlow.Clone);
+            creation.Subscribe(_ => { }, _ => d.Dispose());
+            var x = new WindowController(creation);
+            x.Show();
+
+            d.Value.Start();
+        }
+    }
+}
---
+++ src/GitHub.VisualStudio/UI/Views/GitHubConnectContent.xaml.cs
@@ -0,0 +1,57 @@
+﻿using GitHub.VisualStudio.TeamExplorerConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GitHub.VisualStudio.UI.Views
+{
+    /// <summary>
+    /// This is a temporary placeholder class until the VS Connect section
+    /// is done
+    /// </summary>
+    public partial class GitHubConnectContent : UserControl
+    {
+        public GitHubConnectContent()
+        {
+            InitializeComponent();
+        }
+
+        private void cloneLink_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.DoClone();
+        }
+
+        private void createLink_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.DoCreate();
+        }
+
+        private void m_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public PlaceholderGitHubSection ViewModel
+        {
+            get { return (PlaceholderGitHubSection)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register(
+                "ViewModel",
+                typeof(PlaceholderGitHubSection),
+                typeof(GitHubConnectContent));
+    }
+}
