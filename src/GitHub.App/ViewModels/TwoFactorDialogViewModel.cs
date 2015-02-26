433131e2ca10346ef3ee69f9a8f13dacf082f301
Andreia Gaita <shana@github.com>
2015-02-26 16:53:51 +0100

    Export 2fa viewmodel for the 2fa challenge handler

--- src/GitHub.App/ViewModels/TwoFactorDialogViewModel.cs
+++ src/GitHub.App/ViewModels/TwoFactorDialogViewModel.cs
@@ -14,6 +14,7 @@ using ReactiveUI;
 namespace GitHub.ViewModels
 {
     [ExportViewModel(ViewType=UIViewType.TwoFactor)]
+    [Export(typeof(ITwoFactorViewModel))]
     [PartCreationPolicy(CreationPolicy.Shared)]
     public class TwoFactorDialogViewModel : ReactiveValidatableObject, ITwoFactorViewModel
     {
