04456a47f4528261380adcee9c5766830bdb8f7d
Andreia Gaita <shana@github.com>
2015-02-26 21:58:18 +0100

    Disable nullguard on GittHub.UI
    While doing UI work, the designer triggers nullguard everywhere and
    makes it extremely hard to prototype. Also might not be that useful on a
    control library, need to evaluate it here.

---
+++ src/GitHub.UI/Fakes/NullGuard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullGuard
+{
+    [AttributeUsage(AttributeTargets.All)]
+    internal sealed class AllowNullAttribute : Attribute
+    {
+        
+    }
+}
