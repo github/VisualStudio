bf4dd5090e907ab6314a61b7b1ecc439a17c259c
Haacked <haacked@gmail.com>
2015-02-26 14:08:26 -0800

    Add unit test of LoginControlViewModel
    Setting up some test infrastructure for the view models before
    I start to implement stuff.

---
+++ src/UnitTests/GitHub.App/ViewModels/LoginControlViewModelTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GitHub.Info;
+using GitHub.Models;
+using GitHub.Services;
+using GitHub.ViewModels;
+using NSubstitute;
+using Xunit;
+
+public class LoginControlViewModelTests
+{
+    public class TheSignupCommand
+    {
+        [Fact]
+        public void LaunchesBrowserToSignUpPage()
+        {
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            var repositoryHosts = Substitute.For<IRepositoryHosts>();
+            var browser = Substitute.For<IBrowser>();
+            var enterpriseProbe = LazySubstitute.For<IEnterpriseProbe>();
+            var loginViewModel = new LoginControlViewModel(serviceProvider, repositoryHosts, browser, enterpriseProbe);
+
+            loginViewModel.SignupCommand.Execute(null);
+
+            browser.Received().OpenUrl(GitHubUrls.Plans);
+        }
+    }
+}
