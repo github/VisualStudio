b94d91e3611c17478041d58ef3b81e3b5663fc3d
Andreia Gaita <shana@github.com>
2015-02-26 16:55:06 +0100

    Fix ExportFactoryProvider build and code issues

--- src/GitHub.Exports/Services/ExportFactoryProvider.cs
+++ src/GitHub.Exports/Services/ExportFactoryProvider.cs
@@ -17,24 +17,27 @@ namespace GitHub.Services
         {
             cc.SatisfyImportsOnce(this);
         }
-        
+
+        [Import(AllowRecomposition = true)]
+        public ExportFactory<IUIController> UIControllerFactory { get; set; }
+
         [ImportMany(AllowRecomposition = true)]
         public IEnumerable<ExportFactory<IViewModel, IViewModelMetadata>> ViewModelFactory { get; set; }
 
         [ImportMany(AllowRecomposition = true)]
-        public IEnumerable<ExportFactory<IViewFor, IViewModelMetadata>> ViewFactory { get; set; }
+        public IEnumerable<ExportFactory<IView, IViewModelMetadata>> ViewFactory { get; set; }
 
         public ExportLifetimeContext<IViewModel> GetViewModel(UIViewType viewType)
         {
             var f = ViewModelFactory.FirstOrDefault(x => x.Metadata.ViewType == viewType);
-            Debug.Assert(f != null, string.Format("Could not locate view model for {0}.", viewtype));
+            Debug.Assert(f != null, string.Format(CultureInfo.InvariantCulture, "Could not locate view model for {0}.", viewType));
             return f.CreateExport();
         }
 
-        public ExportLifetimeContext<IViewFor> GetView(UIViewType viewType)
+        public ExportLifetimeContext<IView> GetView(UIViewType viewType)
         {
             var f = ViewFactory.FirstOrDefault(x => x.Metadata.ViewType == viewType);
-            Debug.Assert(f != null, string.Format("Could not locate view for {0}.", viewtype));
+            Debug.Assert(f != null, string.Format(CultureInfo.InvariantCulture, "Could not locate view for {0}.", viewType));
             return f.CreateExport();
         }
     }
