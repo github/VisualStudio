a745983012a82829c1fa5b9901e8ad9b6f7eec15
Andreia Gaita <shana@github.com>
2015-02-26 16:55:31 +0100

    Fix code analysis

--- src/GitHub.Exports/ViewModels/IViewModel.cs
+++ src/GitHub.Exports/ViewModels/IViewModel.cs
@@ -1,8 +1,9 @@
 using System;
 
-namespace GitHub.ViewModels {
-	
-	public interface IViewModel {
+namespace GitHub.ViewModels
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces")]
+    public interface IViewModel {
 	
 	}
 }
