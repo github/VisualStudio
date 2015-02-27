a9ecb808f47f68a6df6bfb3fcf8bab75afb3f184
Haacked <haacked@gmail.com>
2015-02-26 21:58:06 -0800

    Hook up the Forgot Password button

--- src/UnitTests/GitHub.App/ViewModels/LoginControlViewModelTests.cs
+++ src/UnitTests/GitHub.App/ViewModels/LoginControlViewModelTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using GitHub.Info;
 using GitHub.Models;
+using GitHub.Primitives;
 using GitHub.Services;
 using GitHub.ViewModels;
 using NSubstitute;
@@ -22,6 +23,23 @@ public class LoginControlViewModelTests
             loginViewModel.SignUpCommand.Execute(null);
 
             browser.Received().OpenUrl(GitHubUrls.Plans);
+        }
+    }
+
+    public class TheForgotPasswordCommand
+    {
+        [Fact]
+        public void LaunchesBrowserToForgotPasswordPage()
+        {
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            var repositoryHosts = Substitute.For<IRepositoryHosts>();
+            var browser = Substitute.For<IBrowser>();
+            var enterpriseProbe = LazySubstitute.For<IEnterpriseProbe>();
+            var loginViewModel = new LoginControlViewModel(serviceProvider, repositoryHosts, browser, enterpriseProbe);
+
+            loginViewModel.ForgotPasswordCommand.Execute(null);
+
+            browser.Received().OpenUrl(new Uri(HostAddress.GitHubDotComHostAddress.WebUri, GitHubUrls.ForgotPasswordPath));
         }
     }
 }
