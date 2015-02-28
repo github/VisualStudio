d7a054bea5bc189d08788cbd508f82f047dae161
Haacked <haacked@gmail.com>
2015-02-27 23:43:51 -0800

    Made some tweaks of the home section
    I tweaked the home section a bit. I added it to the
    DesignTimeStyleHelper app so I could easily bring it up. I also made it
    show content at design time.

---
+++ src/DesignTimeStyleHelper/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GitHub.VisualStudio.TeamExplorerHome;
+
+namespace DesignTimeStyleHelper
+{
+    public class HomeContentDesignContext
+    {
+        public DesignTimeGitHubHomeSection ViewModel { get { return new DesignTimeGitHubHomeSection(); } }
+    }
+
+    public class DesignTimeGitHubHomeSection : IGitHubHomeSection
+    {
+        public string RepoName { get { return "Repository: octokit/octokit.net"; }  set { } }
+        public string RepoUrl { get { return "Remote: https://github.com/octokit/octokit.net"; } set { } }
+    }
+}
