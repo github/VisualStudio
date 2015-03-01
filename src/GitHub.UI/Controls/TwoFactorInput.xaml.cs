fdeecab79913023a6aeb102f0c386763093b3037
Haacked <haacked@gmail.com>
2015-03-01 00:05:42 -0800

    TwoFactorInput now handles paste event

--- src/GitHub.UI/Controls/TwoFactorInput.xaml.cs
+++ src/GitHub.UI/Controls/TwoFactorInput.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using NullGuard;
 
 namespace GitHub.UI
 {
@@ -13,26 +15,68 @@ namespace GitHub.UI
         public static readonly DependencyProperty TextProperty =
             DependencyProperty.Register("Text", typeof(string), typeof(TwoFactorInput), new PropertyMetadata(""));
 
+        TextBox[] TextBoxes;
+
         public TwoFactorInput()
         {
             InitializeComponent();
 
-            SetupTextBox(one);
-            SetupTextBox(two);
-            SetupTextBox(three);
-            SetupTextBox(four);
-            SetupTextBox(five);
-            SetupTextBox(six);
+            TextBoxes = new[]
+            {
+                one,
+                two,
+                three,
+                four,
+                five,
+                six
+            };
+
+            foreach(var textBox in TextBoxes)
+            {
+                SetupTextBox(textBox);
+            }
+        }
+
+        private void OnPaste(object sender, DataObjectPastingEventArgs e)
+        {
+            var isText = e.SourceDataObject.GetDataPresent(DataFormats.Text, true);
+            if (!isText) return;
+
+            var text = e.SourceDataObject.GetData(DataFormats.Text) as string;
+            if (text == null) return;
+            e.CancelCommand();
+            SetText(text);
+        }
+
+        void SetText(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                foreach (var textBox in TextBoxes)
+                {
+                    textBox.Text = "";
+                }
+                SetValue(TextProperty, text);
+                return;
+            }
+            var digits = text.Where(Char.IsDigit).ToList();
+            for (int i = 0; i < Math.Min(6, digits.Count); i++)
+            {
+                TextBoxes[i].Text = digits[i].ToString();
+            }
+            SetValue(TextProperty, String.Join("", digits));
         }
 
         public string Text
         {
             get { return (string)GetValue(TextProperty); }
-            private set { SetValue(TextProperty, value); }
+            set { SetText(value); }
         }
 
         private void SetupTextBox(TextBox textBox)
         {
+            DataObject.AddPastingHandler(textBox, new DataObjectPastingEventHandler(OnPaste));
+
             textBox.GotFocus += (sender, args) => textBox.SelectAll();
 
             textBox.PreviewKeyDown += (sender, args) =>
@@ -48,9 +92,21 @@ namespace GitHub.UI
                     && args.Key != Key.D8
                     && args.Key != Key.D9
                     && args.Key != Key.Tab
-                    && args.Key != Key.Escape)
+                    && args.Key != Key.Escape
+                    && (!(args.Key == Key.V && args.KeyboardDevice.Modifiers == ModifierKeys.Control))
+                    && (!(args.Key == Key.Insert && args.KeyboardDevice.Modifiers == ModifierKeys.Shift)))
                 {
                     args.Handled = true;
+                }
+            };
+
+            textBox.SelectionChanged += (sender, args) =>
+            {
+                // Make sure we can't insert additional text into a textbox.
+                // Each textbox should only allow one character.
+                if (textBox.SelectionLength == 0 && textBox.Text.Any())
+                {
+                    textBox.SelectAll();
                 }
             };
 
@@ -59,7 +115,7 @@ namespace GitHub.UI
                 var tRequest = new TraversalRequest(FocusNavigationDirection.Next);
                 var keyboardFocus = Keyboard.FocusedElement as UIElement;
 
-                Text = GetTwoFactorCode();
+                SetValue(TextProperty, String.Join("", GetTwoFactorCode()));
 
                 if (keyboardFocus != null)
                 {
@@ -68,19 +124,14 @@ namespace GitHub.UI
             };
         }
 
-        private string GetTextBoxValue(TextBox textBox)
+        private static string GetTextBoxValue(TextBox textBox)
         {
             return String.IsNullOrEmpty(textBox.Text) ? " " : textBox.Text;
         }
 
         private string GetTwoFactorCode()
         {
-            return GetTextBoxValue(one)
-                   + GetTextBoxValue(two)
-                   + GetTextBoxValue(three)
-                   + GetTextBoxValue(four)
-                   + GetTextBoxValue(five)
-                   + GetTextBoxValue(six);
+            return String.Join("", TextBoxes.Select(textBox => textBox.Text));
         }
     }
 }
