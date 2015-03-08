015de6699fef80eb9859f8d47f02e2299a1d5e4c
Haacked <haacked@gmail.com>
2015-03-08 15:27:04 -0700

    Add the LoginToGitHubViewModel tests

---
+++ src/UnitTests/GitHub.App/ViewModels/LoginToGitHubViewModelTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using GitHub.Info;
+using GitHub.Models;
+using GitHub.Primitives;
+using GitHub.Services;
+using GitHub.ViewModels;
+using NSubstitute;
+using Xunit;
+
+public class LoginToGitHubViewModelTests
+{
+    public class TheSignupCommand
+    {
+        [Fact]
+        public void LaunchesBrowserToSignUpPage()
+        {
+            var repositoryHosts = Substitute.For<IRepositoryHosts>();
+            var gitHubHost = Substitute.For<IRepositoryHost>();
+            gitHubHost.Address.Returns(HostAddress.GitHubDotComHostAddress);
+            repositoryHosts.GitHubHost.Returns(gitHubHost);
+            var browser = Substitute.For<IBrowser>();
+            var loginViewModel = new LoginToGitHubViewModel(repositoryHosts, browser);
+
+            loginViewModel.SignUp.Execute(null);
+
+            browser.Received().OpenUrl(GitHubUrls.Plans);
+        }
+    }
+
+    public class TheForgotPasswordCommand
+    {
+        [Fact]
+        public void LaunchesBrowserToForgotPasswordPage()
+        {
+            var repositoryHosts = Substitute.For<IRepositoryHosts>();
+            var gitHubHost = Substitute.For<IRepositoryHost>();
+            gitHubHost.Address.Returns(HostAddress.GitHubDotComHostAddress);
+            repositoryHosts.GitHubHost.Returns(gitHubHost);
+            var browser = Substitute.For<IBrowser>();
+            var loginViewModel = new LoginToGitHubViewModel(repositoryHosts, browser);
+
+            loginViewModel.ForgotPassword.Execute(null);
+
+            browser.Received().OpenUrl(new Uri(HostAddress.GitHubDotComHostAddress.WebUri, GitHubUrls.ForgotPasswordPath));
+        }
+    }
+}
+  
