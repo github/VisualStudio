ad5a2e4ef01e34312cca6fee6e16040a364150bb
Haacked <haacked@gmail.com>
2015-03-08 14:32:51 -0700

    Fix bug where Dispose throws a NullRef

---
+++ src/UnitTests/GitHub.App/Controllers/UIControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using GitHub.Controllers;
+using GitHub.Models;
+using GitHub.Services;
+using NSubstitute;
+using Xunit;
+
+public class UIControllerTests
+{
+    public class TheDisposeMethod
+    {
+        [Fact]
+        public void WithMultipleCallsDoesNotThrowException()
+        {
+            var uiProvider = Substitute.For<IUIProvider>();
+            var hosts = Substitute.For<IRepositoryHosts>();
+            var factory = new ExportFactoryProvider(Substitute.For<ICompositionService>());
+            var uiController = new UIController(uiProvider, hosts, factory);
+
+            uiController.Dispose();
+            uiController.Dispose();
+        }
+    }
+}
