055e0777a84c3d4d34ef6194925f805b694a71b3
Haacked <haacked@gmail.com>
2015-03-16 13:20:57 -0700

    Implement SafeRepositoryName with unit tests

---
+++ src/UnitTests/GitHub.App/ViewModels/RepositoryCreationViewModelTests.cs
@@ -0,0 +1,40 @@
+﻿using GitHub.ViewModels;
+using Xunit;
+
+public class RepositoryCreationViewModelTests
+{
+    public class TheSafeRepositoryNameProperty
+    {
+        [Fact]
+        public void IsTheSameAsTheRepositoryNameWhenTheInputIsSafe()
+        {
+            var vm = new RepositoryCreationViewModel();
+
+            vm.BaseRepositoryPath = @"c:\fake\";
+            vm.RepositoryName = "this-is-bad";
+
+            Assert.Equal(vm.RepositoryName, vm.SafeRepositoryName);
+        }
+
+        [Fact]
+        public void IsConvertedWhenTheRepositoryNameIsNotSafe()
+        {
+            var vm = new RepositoryCreationViewModel();
+
+            vm.RepositoryName = "this is bad";
+
+            Assert.Equal("this-is-bad", vm.SafeRepositoryName);
+        }
+
+        [Fact]
+        public void IsNullWhenRepositoryNameIsNull()
+        {
+            var vm = new RepositoryCreationViewModel();
+            Assert.Null(vm.SafeRepositoryName);
+            vm.RepositoryName = "not-null";
+            vm.RepositoryName = null;
+
+            Assert.Null(vm.SafeRepositoryName);
+        }
+    }
+}
