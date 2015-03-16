12b2c3de2ded49bacd702ab4e79956d4898df61c
Haacked <haacked@gmail.com>
2015-03-16 13:08:07 -0700

    Fix up IRepositoryCreationViewModel with what's needed

--- src/GitHub.App/ViewModels/RepositoryCreationViewModel.cs
+++ src/GitHub.App/ViewModels/RepositoryCreationViewModel.cs
@@ -1,41 +1,136 @@
-﻿using System;
-using System.Reactive;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using GitHub.Models;
 using GitHub.Validation;
 using ReactiveUI;
 using GitHub.Exports;
+using System.ComponentModel.Composition;
 
 namespace GitHub.ViewModels
 {
     [ExportViewModel(ViewType=UIViewType.Create)]
-    public class RepositoryCreationViewModel : IRepositoryCreationViewModel
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class RepositoryCreationViewModel : ReactiveObject, IRepositoryCreationViewModel
     {
         public string Title { get { return "Create a GitHub Repository"; } } // TODO: this needs to be contextual
 
-        public ReactiveCommand<object> CancelCommand { get; private set; }
-        public ICommand CancelCmd { get { return CancelCommand; } }
+        public ReactiveList<IAccount> Accounts
+        {
+            get;
+            private set;
+        }
 
-        public ReactiveCommand<object> OkCommand { get; private set; }
-        public ICommand OkCmd { get { return OkCommand; } }
+        public string BaseRepositoryPath
+        {
+            get;
+            set;
+        }
 
-        public string RepositoryName { get; private set; }
-        public string SafeRepositoryName { get; private set; }
-        public bool ShowRepositoryNameWarning { get; private set; }
-        public string RepositoryNameWarningText { get; private set; }
-        public ReactivePropertyValidator<string> RepositoryNameValidator { get; private set; }
-        public string Description { get; set; }
-        public ReactivePropertyValidator<IAccount> SelectedAccountValidator { get; private set; }
-        public bool KeepPrivate { get; set; }
-        public bool CanKeepPrivate { get; private set; }
-        public bool ShowUpgradeToMicroPlanWarning { get; private set; }
-        public bool ShowUpgradePlanWarning { get; private set; }
-        public ReactiveCommand<Unit> CreateRepository { get; private set; }
-        public bool IsPublishing { get; private set; }
-        public ReactiveCommand<Object> UpgradeAccountPlan { get; private set; }
-        public ReactiveCommand<Object> Reset { get; private set; }
-        public ReactiveList<IAccount> Accounts { get; private set; }
-        public IAccount SelectedAccount { get; private set; }
+        public ReactivePropertyValidator<string> BaseRepositoryPathValidator
+        {
+            get;
+            private set;
+        }
 
+        public ICommand BrowseForDirectory
+        {
+            get;
+            private set;
+        }
+
+        public bool CanKeepPrivate
+        {
+            get;
+            private set;
+        }
+
+        public ICommand CreateRepository
+        {
+            get;
+            private set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public bool IsPublishing
+        {
+            get;
+            private set;
+        }
+
+        public bool KeepPrivate
+        {
+            get;
+            set;
+        }
+
+        public string RepositoryName
+        {
+            get;
+            set;
+        }
+
+        public ReactivePropertyValidator<string> RepositoryNameValidator
+        {
+            get;
+            private set;
+        }
+
+        public string RepositoryNameWarningText
+        {
+            get;
+            private set;
+        }
+
+        public ICommand Reset
+        {
+            get;
+            private set;
+        }
+
+        public string SafeRepositoryName
+        {
+            get;
+            private set;
+        }
+
+        public ReactivePropertyValidator<string> SafeRepositoryNameWarningValidator
+        {
+            get;
+            private set;
+        }
+
+        public IAccount SelectedAccount
+        {
+            get;
+            private set;
+        }
+
+        public bool ShowRepositoryNameWarning
+        {
+            get;
+            private set;
+        }
+
+        public bool ShowUpgradePlanWarning
+        {
+            get;
+            private set;
+        }
+
+        public bool ShowUpgradeToMicroPlanWarning
+        {
+            get;
+            private set;
+        }
+
+        public ICommand UpgradeAccountPlan
+        {
+            get;
+            private set;
+        }
     }
 }
