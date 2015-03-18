e771eee5a6040c19def6d4c98c3e7f0546196d95
Haacked <haacked@gmail.com>
2015-03-18 14:07:49 -0700

    Add a guard against empty strings

---
+++ src/GitHub.Exports.Reactive/Helpers/Guard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using Splat;
+
+namespace GitHub
+{
+    public static class Guard
+    {
+        /// <summary>
+        /// Validates that the string is not empty.
+        /// </summary>
+        /// <param name="value"></param>
+        public static void ArgumentNotEmptyString(string value, string name)
+        {
+            // We already know the value is not null because of NullGuard.Fody.
+            if (!string.IsNullOrWhiteSpace(value)) return;
+
+            string message = string.Format(CultureInfo.InvariantCulture, "The value for '{0}' must not be empty", name);
+#if DEBUG
+            if (!ModeDetector.InUnitTestRunner())
+            {
+                Debug.Fail(message);
+            }
+#endif
+            throw new ArgumentException("String cannot be empty", name);
+        }
+
+        [AttributeUsage(AttributeTargets.Parameter)]
+        internal sealed class ValidatedNotNullAttribute : Attribute
+        {
+        }
+    }
+}
