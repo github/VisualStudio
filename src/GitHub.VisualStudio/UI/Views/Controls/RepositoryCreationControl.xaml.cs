8c2cd3d5e6d99b05dea74dec4d45f46d23168ecc
Haacked <haacked@gmail.com>
2015-03-20 15:41:38 -0700

    Bind the base repository path to the UI

--- src/GitHub.VisualStudio/UI/Views/Controls/RepositoryCreationControl.xaml.cs
+++ src/GitHub.VisualStudio/UI/Views/Controls/RepositoryCreationControl.xaml.cs
@@ -44,6 +44,9 @@ namespace GitHub.VisualStudio.UI.Views.Controls
                 d(this.OneWayBind(ViewModel, vm => vm.RepositoryNameValidator, v => v.nameValidationMessage.ReactiveValidator));
                 d(this.OneWayBind(ViewModel, vm => vm.SafeRepositoryNameWarningValidator, v => v.safeRepositoryNameWarning.ReactiveValidator));
 
+                d(this.Bind(ViewModel, vm => vm.BaseRepositoryPath, v => v.localPathText.Text));
+                d(this.Bind(ViewModel, vm => vm.BaseRepositoryPathValidator, v => v.pathValidationMessage.ReactiveValidator));
+
                 d(this.Bind(ViewModel, vm => vm.Description, v => v.description.Text));
                 d(this.Bind(ViewModel, vm => vm.KeepPrivate, v => v.makePrivate.IsChecked));
                 d(this.OneWayBind(ViewModel, vm => vm.CanKeepPrivate, v => v.makePrivate.IsEnabled));
