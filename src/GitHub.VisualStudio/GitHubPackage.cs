a7d27e0baad6b739c5db98bb9abe51609d9f7ffb
Andreia Gaita <shana@github.com>
2015-03-23 15:00:58 +0100

    Remove GitHub test menu

--- src/GitHub.VisualStudio/GitHubPackage.cs
+++ src/GitHub.VisualStudio/GitHubPackage.cs
@@ -25,8 +25,6 @@ namespace GitHub.VisualStudio
     // This attribute is used to register the information needed to show this package
     // in the Help/About dialog of Visual Studio.
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-    // This attribute is needed to let the shell know that this package exposes some menus.
-    [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(GuidList.guidGitHubPkgString)]
     [ProvideBindingPath]
     public class GitHubPackage : PackageBase
