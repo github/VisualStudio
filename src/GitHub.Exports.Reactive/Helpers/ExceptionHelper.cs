c7728a341c70ce0f5b858d247972c5dbe956d0e0
Andreia Gaita <shana@github.com>
2015-03-24 14:20:17 +0100

    Remove uneeded using statements 'cause

--- src/GitHub.Exports.Reactive/Helpers/ExceptionHelper.cs
+++ src/GitHub.Exports.Reactive/Helpers/ExceptionHelper.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GitHub.Helpers
 {
