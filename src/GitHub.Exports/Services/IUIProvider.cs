289ae183392330e40d11d2e97b50a17535deb475
Andreia Gaita <shana@spoiledcat.net>
2015-03-25 13:58:56 +0100

    Extract the default repo path from the bowels of VS

--- src/GitHub.Exports/Services/IUIProvider.cs
+++ src/GitHub.Exports/Services/IUIProvider.cs
@@ -11,5 +11,8 @@ namespace GitHub.Services
         T GetService<T>();
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
         Ret GetService<T, Ret>() where Ret : class;
+
+        object TryGetService(Type t);
+        T TryGetService<T>() where T : class;
     }
 }
