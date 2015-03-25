561538eb5e30160e71c91c6ec734c20f163a8950
Haacked <haacked@gmail.com>
2015-03-25 14:52:54 -0700

    :art: Refactor common code for creation and publish

--- src/GitHub.Exports.Reactive/ViewModels/IRepositoryCreationViewModel.cs
+++ src/GitHub.Exports.Reactive/ViewModels/IRepositoryCreationViewModel.cs
@@ -1,65 +1,21 @@
-﻿using System.Windows.Input;
-using GitHub.Models;
+﻿using System.Reactive;
+using System.Windows.Input;
 using GitHub.Validation;
 using ReactiveUI;
-using System;
-using System.Reactive;
 
 namespace GitHub.ViewModels
 {
-    public interface IRepositoryCreationViewModel : IViewModel
+    public interface IRepositoryCreationViewModel : IRepositoryForm
     {
-        /// <summary>
-        /// The name of the repository.
-        /// </summary>
-        string RepositoryName { get; set; }
-
         /// <summary>
         /// The path where the repository is created. A folder named after the repository is created in this directory.
         /// </summary>
         string BaseRepositoryPath { get; set; }
 
         /// <summary>
-        /// The name of the repository after characters not allowed in a git repository name are replaced with
-        /// dashes.
+        /// Validates the base repository path.
         /// </summary>
-        string SafeRepositoryName { get; }
-        ReactivePropertyValidator<string> RepositoryNameValidator { get; }
-        ReactivePropertyValidator<string> SafeRepositoryNameWarningValidator { get; }
         ReactivePropertyValidator<string> BaseRepositoryPathValidator { get; }
-        string Description { get; set; }
-
-        ReactiveList<IAccount> Accounts { get; }
-
-        /// <summary>
-        /// The account or organization that will be the owner of the created repository.
-        /// </summary>
-        IAccount SelectedAccount { get; set; }
-
-        GitIgnoreItem SelectedGitIgnoreTemplate { get; set; }
-
-        LicenseItem SelectedLicense { get; set; }
-
-        /// <summary>
-        /// The list of GitIgnore templates supported by repository creation
-        /// </summary>
-        ReactiveList<GitIgnoreItem> GitIgnoreTemplates { get; }
-        ReactiveList<LicenseItem> Licenses { get; }
-
-        /// <summary>
-        /// Indicates whether the created repository should be private or not.
-        /// </summary>
-        bool KeepPrivate { get; set; }
-
-        /// <summary>
-        /// Indicates whether the user can create a private repository. This is false if the user is not a paid
-        /// account or if the user has run out of repositories for their current plan.
-        /// </summary>
-        bool CanKeepPrivate { get; }
-        bool ShowUpgradeToMicroPlanWarning { get; }
-        bool ShowUpgradePlanWarning { get; }
-
-        bool IsPublishing { get; }
 
         /// <summary>
         /// Command that launches a dialog to browse for the directory in which to create the repository.
@@ -72,8 +28,8 @@ namespace GitHub.ViewModels
         IReactiveCommand<Unit> CreateRepository { get; }
 
         /// <summary>
-        /// Command that opens a browser to a page for upgrading the user's plan.
+        /// True when creation is in progress.
         /// </summary>
-        ICommand UpgradeAccountPlan { get; }
+        bool IsCreating { get; }
     }
 }
---
+++ src/GitHub.Exports.Reactive/ViewModels/IRepositoryPublishViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Reactive;
+using GitHub.Models;
+using ReactiveUI;
+
+namespace GitHub.ViewModels
+{
+    public interface IRepositoryPublishViewModel : IRepositoryForm
+    {
+        ReactiveList<IRepositoryHost> RepositoryHosts { get; }
+
+        /// <summary>
+        /// Command that creates the repository.
+        /// </summary>
+        IReactiveCommand<Unit> PublishRepository { get; }
+
+        /// <summary>
+        /// True when publishing is in progress.
+        /// </summary>
+        bool IsPublishing { get; }
+    }
+}
