d6088bbf8a7f9ce40f591bee5a83cc72b1e1174f
Andreia Gaita <shana@spoiledcat.net>
2015-03-25 16:40:57 +0100

    A connection manager for handling state between VS runs

---
+++ src/GitHub.VisualStudio/Services/ConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using GitHub.Models;
+using GitHub.Services;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+
+namespace GitHub.VisualStudio
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class ConnectionManager
+    {
+        public ObservableCollection<IConnection> Connections { get; private set; }
+
+        public ConnectionManager()
+        {
+            Connections = new ObservableCollection<IConnection>();
+
+            // TODO: Load list of known connections from cache
+            Connections.CollectionChanged += RefreshConnections;
+        }
+
+        private void RefreshConnections(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            // TODO: save list of known connections to cache
+        }
+    }
+}
