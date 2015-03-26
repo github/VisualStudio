1032212d0e8e0357302eadbce04a420087208acb
Andreia Gaita <shana@spoiledcat.net>
2015-03-26 15:40:21 +0100

    Move vs reflection stuff out to it's own class
    Also add some handy extension methods.
    I get the feeling this is going to come in handy
    in more places.

--- src/GitHub.App/Services/RepositoryCloneService.cs
+++ src/GitHub.App/Services/RepositoryCloneService.cs
@@ -46,53 +46,21 @@ namespace GitHub.Services
 
                 operatingSystem.Directory.CreateDirectory(path);
 
-                var gitExt = ServiceProvider.TryGetService<IGitRepositoriesExt>();
-                Debug.Assert(gitExt != null, "Could not get an instance of IGitRepositoriesExt");
+                // this will throw if it can't find it
+                var gitExt = ServiceProvider.GetService<IGitRepositoriesExt>();
 
                 gitExt.Clone(cloneUrl, path, CloneOptions.RecurseSubmodule);
                 return Unit.Default;
             });
         }
 
-        // The Default Repository Path that VS uses is hidden in an internal
-        // service 'ISccSettingsService' registered in an internal service
-        // 'ISccServiceHost' in an assembly with no public types that's
-        // always loaded with VS if the git service provider is loaded
         public string GetLocalClonePathFromGitProvider(string fallbackPath)
         {
-            var provider = (IUIProvider)ServiceProvider;
-            var ret = fallbackPath;
-            try
-            {
-                var ns = "Microsoft.TeamFoundation.Git.CoreServices.";
-                var scchost = "ISccServiceHost";
-                var sccservice = "ISccSettingsService";
-
-                var asm = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == "Microsoft.TeamFoundation.Git.CoreServices");
-                if (asm == null)
-                    return ret;
-
-                var type = asm.GetType(ns + scchost);
-                if (type == null)
-                    return ret;
-                var hostService = provider.TryGetService(type) as IServiceProvider;
-                if (hostService == null)
-                    return ret;
-                type = asm.GetType(ns + sccservice);
-                if (type == null)
-                    return ret;
-                var settings = hostService.GetService(type);
-                if (settings == null)
-                    return ret;
-                var prop = type.GetProperty("DefaultRepositoryPath");
-                if (prop == null)
-                    return ret;
-                var getm = prop.GetGetMethod();
-                ret = (string)getm.Invoke(settings, null);
-                ret = System.Environment.ExpandEnvironmentVariables(ret);
-            }
-            catch { }
-
+            var ret = VSServices.GetLocalClonePathFromGitProvider(ServiceProvider);
+            if (!string.IsNullOrEmpty(ret))
+                ret = operatingSystem.Environment.ExpandEnvironmentVariables(ret);
+            else
+                ret = fallbackPath;
             return ret;
         }
 
