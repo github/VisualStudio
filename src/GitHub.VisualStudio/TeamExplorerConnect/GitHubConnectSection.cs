46bcdab06c399c060d57d4eab16ed6bf5cebb24d
Andreia Gaita <shana@spoiledcat.net>
2015-03-28 13:26:57 +0100

    Create connect sections and track connections on them
    Support up to 5 different connections concurrently. When connections
    are added to the connection manager, connect sections are shown for
    them, and operations started from them (clone, create) get a connection
    object to contextualize what they're operating on (added to our service
    provider via a temporary service container)

---
+++ src/GitHub.VisualStudio/TeamExplorerConnect/GitHubConnectSection.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using GitHub.Services;
+using GitHub.UI;
+using GitHub.VisualStudio.Base;
+using GitHub.VisualStudio.UI;
+using GitHub.VisualStudio.UI.Views;
+using Microsoft.TeamFoundation.Controls;
+using Microsoft.VisualStudio.Shell;
+using GitHub.Models;
+using System.Collections.Specialized;
+using NullGuard;
+
+namespace GitHub.VisualStudio.TeamExplorerConnect
+{
+    public class GitHubConnectSection : TeamExplorerSectionBase
+    {
+        IServiceProvider gitServiceProvider;
+        readonly IConnectionManager connectionManager;
+        readonly int sectionIndex;
+
+        protected GitHubConnectContent View
+        {
+            get { return SectionContent as GitHubConnectContent; }
+            set { SectionContent = value; }
+        }
+
+        protected IConnection SectionConnection { get; set; }
+
+        public GitHubConnectSection(IConnectionManager manager, int index)
+        {
+            Title = "GitHub";
+            IsVisible = true;
+            IsEnabled = true;
+            IsExpanded = true;
+
+            connectionManager = manager;
+            sectionIndex = index;
+            connectionManager.Connections.CollectionChanged += RefreshConnections;
+
+            PropertyChanged += OnPropertyChange;
+        }
+
+        private void RefreshConnections(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    if (connectionManager.Connections.Count > sectionIndex)
+                        Refresh(connectionManager.Connections[sectionIndex]);
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    if (connectionManager.Connections.Count <= sectionIndex)
+                        Refresh(null);
+                    break;
+            }
+        }
+
+        protected void Refresh(IConnection connection)
+        {
+            if (connection == null)
+            {
+                IsVisible = false;
+            }
+            else if (SectionConnection != connection)
+            {
+                SectionConnection = connection;
+                // TODO: set title according to type of host
+                IsVisible = true;
+            }
+        }
+
+        public override void Initialize(object sender, SectionInitializeEventArgs e)
+        {
+            base.Initialize(sender, e);
+
+            gitServiceProvider = e.ServiceProvider;
+
+            // when we start up the connection list is loaded before we can get notifications so refresh manually
+            if (SectionConnection == null && connectionManager.Connections.Count > sectionIndex)
+                Refresh(connectionManager.Connections[sectionIndex]);
+        }
+
+        private void OnPropertyChange(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "IsVisible" && IsVisible && View == null)
+            {
+                View = new GitHubConnectContent();
+                View.ViewModel = this;
+            }
+        }
+
+        public void DoCreate()
+        {
+            // this is done here and not via the constructor so nothing gets loaded
+            // until we get here
+            StartFlow(UIControllerFlow.Create);
+        }
+
+        public void DoClone()
+        {
+            // this is done here and not via the constructor so nothing gets loaded
+            // until we get here
+            StartFlow(UIControllerFlow.Clone);
+        }
+
+        void StartFlow(UIControllerFlow controllerFlow)
+        {
+            var uiProvider = ServiceProvider.GetExportedValue<IUIProvider>();
+            uiProvider.AddService(typeof(IConnection), SectionConnection);
+
+            uiProvider.GitServiceProvider = gitServiceProvider;
+            var factory = uiProvider.GetService<IExportFactoryProvider>();
+            var uiControllerExport = factory.UIControllerFactory.CreateExport();
+            var uiController = uiControllerExport.Value;
+            var creation = uiController.SelectFlow(controllerFlow);
+            var windowController = new WindowController(creation);
+            creation.Subscribe(_ => { }, _ =>
+            {
+                windowController.Close();
+                uiControllerExport.Dispose();
+                uiProvider.RemoveService(typeof(IConnection));
+            });
+            windowController.Show();
+
+            uiController.Start();
+        }
+    }
+}
---
+++ src/GitHub.VisualStudio/TeamExplorerConnect/GitHubConnectSection4.cs
@@ -0,0 +1,17 @@
+﻿using GitHub.Models;
+using Microsoft.TeamFoundation.Controls;
+using System.ComponentModel.Composition;
+
+namespace GitHub.VisualStudio.TeamExplorerConnect
+{
+    [TeamExplorerSection(GitHubConnectSection4Id, TeamExplorerPageIds.Connect, 10)]
+    public class GitHubConnectSection4 : GitHubConnectSection
+    {
+        public const string GitHubConnectSection4Id = "519B47D3-F2A9-4E19-8491-8C9FA25ABE94";
+
+        [ImportingConstructor]
+        public GitHubConnectSection4(IConnectionManager manager) : base(manager, 4)
+        {
+        }
+    }
+}
