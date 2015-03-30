b4a2db5592066add0575a6eb9e74c1eb46e6c0f6
Haacked <haacked@gmail.com>
2015-03-29 20:34:14 -0700

    Add unit tests and fix ConnectionManager

--- src/GitHub.Exports/Services/Connection.cs
+++ src/GitHub.Exports/Services/Connection.cs
@@ -1,17 +1,17 @@
 ﻿using GitHub.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using GitHub.Primitives;
-using System.ComponentModel.Composition;
 
 namespace GitHub.Services
 {
     public class Connection : IConnection
     {
-        public HostAddress HostAddress { get; set; }
-        public string Username { get; set; }
+        public Connection(HostAddress hostAddress, string userName)
+        {
+            HostAddress = hostAddress;
+            Username = userName;
+        }
+
+        public HostAddress HostAddress { get; private set; }
+        public string Username { get; private set; }
     }
 }
---
+++ src/UnitTests/GitHub.VisualStudio/Services/ConnectionManagerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using GitHub.Models;
+using GitHub.VisualStudio;
+using NSubstitute;
+using Rothko;
+using Xunit;
+
+public class ConnectionManagerTests
+{
+    public class TheConnectionsProperty
+    {
+        [Fact]
+        public void IsLoadedFromCache()
+        {
+            const string cacheData = @"{""connections"":[{""HostUrl"":""https://github.com"", ""UserName"":""shana""},{""HostUrl"":""https://ghe.io"", ""UserName"":""haacked""}]}";
+            var program = Substitute.For<IProgram>();
+            program.ApplicationProvider.Returns("GHfVS");
+            var operatingSystem = Substitute.For<IOperatingSystem>();
+            operatingSystem.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData)
+                .Returns(@"c:\fake");
+            operatingSystem.File.Exists(@"c:\fake\GHfVS\ghfvs.connections").Returns(true);
+            operatingSystem.File.ReadAllText(@"c:\fake\GHfVS\ghfvs.connections", Encoding.UTF8).Returns(cacheData);
+            var manager = new ConnectionManager(program, operatingSystem);
+
+            var connections = manager.Connections;
+
+            Assert.Equal(2, connections.Count);
+            Assert.Equal(new Uri("https://api.github.com"), connections[0].HostAddress.ApiUri);
+            Assert.Equal(new Uri("https://ghe.io/api/v3/"), connections[1].HostAddress.ApiUri);
+        }
+
+        [Fact]
+        public void IsEmptyWhenCacheCorrupt()
+        {
+            const string cacheData = @"|!This ain't no JSON what even is this?";
+            var program = Substitute.For<IProgram>();
+            program.ApplicationProvider.Returns("GHfVS");
+            var operatingSystem = Substitute.For<IOperatingSystem>();
+            operatingSystem.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData)
+                .Returns(@"c:\fake");
+            operatingSystem.File.Exists(@"c:\fake\GHfVS\ghfvs.connections").Returns(true);
+            operatingSystem.File.ReadAllText(@"c:\fake\GHfVS\ghfvs.connections", Encoding.UTF8).Returns(cacheData);
+            var manager = new ConnectionManager(program, operatingSystem);
+
+            var connections = manager.Connections;
+
+            Assert.Equal(0, connections.Count);
+            operatingSystem.File.Received().Delete(@"c:\fake\GHfVS\ghfvs.connections");
+        }
+    }
+}
