0804b49af357cfa8d50717da510e514f3145fce7
Haacked <haacked@gmail.com>
2015-04-02 16:07:16 -0700

    :art: Remove the properties we don't use
    We can add them back later if we change our mind.

--- src/GitHub.Exports.Reactive/Models/IAccount.cs
+++ src/GitHub.Exports.Reactive/Models/IAccount.cs
@@ -1,18 +1,13 @@
-﻿using Octokit;
-
-namespace GitHub.Models
+﻿namespace GitHub.Models
 {
     public interface IAccount
     {
-        string Email { get; }
-        int Id { get; }
         bool IsEnterprise { get; }
         bool IsGitHub { get; }
         bool IsOnFreePlan { get; }
         bool HasMaximumPrivateRepositories { get; }
         bool IsUser { get; }
         string Login { get; }
-        string Name { get; }
         int OwnedPrivateRepos { get; }
         long PrivateReposInPlan { get; }
     }
