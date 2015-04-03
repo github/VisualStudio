ab77fab6afbb7f24367218442730213cfa0b4478
Haacked <haacked@gmail.com>
2015-04-02 22:36:15 -0700

    Add all the image caching code

---
+++ src/GitHub.Exports.Reactive/Models/IAvatarContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GitHub.Models
+{
+    /// <summary>
+    /// Interface for anything that can have an associated avatar such as a user or organization.
+    /// </summary>
+    public interface IAvatarContainer
+    {
+        /// <summary>
+        /// Used as the cache key for the avatar.
+        /// </summary>
+        string Login { get; }
+
+        /// <summary>
+        /// URL to the avatar.
+        /// </summary>
+        Uri AvatarUrl { get; }
+
+        /// <summary>
+        /// Helps determine the default image. Yeah, this interface isn't perfect.
+        /// </summary>
+        bool IsUser { get; }
+    }
+}
