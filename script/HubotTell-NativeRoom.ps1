6f1f4efe316ff78163253182a754973185098eb9
Keith Duncan <keith.duncan@github.com>
2015-04-02 17:22:56 -0700

    Replace campfire with chatterbox

--- script/HubotTell-NativeRoom.ps1
+++ script/HubotTell-NativeRoom.ps1
@@ -14,35 +14,10 @@ Param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
-function Create-MessageXml([string]$message) {
-    $xml = New-Object Xml
-    $bodyElement = $xml.CreateElement('body')
-    $bodyElement.InnerText = $message
-    $messageElement = $xml.CreateElement('message')
-    $messageElement.AppendChild($bodyElement) | Out-Null
-    $xml.AppendChild($messageElement) | Out-Null
-    $xml
-}
+Import-Module C:\psmodules\Chatterbox.psm1
 
-function Set-PostData([System.Net.WebRequest]$request, [string]$data) {
-    $request.Method = "POST"
-
-    $bytes = [System.Text.Encoding]::Default.GetBytes($data)
-    $request.ContentLength = $bytes.Length
-
-    $stream = $request.GetRequestStream()
-    $stream.Write($bytes, 0, $bytes.Length)
-    $stream.Close()
-}
-
-$request = New-CampfireRequest
-
-$xmlString = (Create-MessageXml $Message).OuterXml
-$request.ContentType = "application/xml"
-Set-PostData $request $xmlString
-
-$response = $request.GetResponse()
-if ($response.StatusCode -ne [System.Net.HttpStatusCode]::Created) {
-    Write-Error ("Expected status code 'Created' but got '{0}'" -f $response.StatusDescription)
+$Response = Send-TopicMessage "visualstudio-ci" $Message
+if ($Response.StatusCode -ne [System.Net.HttpStatusCode]::Created) {
+    Write-Error ("Expected status code 'Created' but got '{0}'" -f $Response.StatusDescription)
     exit 1
 }
