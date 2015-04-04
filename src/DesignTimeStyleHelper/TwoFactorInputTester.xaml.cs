3d401c6fea72cc7ea1abd5681ef3ee7019f7636d
Haacked <haacked@gmail.com>
2015-04-04 11:08:44 -0700

    Improve backspace and related key handling
    This adds support for the backspace key, left and right arrows, and the
    Delete key.
    
    Fixes #148

---
+++ src/DesignTimeStyleHelper/TwoFactorInputTester.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace DesignTimeStyleHelper
+{
+    /// <summary>
+    /// Interaction logic for TwoFactorInputTester.xaml
+    /// </summary>
+    public partial class TwoFactorInputTester : Window
+    {
+        public TwoFactorInputTester()
+        {
+            InitializeComponent();
+        }
+    }
+}
