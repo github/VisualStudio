4778e4ce45a7facd2bddb46f0b8efc3024003fe0
Andreia Gaita <shana@spoiledcat.net>
2015-04-07 13:18:53 +0200

    Add new publish section

---
+++ src/GitHub.VisualStudio/TeamExplorerSync/GitHubPublishSection.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using GitHub.UI;
+using GitHub.VisualStudio.Base;
+using GitHub.VisualStudio.Helpers;
+using GitHub.VisualStudio.UI.Views;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Controls;
+using Microsoft.VisualStudio.Shell;
+using System.Linq;
+using GitHub.Models;
+using GitHub.VisualStudio.UI.Views.Controls;
+
+namespace GitHub.VisualStudio.TeamExplorerHome
+{
+    [TeamExplorerSection(GitHubPublishSectionId, TeamExplorerPageIds.GitCommits, 10)]
+    public class GitHubPublishSection : TeamExplorerSectionBase
+    {
+        public const string GitHubPublishSectionId = "92655B52-360D-4BF5-95C5-D9E9E596AC76";
+
+        readonly IConnectionManager connectionManager;
+
+        public Octicon Icon
+        {
+            get;
+            private set;
+        }
+
+        [ImportingConstructor]
+        public GitHubPublishSection([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider, IConnectionManager cm)
+            :  base(serviceProvider)
+        {
+            this.connectionManager = cm;
+            Title = "GitHub";
+            IsVisible = false;
+            IsExpanded = true;
+        }
+
+        public override void Initialize(object sender, SectionInitializeEventArgs e)
+        {
+            base.Initialize(sender, e);
+        }
+
+        protected override void ContextChanged(object sender, ContextChangedEventArgs e)
+        {
+            base.ContextChanged(sender, e);
+            if (e.NewContext != null && e.NewContext.HasTeamProject)
+            {
+                Refresh();
+            }
+        }
+
+        public override void Refresh()
+        {
+            base.Refresh();
+
+            if (activeRepo != null)
+            {
+                var repo = Services.GetRepoFromIGit(activeRepo);
+                var remote = repo.Network.Remotes.FirstOrDefault(x => x.Name.Equals("origin", StringComparison.Ordinal));
+                if (remote == null)
+                {
+                    if (connectionManager.Connections.Count > 0)
+                    {
+                        ShowPublish();
+                    }
+                    else
+                    {
+                        ShowInvitation();
+                    }
+                }
+            }
+        }
+
+        void ShowInvitation()
+        {
+            var view = new GitHubInvitationContent();
+            view.DataContext = this;
+            SectionContent = view;
+        }
+
+        void ShowPublish()
+        {
+            var view = new RepositoryPublishControl();
+            view.DataContext = this;
+            SectionContent = view;
+        }
+    }
+}
---
+++ src/GitHub.VisualStudio/UI/Views/GitHubInvitationContent.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using GitHub.VisualStudio.TeamExplorerHome;
+using GitHub.UI.Helpers;
+
+namespace GitHub.VisualStudio.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for GitHubHomeSection.xaml
+    /// </summary>
+    public partial class GitHubInvitationContent : UserControl
+    {
+        public GitHubInvitationContent()
+        {
+            SharedDictionaryManager.Load("GitHub.UI");
+            Resources.MergedDictionaries.Add(SharedDictionaryManager.SharedDictionary);
+
+            InitializeComponent();
+
+            DataContextChanged += (s, e) => ViewModel = e.NewValue as IGitHubInvitationSection;
+        }
+
+        public IGitHubInvitationSection ViewModel
+        {
+            get { return (IGitHubInvitationSection)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register(
+                "ViewModel",
+                typeof(IGitHubInvitationSection),
+                typeof(GitHubInvitationContent));
+    }
+}
