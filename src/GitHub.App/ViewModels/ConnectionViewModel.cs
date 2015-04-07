92c2dd9c8219e8d0687b25923a00d75344f7bdb5
Andreia Gaita <shana@spoiledcat.net>
2015-04-07 13:26:44 +0200

    Make publish viewmodel not dependent on a specific connection/host

--- src/GitHub.App/ViewModels/ConnectionViewModel.cs
+++ src/GitHub.App/ViewModels/ConnectionViewModel.cs
@@ -1,22 +1,28 @@
 ﻿using GitHub.Models;
+using NullGuard;
 using ReactiveUI;
 
 namespace GitHub.ViewModels
 {
     public class ConnectionViewModel : BaseViewModel
     {
-        protected IConnection Connection { get; private set; }
+        protected IConnection Connection {[return: AllowNull] get; private set; }
 
         /// <summary>
         /// Host owning the repos
         /// </summary>
         public IRepositoryHost RepositoryHost { get; private set; }
 
-        public ConnectionViewModel(IConnection connection, IRepositoryHosts hosts)
+        public ConnectionViewModel([AllowNull]IConnection connection, IRepositoryHosts hosts)
             : base(null)
         {
-            Connection = connection;
-            RepositoryHost = hosts.LookupHost(Connection.HostAddress);
+            if (connection != null)
+            {
+                Connection = connection;
+                RepositoryHost = hosts.LookupHost(Connection.HostAddress);
+            }
+            else
+                RepositoryHost = hosts.LookupHost(null);
         }
     }
 }
