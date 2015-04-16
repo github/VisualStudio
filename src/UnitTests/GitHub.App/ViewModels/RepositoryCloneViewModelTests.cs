6a27baa79c91c3cecc9d52219474c23321ae8014
Haacked <haacked@gmail.com>
2015-04-16 14:25:30 -0700

    Add unit tests of error message display

--- src/UnitTests/GitHub.App/ViewModels/RepositoryCloneViewModelTests.cs
+++ src/UnitTests/GitHub.App/ViewModels/RepositoryCloneViewModelTests.cs
@@ -1,7 +1,16 @@
-﻿using GitHub.Models;
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading;
+using System.Threading.Tasks;
+using GitHub.Models;
 using GitHub.Services;
 using GitHub.ViewModels;
+using Microsoft.Reactive.Testing;
 using NSubstitute;
+using ReactiveUI.Testing;
+using Rothko;
 using Xunit;
 
 public class RepositoryCloneViewModelTests
@@ -13,14 +22,41 @@ public class RepositoryCloneViewModelTests
         {
             var repositoryHost = Substitute.For<IRepositoryHost>();
             var cloneService = Substitute.For<IRepositoryCloneService>();
-            var avatarProvider = Substitute.For<IAvatarProvider>();
-            var vm = new RepositoryCloneViewModel(repositoryHost, cloneService, avatarProvider);
+            var vm = new RepositoryCloneViewModel(
+                repositoryHost,
+                cloneService,
+                Substitute.For<IOperatingSystem>(),
+                Substitute.For<IVSServices>());
 
             Assert.False(vm.CloneCommand.CanExecute(null));
 
             vm.SelectedRepository = Substitute.For<IRepositoryModel>();
 
             Assert.True(vm.CloneCommand.CanExecute(null));
+        }
+
+        [Fact]
+        public async Task DisplaysErrorMessageWhenExceptionOccurs()
+        {
+            var repositoryHost = Substitute.For<IRepositoryHost>();
+            var cloneService = Substitute.For<IRepositoryCloneService>();
+            cloneService.CloneRepository(Args.String, Args.String, Args.String)
+                .Returns(Observable.Throw<Unit>(new InvalidOperationException("Oh my! That was bad.")));
+            var vsServices = Substitute.For<IVSServices>();
+            var vm = new RepositoryCloneViewModel(
+                repositoryHost,
+                cloneService,
+                Substitute.For<IOperatingSystem>(),
+                vsServices);
+            vm.BaseRepositoryPath = @"c:\fake";
+            var repository = Substitute.For<IRepositoryModel>();
+            repository.Name.Returns("octokit");
+            vm.SelectedRepository = repository;
+
+            await vm.CloneCommand.ExecuteAsync(null);
+
+            vsServices.Received().ShowError(@"Failed to clone the repository 'octokit'
+Email support@github.com if you continue to have problems.");
         }
     }
 }
