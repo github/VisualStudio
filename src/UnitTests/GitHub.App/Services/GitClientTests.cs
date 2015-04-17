5466b8e7429dbbe2eea401d34dd9d44542ec2a14
Haacked <haacked@gmail.com>
2015-04-16 19:08:20 -0700

    Do not push empty repo
    We should not push a repository branch with no commits. We just need to
    make sure to set the origin and it'll be tracking.
    
    Fixes #267

---
+++ src/UnitTests/GitHub.App/Services/GitClientTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Reactive.Linq;
+using System.Threading.Tasks;
+using GitHub.Services;
+using LibGit2Sharp;
+using NSubstitute;
+using Xunit;
+
+public class GitClientTests
+{
+    public class ThePushMethod
+    {
+        [Fact]
+        public async Task PushesToDefaultOrigin()
+        {
+            var origin = Substitute.For<Remote>();
+            var head = Substitute.For<Branch>();
+            head.Commits.Returns(new FakeCommitLog { Substitute.For<Commit>() });
+            var repository = Substitute.For<IRepository>();
+            repository.Head.Returns(head);
+            repository.Network.Remotes["origin"].Returns(origin);
+            var gitClient = new GitClient();
+
+            await gitClient.Push(repository, "master", "origin");
+
+            repository.Network.Received().Push(origin,"HEAD", @"refs/heads/master", null, null, null);
+        }
+
+        [Fact]
+        public async Task DoesNotPushEmptyRepository()
+        {
+            var repository = Substitute.For<IRepository>();
+
+            var gitClient = new GitClient();
+            await gitClient.Push(repository, "master", "origin");
+
+            repository.Network.DidNotReceive()
+                .Push(Args.Remote, Args.String, Args.String, Arg.Any<PushOptions>(), Arg.Any<Signature>(), Args.String);
+        }
+    }
+}
