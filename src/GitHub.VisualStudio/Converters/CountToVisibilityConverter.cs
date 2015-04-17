6185372e659b41f47355542d5851b11d962f78c6
praveen sethuraman <pra@dd.com>
2015-04-17 14:26:37 -0700

    Comments and formatting.

--- src/GitHub.VisualStudio/Converters/CountToVisibilityConverter.cs
+++ src/GitHub.VisualStudio/Converters/CountToVisibilityConverter.cs
@@ -1,20 +1,23 @@
-﻿using NullGuard;
-using System;
+﻿using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
+using NullGuard;
 
 namespace GitHub.VisualStudio.Converters
 {
+    /// <summary>
+    /// Convert a count to visibility based on the following rule:
+    /// * If count == 0, return Visibility.Visible
+    /// * If count > 0, return Visibility.Collapsed
+    /// </summary>
     public class CountToVisibilityConverter : IValueConverter
     {
-        [return: AllowNull]
         public object Convert([AllowNull] object value, [AllowNull] Type targetType, [AllowNull] object parameter, [AllowNull] CultureInfo culture)
         {
             return ((int)value == 0) ? Visibility.Visible : Visibility.Collapsed;
         }
 
-        [return: AllowNull]
         public object ConvertBack([AllowNull] object value, [AllowNull] Type targetType, [AllowNull] object parameter, [AllowNull] CultureInfo culture)
         {
             return null;
