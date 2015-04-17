9f03f34fda42d778739893b00cdc7c5ef8c18337
praveen sethuraman <pra@dd.com>
2015-04-17 14:21:53 -0700

    Remove dependencies on TeamFoundation controls - labeled textbox

---
+++ src/GitHub.VisualStudio/Converters/CountToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using NullGuard;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GitHub.VisualStudio.Converters
+{
+    public class CountToVisibilityConverter : IValueConverter
+    {
+        [return: AllowNull]
+        public object Convert([AllowNull] object value, [AllowNull] Type targetType, [AllowNull] object parameter, [AllowNull] CultureInfo culture)
+        {
+            return ((int)value == 0) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        [return: AllowNull]
+        public object ConvertBack([AllowNull] object value, [AllowNull] Type targetType, [AllowNull] object parameter, [AllowNull] CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
