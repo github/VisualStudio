068b0b2303a5704802f8c3b27aa660e79b3e675f
Haacked <haacked@gmail.com>
2015-04-17 17:21:20 -0700

    Add a Cancel signal to IView
    I needed to split ViewUserControl into 2, ViewUserControl and
    SimpleViewUserControl. The latter is used by the Publish dialog which
    should not import GitHub styles.

---
+++ src/GitHub.UI.Reactive/Controls/SimpleViewUserControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reactive.Subjects;
+using System.Windows.Controls;
+
+namespace GitHub.UI
+{
+    /// <summary>
+    /// Base class for all of our user controls. This one does not import GitHub resource/styles and is used by the 
+    /// publish control.
+    /// </summary>
+    public class SimpleViewUserControl : UserControl, IDisposable
+    {
+        readonly Subject<object> close = new Subject<object>();
+        readonly Subject<object> cancel = new Subject<object>();
+
+        public SimpleViewUserControl()
+        {
+        }
+
+        public IObservable<object> Done { get { return close; } }
+
+        public IObservable<object> Cancel { get { return cancel; } }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void NotifyDone()
+        {
+            close.OnNext(null);
+            close.OnCompleted();
+        }
+
+        protected void NotifyCancel()
+        {
+            cancel.OnNext(null);
+            cancel.OnCompleted();
+        }
+
+        bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    close.Dispose();
+                }
+                disposed = true;
+            }
+        }
+
+    }
+}
--- src/GitHub.UI.Reactive/Controls/ViewUserControl.cs
+++ src/GitHub.UI.Reactive/Controls/ViewUserControl.cs
@@ -1,45 +1,17 @@
-﻿using System;
-using System.Reactive.Subjects;
-using System.Windows.Controls;
-using GitHub.UI.Helpers;
+﻿using GitHub.UI.Helpers;
 
 namespace GitHub.UI
 {
-    public class ViewUserControl : UserControl, IDisposable
-    {
-        readonly Subject<object> close = new Subject<object>();
+    /// <summary>
+    /// Base class for all of our user controls This one imports GitHub resources/styles.
+    /// </summary>
 
+    public class ViewUserControl : SimpleViewUserControl
+    {
         public ViewUserControl()
         {
             SharedDictionaryManager.Load("GitHub.UI");
             SharedDictionaryManager.Load("GitHub.UI.Reactive");
-        }
-
-        protected void NotifyDone()
-        {
-            close.OnNext(null);
-            close.OnCompleted();
-        }
-
-        public IObservable<object> Done { get { return close; } }
-
-        bool disposed = false;
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposed)
-            {
-                if (disposing)
-                {
-                    close.Dispose();
-                }
-                disposed = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
         }
     }
 }
