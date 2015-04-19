ceaed1ba0770d521232085af02feaebaa05caca0
Haacked <haacked@gmail.com>
2015-04-19 00:24:14 -0700

    Set correct base uri for github login tab
    Fixes #207

--- src/GitHub.App/ViewModels/LoginToGitHubViewModel.cs
+++ src/GitHub.App/ViewModels/LoginToGitHubViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.Composition;
 using GitHub.Authentication;
+using GitHub.Info;
 using GitHub.Models;
 using GitHub.Primitives;
 using GitHub.Services;
@@ -11,21 +12,18 @@ namespace GitHub.ViewModels
     [PartCreationPolicy(CreationPolicy.NonShared)]
     public class LoginToGitHubViewModel : LoginTabViewModel, ILoginToGitHubViewModel
     {
-        readonly Lazy<Uri> baseUri;
+        Uri baseUri;
 
         [ImportingConstructor]
         public LoginToGitHubViewModel(IRepositoryHosts repositoryHosts, IVisualStudioBrowser browser)
             : base(repositoryHosts, browser)
         {
-            baseUri = new Lazy<Uri>(() => repositoryHosts.GitHubHost.Address.WebUri);
+            baseUri = HostAddress.GitHubDotComHostAddress.WebUri;
         }
 
         protected override Uri BaseUri
         {
-            get
-            {
-                return baseUri.Value;
-            }
+            get { return baseUri; }
         }
 
         protected override IObservable<AuthenticationResult> LogIn(object args)
