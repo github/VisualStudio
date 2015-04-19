1dbb819c337b65e34117c979f3144542563a9ab4
Andreia Gaita <shana@spoiledcat.net>
2015-04-19 21:50:20 +0200

    Set the local branch to track the remote.
    Fixes #213

--- src/GitHub.App/Services/GitClient.cs
+++ src/GitHub.App/Services/GitClient.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
 using LibGit2Sharp;
+using System.Reactive.Subjects;
 
 namespace GitHub.Services
 {
@@ -30,6 +31,18 @@ namespace GitHub.Services
             });
         }
 
+        public IObservable<Unit> Fetch(IRepository repository, string remoteName)
+        {
+            Guard.ArgumentNotEmptyString(remoteName, "remoteName");
+
+            return Observable.Defer(() =>
+            {
+                var remote = repository.Network.Remotes[remoteName];
+                repository.Network.Fetch(remote);
+                return Observable.Return(Unit.Default);
+            });
+        }
+
         public IObservable<Unit> SetRemote(IRepository repository, string remoteName, Uri url)
         {
             Guard.ArgumentNotEmptyString(remoteName, "remoteName");
@@ -39,6 +52,26 @@ namespace GitHub.Services
                 repository.Config.Set("remote." + remoteName + ".url", url.ToString());
                 repository.Config.Set("remote." + remoteName + ".fetch", "+refs/heads/*:refs/remotes/" + remoteName + "/*");
 
+                return Observable.Return(Unit.Default);
+            });
+        }
+
+        public IObservable<Unit> SetTrackingBranch(IRepository repository, string branchName, string remoteName)
+        {
+            Guard.ArgumentNotEmptyString(branchName, "branchName");
+            Guard.ArgumentNotEmptyString(remoteName, "remoteName");
+
+            return Observable.Defer(() =>
+            {
+                var remoteBranchName = "refs/remotes/" + remoteName + "/" + branchName;
+                var remoteBranch = repository.Branches[remoteBranchName];
+                // if it's null, it's because nothing was pushed
+                if (remoteBranch != null)
+                {
+                    var localBranchName = "refs/heads/" + branchName;
+                    var localBranch = repository.Branches[localBranchName];
+                    repository.Branches.Update(localBranch, b => b.TrackedBranch = remoteBranch.CanonicalName);
+                }
                 return Observable.Return(Unit.Default);
             });
         }
--- src/GitHub.Exports.Reactive/Services/IGitClient.cs
+++ src/GitHub.Exports.Reactive/Services/IGitClient.cs
@@ -16,6 +16,14 @@ namespace GitHub.Services
         IObservable<Unit> Push(IRepository repository, string branchName, string remoteName);
 
         /// <summary>
+        /// Fetches the remote.
+        /// </summary>
+        /// <param name="repository">The repository to pull</param>
+        /// <param name="remoteName">The name of the remote</param>
+        /// <returns></returns>
+        IObservable<Unit> Fetch(IRepository repository, string remoteName);
+
+        /// <summary>
         /// Sets the specified remote to the specified URL.
         /// </summary>
         /// <param name="repository">The repository to set</param>
@@ -23,5 +31,14 @@ namespace GitHub.Services
         /// <param name="url">The URL to set as the remote</param>
         /// <returns></returns>
         IObservable<Unit> SetRemote(IRepository repository, string remoteName, Uri url);
+
+        /// <summary>
+        /// Sets the remote branch that the local branch tracks
+        /// </summary>
+        /// <param name="repository">The repository to set</param>
+        /// <param name="branchName">The name of the remote</param>
+        /// <param name="remoteName">The name of the branch (local and remote)</param>
+        /// <returns></returns>
+        IObservable<Unit> SetTrackingBranch(IRepository repository, string branchName, string remoteName);
     }
 }
