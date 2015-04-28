21a5e73f598c2669bc5653333a70fdfe4879deae
Andreia Gaita <shana@spoiledcat.net>
2015-04-28 21:29:39 +0200

    Add another cache initialization fallback

--- src/GitHub.App/Factories/SqlitePersistentBlobCacheFactory.cs
+++ src/GitHub.App/Factories/SqlitePersistentBlobCacheFactory.cs
@@ -1,6 +1,8 @@
-﻿using System.ComponentModel.Composition;
-using Akavache;
+﻿using Akavache;
 using Akavache.Sqlite3;
+using NLog;
+using System;
+using System.ComponentModel.Composition;
 
 namespace GitHub.Factories
 {
@@ -8,12 +10,21 @@ namespace GitHub.Factories
     [PartCreationPolicy(CreationPolicy.Shared)]
     public class SqlitePersistentBlobCacheFactory : IBlobCacheFactory
     {
+        static readonly Logger log = LogManager.GetCurrentClassLogger();
+
         public IBlobCache CreateBlobCache(string path)
         {
             Guard.ArgumentNotEmptyString(path, "path");
 
-            //return new InMemoryBlobCache();
-            return new SQLitePersistentBlobCache(path);
+            try
+            {
+                return new SQLitePersistentBlobCache(path);
+            }
+            catch(Exception ex)
+            {
+                log.Error("Error while creating SQLitePersistentBlobCache for {0}. {1}", path, ex);
+                return new InMemoryBlobCache();
+            }
         }
     }
 }
