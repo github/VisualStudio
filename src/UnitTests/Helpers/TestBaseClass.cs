09c242dfb7826c8389205c75701e10a65708d253
Andreia Gaita <shana@spoiledcat.net>
2015-05-08 17:21:19 +0200

    Bump decorator version to catch namespace change

--- src/UnitTests/Helpers/TestBaseClass.cs
+++ src/UnitTests/Helpers/TestBaseClass.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntryExitDecoratorInterfaces;
 using GitHub.VisualStudio;
-using MethodDecoratorInterfaces;
 
 public class TestBaseClass : IEntryExitDecorator
 {
