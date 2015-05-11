fa87ca8c2262cb494e187ca678e9f0acc3765a3f
Andreia Gaita <shana@spoiledcat.net>
2015-05-11 13:00:04 +0200

    Add helpful comments to the test base class

--- src/UnitTests/Helpers/TestBaseClass.cs
+++ src/UnitTests/Helpers/TestBaseClass.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 using EntryExitDecoratorInterfaces;
 using GitHub.VisualStudio;
 
+/// <summary>
+/// This base class will get its methods called by the most-derived
+/// classes. The calls are injected by the EntryExitMethodDecorator Fody
+/// addin, so don't be surprised if you don't see any calls in the code.
+/// </summary>
 public class TestBaseClass : IEntryExitDecorator
 {
     public void OnEntry()
     {
+        // Ensure that every test has the InUnitTestRunner flag
+        // set, so threading doesn't go nuts.
         Splat.ModeDetector.Current.SetInUnitTestRunner(true);
     }
 
