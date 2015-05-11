af81bbfc48c362f498d27c5b6cfd448bf9485422
Andreia Gaita <shana@spoiledcat.net>
2015-05-11 18:23:02 +0200

    Fix loading resources assemblies
    There are some views that load very early, before VS has had a chance to
    set the proper load contexts. In some situations, it fails to load
    GitHub.UI when referenced from xaml because of this.
    SharedDictionaryManager has an assembly resolver, but it was in
    GitHub.UI so that wasn't going to help much.
    
    Move SharedDictionaryManager to the main assembly and merge it with the
    CachedResourceDictionary so that anything that tries to load resources
    from our assemblies is guaranteed to go through our assembly resolver,
    which knows where things are (in case VS is confused and doesn't know
    where things are).
    
    Fixes #382

--- src/GitHub.UI/Helpers/SharedDictionaryManager.cs
+++ src/GitHub.UI/Helpers/SharedDictionaryManager.cs
@@ -12,23 +12,6 @@ namespace GitHub.UI.Helpers
         static ResourceDictionary mergedDictionaries = new ResourceDictionary();
         static Dictionary<string, ResourceDictionary> dictionaries = new Dictionary<string, ResourceDictionary>();
 
-        static SharedDictionaryManager()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += LoadAssemblyFromRunDir;
-        }
-
-        static Assembly LoadAssemblyFromRunDir(object sender, ResolveEventArgs e)
-        {
-            var name = new AssemblyName(e.Name);
-            if (!dictionaries.ContainsKey(name.Name))
-                return null;
-            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            var filename = Path.Combine(path, name.Name + ".dll");
-            if (!File.Exists(filename))
-                return null;
-            return Assembly.LoadFrom(filename);
-        }
-
         public static ResourceDictionary Load(string assemblyname, ResourceDictionary resources)
         {
             ResourceDictionary dic;
@@ -49,29 +32,6 @@ namespace GitHub.UI.Helpers
             get
             {
                 return mergedDictionaries;
-            }
-        }
-    }
-
-    public class CachedResourceDictionary : ResourceDictionary
-    {
-        static Dictionary<Uri, ResourceDictionary> dictionaries = new Dictionary<Uri, ResourceDictionary>();
-
-        Uri sourceUri;
-        public new Uri Source
-        {
-            get { return sourceUri; }
-            set
-            {
-                sourceUri = value;
-                ResourceDictionary ret;
-                if (dictionaries.TryGetValue(value, out ret))
-                {
-                    MergedDictionaries.Add(ret);
-                    return;
-                }
-                base.Source = value;
-                dictionaries.Add(value, this);
             }
         }
     }
