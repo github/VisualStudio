320476f8da56704035b9da6f558960638ab4d1ef
Haacked <haacked@gmail.com>
2015-06-12 14:18:04 -0700

    Sort and remove unused usings

--- src/GitHub.VisualStudio/Services/SharedResources.cs
+++ src/GitHub.VisualStudio/Services/SharedResources.cs
@@ -1,11 +1,7 @@
-﻿using GitHub.UI;
-using GitHub.VisualStudio.UI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows.Media;
+using GitHub.UI;
+using GitHub.VisualStudio.UI;
 
 namespace GitHub.VisualStudio
 {
