fc55a67a2a59173a95d590b1e79ed546e77c0013
Haacked <haacked@gmail.com>
2015-08-05 16:33:15 -0700

    Add unit tests for SimpleApiClientFactory

---
+++ src/UnitTests/GitHub.App/Api/SimpleApiClientFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using GitHub.Api;
+using GitHub.Primitives;
+using GitHub.Services;
+using GitHub.VisualStudio;
+using NSubstitute;
+using Xunit;
+
+public class SimpleApiClientFactoryTests
+{
+    public class TheCreateMethod
+    {
+        [Fact]
+        public void CreatesNewInstanceOfSimpleApiClient()
+        {
+            var program = new Program();
+            var enterpriseProbe = Substitute.For<IEnterpriseProbeTask>();
+            var wikiProbe = Substitute.For<IWikiProbe>();
+            var factory = new SimpleApiClientFactory(
+                program,
+                new Lazy<IEnterpriseProbeTask>(() => enterpriseProbe),
+                new Lazy<IWikiProbe>(() => wikiProbe));
+
+            var client = factory.Create("https://github.com/github/visualstudio");
+
+            Assert.Equal("https://github.com/github/visualstudio", client.OriginalUrl);
+            Assert.Equal(HostAddress.GitHubDotComHostAddress, client.HostAddress);
+            Assert.Same(client, factory.Create("https://github.com/github/visualstudio")); // Tests caching.
+        }
+    }
+
+    public class TheClearFromCacheMethod
+    {
+        [Fact]
+        public void RemovesClientFromCache()
+        {
+            var program = new Program();
+            var enterpriseProbe = Substitute.For<IEnterpriseProbeTask>();
+            var wikiProbe = Substitute.For<IWikiProbe>();
+            var factory = new SimpleApiClientFactory(
+                program,
+                new Lazy<IEnterpriseProbeTask>(() => enterpriseProbe),
+                new Lazy<IWikiProbe>(() => wikiProbe));
+
+            var client = factory.Create("https://github.com/github/visualstudio");
+            factory.ClearFromCache(client);
+
+            Assert.NotSame(client, factory.Create("https://github.com/github/visualstudio"));
+        }
+    }
+}
