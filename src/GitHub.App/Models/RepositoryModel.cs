bf43378e276d882ec3f62c1efa44e12436f1abbf
Andreia Gaita <shana@spoiledcat.net>
2015-08-25 18:35:45 +0200

    (Simple)RepositoryModel: Fix  comparisons, add tests

--- src/GitHub.App/Models/RepositoryModel.cs
+++ src/GitHub.App/Models/RepositoryModel.cs
@@ -1,8 +1,11 @@
 ﻿using GitHub.Primitives;
+using NullGuard;
+using System;
+using System.Globalization;
 
 namespace GitHub.Models
 {
-    public class RepositoryModel : SimpleRepositoryModel, IRepositoryModel
+    public class RepositoryModel : SimpleRepositoryModel, IRepositoryModel, IEquatable<RepositoryModel>
     {
         public RepositoryModel(string name, UriString cloneUrl, bool isPrivate, bool isFork,  IAccount ownerAccount)
             : base(name, cloneUrl)
@@ -12,5 +15,54 @@ namespace GitHub.Models
         }
 
         public IAccount Owner { get; private set; }
+        public override int GetHashCode()
+        {
+            return (Owner != null ? 0 : Owner.GetHashCode()) ^ base.GetHashCode();
+        }
+
+        /// <summary>
+        /// This equality comparison will check if the references are the same,
+        /// and if they're not, it will explicitely check if the contents of this 
+        /// object are the same. If you only want reference checking, call
+        /// ReferenceEquals
+        /// </summary>
+        public static bool operator ==([AllowNull]RepositoryModel lhs, [AllowNull]RepositoryModel rhs)
+        {
+            if (ReferenceEquals(lhs, rhs))
+                return true;
+            if ((object)lhs == null || (object)rhs == null)
+                return false;
+            return lhs.Owner == rhs.Owner && (lhs as SimpleRepositoryModel) == (rhs as SimpleRepositoryModel);
+        }
+
+        /// <summary>
+        /// This equality comparison will check if the references are the same,
+        /// and if they're not, it will explicitely check if the contents of this 
+        /// object are the same. If you only want reference checking, call
+        /// ReferenceEquals
+        /// </summary>
+        public static bool operator !=([AllowNull]RepositoryModel lhs, [AllowNull]RepositoryModel rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public override bool Equals([AllowNull]object other)
+        {
+            return other != null && Equals(other as RepositoryModel);
+        }
+
+        bool IEquatable<RepositoryModel>.Equals([AllowNull]RepositoryModel other)
+        {
+            return other != null && this == other;
+        }
+
+        internal string DebuggerDisplay
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture,
+                    "{4}\tName: {0} CloneUrl: {1} LocalPath: {2} Account: {3}", Name, CloneUrl, LocalPath, Owner, GetHashCode());
+            }
+        }
     }
 }
--- src/GitHub.Exports/Models/SimpleRepositoryModel.cs
+++ src/GitHub.Exports/Models/SimpleRepositoryModel.cs
@@ -8,7 +8,7 @@ using System.Globalization;
 namespace GitHub.Models
 {
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
-    public class SimpleRepositoryModel : NotificationAwareObject, ISimpleRepositoryModel, INotifyPropertySource, IEquatable<ISimpleRepositoryModel>
+    public class SimpleRepositoryModel : NotificationAwareObject, ISimpleRepositoryModel, INotifyPropertySource, IEquatable<SimpleRepositoryModel>
     {
         public SimpleRepositoryModel(string name, UriString cloneUrl, string localPath = null)
         {
@@ -33,38 +33,45 @@ namespace GitHub.Models
         Octicon icon;
         public Octicon Icon { get { return icon; } set { icon = value; this.RaisePropertyChange(); } }
 
-        public bool Equals(ISimpleRepositoryModel other)
-        {
-            if (Object.ReferenceEquals(other, null)) return false;
-            if (Object.ReferenceEquals(this, other)) return true;
-            return Name.Equals(other.Name) && String.Equals(LocalPath, other.LocalPath, StringComparison.CurrentCultureIgnoreCase);
-        }
-
         public override int GetHashCode()
         {
-            return (Name == null ? 0 : Name.GetHashCode()) ^ (LocalPath == null ? 0 : LocalPath.TrimEnd('\\').ToUpperInvariant().GetHashCode());
+            return (Name == null ? 0 : Name.GetHashCode()) ^ (CloneUrl == null ? 0 : CloneUrl.GetHashCode()) ^ (LocalPath == null ? 0 : LocalPath.TrimEnd('\\').ToUpperInvariant().GetHashCode());
         }
 
+        /// <summary>
+        /// This equality comparison will check if the references are the same,
+        /// and if they're not, it will explicitely check if the contents of this 
+        /// object are the same. If you only want reference checking, call
+        /// ReferenceEquals
+        /// </summary>
         public static bool operator==(SimpleRepositoryModel lhs, SimpleRepositoryModel rhs)
         {
-            if ((object)lhs == null && (object)rhs == null)
+            if (ReferenceEquals(lhs, rhs))
                 return true;
-            if ((object)lhs != null && (object)rhs != null)
-                return lhs.Equals(rhs);
-            return false;
+            if ((object)lhs == null || (object)rhs == null)
+                return false;
+            return String.Equals(lhs.Name, rhs.Name) && String.Equals(lhs.CloneUrl, rhs.CloneUrl) && String.Equals(lhs.LocalPath?.TrimEnd('\\'), rhs.LocalPath?.TrimEnd('\\'), StringComparison.CurrentCultureIgnoreCase);
         }
 
+        /// <summary>
+        /// This equality comparison will check if the references are the same,
+        /// and if they're not, it will explicitely check if the contents of this 
+        /// object are the same. If you only want reference checking, call
+        /// ReferenceEquals
+        /// </summary>
         public static bool operator!=(SimpleRepositoryModel lhs, SimpleRepositoryModel rhs)
         {
             return !(lhs == rhs);
         }
 
-        public override bool Equals(object obj)
+        public override bool Equals(object other)
         {
-            var o = obj as ISimpleRepositoryModel;
-            if (o != null)
-                return Equals(o);
-            return false;
+            return other != null && Equals(other as SimpleRepositoryModel);
+        }
+
+        bool IEquatable<SimpleRepositoryModel>.Equals(SimpleRepositoryModel other)
+        {
+            return other != null && this == other;
         }
 
         internal string DebuggerDisplay
@@ -72,7 +79,7 @@ namespace GitHub.Models
             get
             {
                 return String.Format(CultureInfo.InvariantCulture,
-                    "{3}: Name: {0} CloneUrl: {1} LocalPath: {2}", Name, CloneUrl, LocalPath, GetHashCode());
+                    "{3}\tName: {0} CloneUrl: {1} LocalPath: {2}", Name, CloneUrl, LocalPath, GetHashCode());
             }
         }
     }
