3f9cc24df5416baad431486ab75de7bfa5a84ac3
Andreia Gaita <shana@spoiledcat.net>
2015-09-25 02:12:22 +0200

    Load all the pull requests and sort, for now

--- src/GitHub.App/Api/ApiClient.cs
+++ src/GitHub.App/Api/ApiClient.cs
@@ -201,7 +201,8 @@ namespace GitHub.Api
 
         public IObservable<PullRequest> GetPullRequestsForRepository(string owner, string name)
         {
-            return gitHubClient.PullRequest.GetAllForRepository(owner, name);
+            return gitHubClient.PullRequest.GetAllForRepository(owner, name, 
+                new PullRequestRequest() { State = ItemState.All, SortProperty = PullRequestSort.Updated, SortDirection = SortDirection.Descending });
         }
     }
 }
