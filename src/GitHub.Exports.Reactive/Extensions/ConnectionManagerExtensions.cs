cfb12fd1a4c723354ce840a06bcdf5eed4bd6f88
Andreia Gaita <shana@spoiledcat.net>
2015-10-26 15:48:35 +0100

    Login in the sync page
    Fixes #57
    
    If the current repository is a github/github enterprise repo but the
    user isn't logged in to its instance, then network git operations might
    fail. Show a warning on the Sync page if this is the case, and allow the
    user to login from there.

--- src/GitHub.Exports.Reactive/Extensions/ConnectionManagerExtensions.cs
+++ src/GitHub.Exports.Reactive/Extensions/ConnectionManagerExtensions.cs
@@ -2,6 +2,7 @@
 using GitHub.Models;
 using System;
 using ReactiveUI;
+using GitHub.Primitives;
 
 namespace GitHub.Extensions
 {
@@ -10,6 +11,14 @@ namespace GitHub.Extensions
         public static IObservable<bool> IsLoggedIn(this IConnectionManager cm, IRepositoryHosts hosts)
         {
             return cm.Connections.ToObservable()
+                    .SelectMany(c => c.Login())
+                    .Select(c => hosts.LookupHost(c.HostAddress)).Any(h => h.IsLoggedIn);
+        }
+
+        public static IObservable<bool> IsLoggedIn(this IConnectionManager cm, IRepositoryHosts hosts, HostAddress address)
+        {
+            return cm.Connections.ToObservable()
+                    .Where(c => c.HostAddress.Equals(address))
                     .SelectMany(c => c.Login())
                     .Select(c => hosts.LookupHost(c.HostAddress)).Any(h => h.IsLoggedIn);
         }
---
+++ src/GitHub.VisualStudio/Base/EnsureLoggedInSection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reactive.Linq;
+using GitHub.Api;
+using GitHub.Extensions;
+using GitHub.Models;
+using GitHub.Services;
+using GitHub.UI;
+using GitHub.VisualStudio.Base;
+using Microsoft.TeamFoundation.MVVM;
+using System.Globalization;
+using GitHub.Primitives;
+
+namespace GitHub.VisualStudio.TeamExplorer.Sync
+{
+    public class EnsureLoggedInSection : TeamExplorerSectionBase
+    {
+        readonly IRepositoryHosts hosts;
+        readonly IVSServices vsServices;
+
+        public EnsureLoggedInSection(ISimpleApiClientFactory apiFactory, ITeamExplorerServiceHolder holder,
+            IConnectionManager cm, IRepositoryHosts hosts, IVSServices vsServices)
+            : base(apiFactory, holder, cm)
+        {
+            IsVisible = false;
+            this.hosts = hosts;
+            this.vsServices = vsServices;
+        }
+
+        public override void Initialize(IServiceProvider serviceProvider)
+        {
+            base.Initialize(serviceProvider);
+            CheckLogin();
+        }
+
+        protected override void RepoChanged()
+        {
+            base.RepoChanged();
+            CheckLogin();
+        }
+
+        async void CheckLogin()
+        {
+            // this is not a github repo, or it hasn't been published yet
+            if (ActiveRepo == null || ActiveRepoUri == null)
+                return;
+
+            vsServices.ClearNotifications();
+            var add = HostAddress.Create(ActiveRepoUri);
+            bool loggedIn = await connectionManager.IsLoggedIn(hosts, add);
+            if (!loggedIn)
+            {
+                var msg = string.Format(CultureInfo.CurrentUICulture, Resources.NotLoggedInMessage, add.Title, add.ApiUri);
+                vsServices.ShowMessage(
+                    msg,
+                    new RelayCommand(() => StartFlow(UIControllerFlow.Authentication))
+                );
+            }
+        }
+
+        void StartFlow(UIControllerFlow controllerFlow)
+        {
+            var uiProvider = ServiceProvider.GetExportedValue<IUIProvider>();
+            var ret = uiProvider.SetupUI(controllerFlow, null);
+            ret.Subscribe(c => { }, CheckLogin);
+            uiProvider.RunUI();
+        }
+    }
+}
