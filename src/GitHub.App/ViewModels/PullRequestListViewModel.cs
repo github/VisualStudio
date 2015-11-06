Acidburn0zzz/Github-io-VisualStudio

PR list view and viewmodel

src/GitHub.App/ViewModels/PullRequestListViewModel.cs
@@ -1,0 +1,55 @@
+﻿using GitHub.Exports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitHub.Models;
+using System.Collections.ObjectModel;
+using ReactiveUI;
+using NullGuard;
+using System.ComponentModel.Composition;
+using GitHub.Services;
+using System.Reactive.Linq;
+using GitHub.Extensions.Reactive;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace GitHub.ViewModels
+{
+    [ExportViewModel(ViewType = UIViewType.PullRequestList)]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class PullRequestListViewModel : BaseViewModel, IPullRequestListViewModel
+    {
+        [ImportingConstructor]
+        PullRequestListViewModel(
+            IConnectionRepositoryHostMap connectionRepositoryHostMap, ITeamExplorerServiceHolder teservice)
+            : this(connectionRepositoryHostMap.CurrentRepositoryHost, teservice.ActiveRepo)
+        { }
+
+        public PullRequestListViewModel(IRepositoryHost repositoryHost, ISimpleRepositoryModel repository)
+        {
+            var list = repositoryHost.ModelService.GetPullRequests(repository);
+            list.SetComparer(OrderedComparer<IPullRequestModel>.OrderBy(x => x.UpdatedAt).Compare);
+            PullRequests = list;
+            list.Subscribe();
+        }
+
+        IReadOnlyCollection<IPullRequestModel> pullRequests;
+        public IReadOnlyCollection<IPullRequestModel> PullRequests
+        {
+            get { return pullRequests; }
+            private set { this.RaiseAndSetIfChanged(ref pullRequests, value); }
+        }
+
+        IPullRequestModel selectedPullRequest;
+        [AllowNull]
+        public IPullRequestModel SelectedPullRequest
+        {
+            [return: AllowNull]
+            get
+            { return selectedPullRequest; }
+            set { this.RaiseAndSetIfChanged(ref selectedPullRequest, value); }
+        }
+    }
+}

src/GitHub.Exports.Reactive/ViewModels/IPullRequestListViewModel.cs
@@ -1,0 +1,15 @@
+﻿using GitHub.Models;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace GitHub.ViewModels
+{
+    public interface IPullRequestListViewModel : IViewModel
+    {
+        IReadOnlyCollection<IPullRequestModel> PullRequests { get; }
+        IPullRequestModel SelectedPullRequest { get; }
+    }
+}

github/VisualStudio

src/GitHub.App/ViewModels/PullRequestListViewModel.cs
@@ -1,0 +1,56 @@
+﻿using GitHub.Exports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitHub.Models;
+using System.Collections.ObjectModel;
+using ReactiveUI;
+using NullGuard;
+using System.ComponentModel.Composition;
+using GitHub.Services;
+using System.Reactive.Linq;
+using GitHub.Extensions.Reactive;
+using System.Windows.Data;
+using System.ComponentModel;
+using GitHub.Collections;
+
+namespace GitHub.ViewModels
+{
+    [ExportViewModel(ViewType = UIViewType.PullRequestList)]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class PullRequestListViewModel : BaseViewModel, IPullRequestListViewModel
+    {
+        [ImportingConstructor]
+        PullRequestListViewModel(
+            IConnectionRepositoryHostMap connectionRepositoryHostMap, ITeamExplorerServiceHolder teservice)
+            : this(connectionRepositoryHostMap.CurrentRepositoryHost, teservice.ActiveRepo)
+        { }
+
+        public PullRequestListViewModel(IRepositoryHost repositoryHost, ISimpleRepositoryModel repository)
+        {
+            var list = repositoryHost.ModelService.GetPullRequests(repository);
+            list.SetComparer(OrderedComparer<IPullRequestModel>.OrderBy(x => x.UpdatedAt).Compare);
+            PullRequests = list;
+            list.Subscribe();
+        }
+
+        ITrackingCollection<IPullRequestModel> pullRequests;
+        public ITrackingCollection<IPullRequestModel> PullRequests
+        {
+            get { return pullRequests; }
+            private set { this.RaiseAndSetIfChanged(ref pullRequests, value); }
+        }
+
+        IPullRequestModel selectedPullRequest;
+        [AllowNull]
+        public IPullRequestModel SelectedPullRequest
+        {
+            [return: AllowNull]
+            get
+            { return selectedPullRequest; }
+            set { this.RaiseAndSetIfChanged(ref selectedPullRequest, value); }
+        }
+    }
+}

src/GitHub.Exports.Reactive/ViewModels/IPullRequestListViewModel.cs
@@ -1,0 +1,16 @@
+﻿using GitHub.Collections;
+using GitHub.Models;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace GitHub.ViewModels
+{
+    public interface IPullRequestListViewModel : IViewModel
+    {
+        ITrackingCollection<IPullRequestModel> PullRequests { get; }
+        IPullRequestModel SelectedPullRequest { get; }
+    }
+}