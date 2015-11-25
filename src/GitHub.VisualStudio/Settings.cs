04389f664921e5b098935b3e9463ef828c4025a1
George Bearden <george@goerden.com>
2015-11-24 22:58:10 -0500

    Add a 'Create Gist' context menu item.

--- src/GitHub.VisualStudio/Settings.cs
+++ src/GitHub.VisualStudio/Settings.cs
@@ -10,9 +10,11 @@ namespace GitHub.VisualStudio
         public const string guidGitHubPkgString = "c3d3dc68-c977-411f-b3e8-03b0dccf7dfc";
         public const string guidGitHubCmdSetString = "c4c91892-8881-4588-a5d9-b41e8f540f5a";
         public const string guidGitHubToolbarCmdSetString = "C5F1193E-F300-41B3-B4C4-5A703DD3C1C6";
+        public const string guidCreateGistCommandPackageCmdSetString = "D265E7EB-F3FC-411D-B4DC-1C6AD9264C4B";
 
         public static readonly Guid guidGitHubCmdSet = new Guid(guidGitHubCmdSetString);
         public static readonly Guid guidGitHubToolbarCmdSet = new Guid(guidGitHubToolbarCmdSetString);
+        public static readonly Guid guidCreateGistCommandPackageCmdSet = new Guid(guidCreateGistCommandPackageCmdSetString);
     }
 
     static class NavigationItemPriority
