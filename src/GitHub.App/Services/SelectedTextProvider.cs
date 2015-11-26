f0e373872e63ea39c2c5ce0d0292054b404a468d
George Bearden <george@goerden.com>
2015-11-25 22:47:03 -0500

    Create a service to get selected text in a text editor.

---
+++ src/GitHub.App/Services/SelectedTextProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace GitHub.Services
+{
+    [Export(typeof(ISelectedTextProvider))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class SelectedTextProvider : ISelectedTextProvider
+    {
+        readonly IServiceProvider serviceProvider;
+
+        [ImportingConstructor]
+        public SelectedTextProvider([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public IObservable<string> GetSelectedText()
+        {
+            return Observable.Create<string>(observer =>
+            {
+                var textManager = serviceProvider.GetService(typeof(SVsTextManager)) as IVsTextManager;
+                if (textManager == null)
+                {
+                    observer.OnError(new InvalidOperationException("cannot get SVsTextManager service"));
+                    return Disposable.Empty;
+                }
+
+                IVsTextView activeView;
+                if (textManager.GetActiveView(1, null, out activeView) != VSConstants.S_OK)
+                {
+                    observer.OnError(new InvalidOperationException("no active view in focus"));
+                    return Disposable.Empty;
+                }
+
+                // Maybe we should log here that no text was actually highlighted?
+                string highlightedText;
+                if (activeView.GetSelectedText(out highlightedText) != VSConstants.S_OK)
+                    highlightedText = string.Empty;
+
+                observer.OnNext(highlightedText);
+                observer.OnCompleted();
+                return Disposable.Empty;
+            });
+
+            
+        }
+    }
+}
+
---
+++ src/GitHub.Exports.Reactive/Services/ISelectedTextProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitHub.Services
+{
+    /// <summary>
+    /// Provides a way to get any currently selected text in the text editor area.
+    /// </summary>
+    public interface ISelectedTextProvider
+    {
+        /// <summary>
+        /// Gets the currently selected text.
+        /// </summary>
+        /// <returns></returns>
+        IObservable<string> GetSelectedText();
+    }
+}
