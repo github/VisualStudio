fd6e80a4e824e4f10fdaf1b824de7ad05997ad2b
George Bearden <george@goerden.com>
2015-11-25 22:58:32 -0500

    Create a service for creating Gists.

---
+++ src/GitHub.App/Services/GistCreator.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using NLog;
+using Octokit;
+
+namespace GitHub.Services
+{
+    [Export(typeof(IGistCreator))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class GistCreator : IGistCreator
+    {
+        static readonly Logger log = LogManager.GetCurrentClassLogger();
+        readonly IGitHubClient gitHubClient;
+
+        [ImportingConstructor]
+        public GistCreator(IGitHubClient gitHubClient)
+        {
+            this.gitHubClient = gitHubClient;
+        }
+
+        public async Task<Gist> CreateGist(string fileName, bool isPublic, string content = "", string description = "")
+        {
+            // No good reason to guard against optional content and description.
+            Guard.ArgumentNotEmptyString(fileName, nameof(fileName));
+
+            var newGist = new NewGist
+            {
+                Description = description,
+                Public = isPublic
+            };
+            newGist.Files.Add(fileName, content);
+
+            var createdGist = await gitHubClient.Gist.Create(newGist);
+            log.Debug("Created gist here: {0}", createdGist.HtmlUrl);
+            return createdGist;
+        }
+    }
+}
---
+++ src/GitHub.Exports/Services/IGistCreator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Octokit;
+
+namespace GitHub.Services
+{
+    public interface IGistCreator
+    {
+        Task<Gist> CreateGist(string fileName, bool isPublic, string content = "", string description = "");
+    }
+}
