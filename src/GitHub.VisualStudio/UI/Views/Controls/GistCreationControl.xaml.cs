2a1692273063dd2a0398829708a914ce4d207210
George Bearden <george@goerden.com>
2015-12-05 00:08:48 -0500

    Create the GistCreationView and setup the bindings to the view model.

--- src/GitHub.VisualStudio/UI/Views/Controls/GistCreationControl.xaml.cs
+++ src/GitHub.VisualStudio/UI/Views/Controls/GistCreationControl.xaml.cs
@@ -1,21 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.ComponentModel.Composition;
 using GitHub.Exports;
+using GitHub.Extensions;
 using GitHub.UI;
 using GitHub.ViewModels;
+using ReactiveUI;
+using System;
+using System.Reactive.Linq;
 
 namespace GitHub.VisualStudio.UI.Views.Controls
 {
@@ -32,6 +22,16 @@ namespace GitHub.VisualStudio.UI.Views.Controls
         public GistCreationControl()
         {
             InitializeComponent();
+
+            this.WhenActivated(d =>
+            {
+                d(this.Bind(ViewModel, vm => vm.Description, v => v.descriptionTextBox.Text));
+                d(this.Bind(ViewModel, vm => vm.FileName, v => v.fileNameTextBox.Text));
+                d(this.Bind(ViewModel, vm => vm.IsPrivate, v => v.makePrivate.IsChecked));
+                d(this.BindCommand(ViewModel, vm => vm.CreateGist, v => v.createGistButton));
+
+                ViewModel.CreateGist.Subscribe(_ => NotifyDone());
+            });
         }
     }
 }
