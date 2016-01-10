c1e059b94e26a4d5de13a7eac27e374a847a56d8
t_kirill <t_kirill@skbkontur.ru>
2016-01-10 18:23:12 +0500

    Add SuppressMessage attribute instead of removing the field.
    This field is already used in another branch (PR https://github.com/github/VisualStudio/pull/156).

--- src/GitHub.VisualStudio/Settings.cs
+++ src/GitHub.VisualStudio/Settings.cs
@@ -2,6 +2,7 @@
 // MUST match guids.h
 using Microsoft.TeamFoundation.Controls;
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GitHub.VisualStudio
 {
@@ -10,9 +11,12 @@ namespace GitHub.VisualStudio
         public const string guidGitHubPkgString = "c3d3dc68-c977-411f-b3e8-03b0dccf7dfc";
         public const string guidGitHubCmdSetString = "c4c91892-8881-4588-a5d9-b41e8f540f5a";
         public const string guidGitHubToolbarCmdSetString = "C5F1193E-F300-41B3-B4C4-5A703DD3C1C6";
+        public const string guidContextMenuSetString = "31057D08-8C3C-4C5B-9F91-8682EA08EC27";
 
         public static readonly Guid guidGitHubCmdSet = new Guid(guidGitHubCmdSetString);
         public static readonly Guid guidGitHubToolbarCmdSet = new Guid(guidGitHubToolbarCmdSetString);
+        [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields", Justification = "Already used in https://github.com/github/VisualStudio/pull/156")]
+        public static readonly Guid guidContextMenuSet = new Guid(guidContextMenuSetString);
     }
 
     static class NavigationItemPriority
