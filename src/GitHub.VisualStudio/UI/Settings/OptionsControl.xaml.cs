1aa3dcee0ae441318cf203013e16f22399134b49
Andreia Gaita <shana@spoiledcat.net>
2016-02-25 15:38:40 +0100

    Add link to sample usage data

--- src/GitHub.VisualStudio/UI/Settings/OptionsControl.xaml.cs
+++ src/GitHub.VisualStudio/UI/Settings/OptionsControl.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using GitHub.Services;
 
 namespace GitHub.VisualStudio.UI
 {
@@ -29,6 +17,12 @@ namespace GitHub.VisualStudio.UI
         {
             get { return chkMetrics.IsChecked ?? false; }
             set { chkMetrics.IsChecked = value; }
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            var browser = Services.DefaultExportProvider.GetExportedValue<IVisualStudioBrowser>();
+            browser?.OpenUrl(e.Uri);
         }
     }
 }
