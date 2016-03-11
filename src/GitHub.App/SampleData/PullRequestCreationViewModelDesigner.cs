46c5fd8c62f9822f7e608bc8b1e252363a9ef293
Andreia Gaita <shana@spoiledcat.net>
2016-03-11 02:24:08 +0100

    Set up example data for the creation view

--- src/GitHub.App/SampleData/PullRequestCreationViewModelDesigner.cs
+++ src/GitHub.App/SampleData/PullRequestCreationViewModelDesigner.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using GitHub.ViewModels;
+using System.Collections.Generic;
 
 namespace GitHub.SampleData
 {
@@ -8,7 +9,15 @@ namespace GitHub.SampleData
     {
         public PullRequestCreationViewModelDesigner()
         {
-
+            Name = "Bla bla bla";
+            Users = new List<string>()
+            {
+                "haacked",
+                "shana"
+            };
         }
+
+        public string Name { get; set; }
+        public List<string> Users { get; set; }
     }
 }
