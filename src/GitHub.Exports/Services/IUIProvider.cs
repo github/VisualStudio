f7acb70cfd05a177c3fbcad3245112ef83f1a3d1
Andreia Gaita <shana@spoiledcat.net>
2016-03-30 02:18:02 +0200

    Fix crasher

--- src/GitHub.Exports/Services/IUIProvider.cs
+++ src/GitHub.Exports/Services/IUIProvider.cs
@@ -18,9 +18,15 @@ namespace GitHub.Services
         object TryGetService(string typename);
         T TryGetService<T>() where T : class;
 
-        void AddService(Type t, object instance);
-        void AddService<T>(T instance);
-        void RemoveService(Type t);
+        void AddService(Type t, object owner, object instance);
+        void AddService<T>(object owner, T instance);
+        /// <summary>
+        /// Removes a service from the catalog
+        /// </summary>
+        /// <param name="t">The type we want to remove</param>
+        /// <param name="owner">The owner, which either has to match what was passed to AddService,
+        /// or if it's null, the service will be removed without checking for ownership</param>
+        void RemoveService(Type t, object owner);
 
         IObservable<UserControl> SetupUI(UIControllerFlow controllerFlow, IConnection connection);
         void RunUI();
