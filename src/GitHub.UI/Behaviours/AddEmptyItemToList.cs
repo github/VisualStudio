a75f2e2bd85fb59d864538aea4103b2cfe2d7a1b
Andreia Gaita <shana@spoiledcat.net>
2016-04-22 17:37:07 +0200

    Add a xaml behavior and converter to support [None] items on top of lists
    For lists where we want to support having no selection, it's handy to
    have an "empty" item on top of the list for the user to select. The
    behavior added here will add such an item to the top of a list if the
    selected property has a value set in it. If the selected property is
    null, the behavior removes the "empty" item.
    
    The behavior requires that the `ItemsSource` binding of the ItemsControl
    it's attached to be a MultiBinding, where the first entry is the bound
    property that contains the "empty" item to show.
    
    The `StickieListItemConverter` makes sure that the ItemsSource gets set
    with the second entry of the MultiBinding.

---
+++ src/GitHub.UI/Behaviours/AddEmptyItemToList.cs
@@ -0,0 +1,73 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+
+namespace GitHub.UI
+{
+    public class AddEmptyItemToList : Behavior<ListBox>
+    {
+        object previousSelection = null;
+        dynamic defaultValue;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.SelectionChanged += (s, e) =>
+            {
+                if (e.AddedItems.Count > 0)
+                {
+                    if (previousSelection == null)
+                    {
+                        previousSelection = AssociatedObject.SelectedItem;
+                        if (defaultValue == null)
+                        {
+                            var binding = AssociatedObject.GetBindingExpression(ListBox.ItemsSourceProperty);
+                            if (binding == null)
+                            {
+                                var m = BindingOperations.GetMultiBindingExpression(AssociatedObject, ListBox.ItemsSourceProperty);
+                                binding = (BindingExpression)m.BindingExpressions[0];
+                            }
+                            defaultValue = PropertyPathHelper.GetValue(binding.ResolvedSource, binding.ResolvedSourcePropertyName);
+                        }
+                        dynamic items = AssociatedObject.ItemsSource;
+                        items.Insert(0, defaultValue);
+                    }
+                    else if (AssociatedObject.SelectedIndex == 0)
+                    {
+                        dynamic items = AssociatedObject.ItemsSource;
+                        items.RemoveAt(0);
+                    }
+                }
+                if (e.RemovedItems.Count > 0)
+                {
+                    if (e.RemovedItems[0] == defaultValue)
+                    {
+                        previousSelection = null;
+                        AssociatedObject.SelectedItem = null;
+                    }
+                }
+            };
+        }
+
+        static class PropertyPathHelper
+        {
+            public static object GetValue(object obj, string propertyPath)
+            {
+                var binding = new Binding(propertyPath);
+                binding.Mode = BindingMode.OneTime;
+                binding.Source = obj;
+                BindingOperations.SetBinding(dummy, Dummy.ValueProperty, binding);
+                return dummy.GetValue(Dummy.ValueProperty);
+            }
+
+            static readonly Dummy dummy = new Dummy();
+
+            class Dummy : DependencyObject
+            {
+                public static readonly DependencyProperty ValueProperty =
+                    DependencyProperty.Register("Value", typeof(object), typeof(Dummy), new UIPropertyMetadata(null));
+            }
+        }
+    }
+}
