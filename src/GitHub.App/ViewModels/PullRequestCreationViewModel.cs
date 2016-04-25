2145fee4754e01df43bbdd490dba9386f9b2a06b
Jasmine <paladique@github.com>
2016-04-25 15:02:06 -0400

    resolving existing errors

--- src/GitHub.App/ViewModels/PullRequestCreationViewModel.cs
+++ src/GitHub.App/ViewModels/PullRequestCreationViewModel.cs
@@ -22,26 +22,44 @@ namespace GitHub.ViewModels
     class PullRequestCreationViewModel : BaseViewModel, IPullRequestCreationViewModel
     {
         static readonly Logger log = LogManager.GetCurrentClassLogger();
-        readonly IReactiveCommand<IReadOnlyList<IBranch>> loadBranchesCommand;
-        readonly ObservableAsPropertyHelper<bool> isLoading;
-        private bool IsLoading;
-
+       // readonly IReactiveCommand<IReadOnlyList<IBranch>> loadBranchesCommand;
+        //readonly ObservableAsPropertyHelper<bool> isLoading;
+        readonly IRepositoryHost repositoryHost;
+        readonly ISimpleRepositoryModel repository;
 
         [ImportingConstructor]
+        PullRequestCreationViewModel(
+            IConnectionRepositoryHostMap connectionRepositoryHostMap, ITeamExplorerServiceHolder teservice)
+            : this(connectionRepositoryHostMap.CurrentRepositoryHost, teservice.ActiveRepo)
+        { }
+
         public PullRequestCreationViewModel(IRepositoryHost repositoryHost, ISimpleRepositoryModel repository)
         {
-            Branches = new ReactiveList<IBranch>();
-            loadBranchesCommand = ReactiveCommand.CreateAsyncObservable(loadBranches);
-            isLoading = this.WhenAny(x => x.LoadingFailed, x => x.Value)
-                .CombineLatest(loadBranchesCommand.IsExecuting, (failed, loading) => !failed && loading)
-                .ToProperty(this, x => x.IsLoading);
-            loadBranchesCommand.Subscribe(Branches.AddRange);
+            this.repositoryHost = repositoryHost;
+            this.repository = repository;
+
+            //We don't need a reactive list
+            // Branches = new ReactiveList<IBranch>();
+            //loadBranchesCommand = ReactiveCommand.CreateAsyncObservable(LoadBranches);
+            Branches = new ReactiveUI.ReactiveList<IBranch>
+            {
+               new Models.Branch { Name = "don/stub-ui" },
+               new Models.Branch { Name = "feature/pr/views" },
+               new Models.Branch { Name = "release-1.0.17.0" }
+            };
+            //isLoading = this.WhenAny(x => x.LoadingFailed, x => x.Value)
+            // .CombineLatest(loadBranchesCommand.IsExecuting, (failed, loading) => !failed && loading)
+            //.ToProperty(this, x => x.IsLoading);
+           // loadBranchesCommand.Subscribe(Branches.AddRange);
            
         }
 
-        IReadOnlyList<IRepositoryModel> branches;
-
-        public IReactiveList<IBranch> Branches { get; }
+        IReactiveList<IBranch> branches;
+        public IReactiveList<IBranch> Branches
+        {
+            get { return branches ; }
+            private set { this.RaiseAndSetIfChanged(ref branches, value); }
+        }
 
         public IBranch CurrentBranch { get; private set; }
 
@@ -57,19 +75,23 @@ namespace GitHub.ViewModels
             }
         }
 
+        bool loadingFailed;
         public bool LoadingFailed
         {
             get { return loadingFailed; }
             private set { this.RaiseAndSetIfChanged(ref loadingFailed, value); }
         }
 
-        readonly IBranch repositoryHost;
-        private bool loadingFailed;
 
-        IObservable<IReadOnlyList<IBranch>> loadBranches(ISimpleRepositoryModel repository)
+       
+
+
+        IObservable<IReadOnlyList<IBranch>> LoadBranches(object value)
         {
-            //TODO:Need a Branch host, like RepositoryHost?
-            return new ModelService.GetBranches(repository)
+
+            var list = repositoryHost.ModelService.GetBranches(repository);
+
+            return list
                 .Catch<IReadOnlyList<IBranch>, Exception>(ex =>
                 {
                     log.Error("Error while loading repositories", ex);
