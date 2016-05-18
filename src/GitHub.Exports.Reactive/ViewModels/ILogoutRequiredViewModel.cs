46164950d78cfc2e15e4cba19e8781bfa100e599
Steven Kirk <grokys@gmail.com>
2016-05-18 14:45:27 +0100

    Removed unneeded directive.

--- src/GitHub.Exports.Reactive/ViewModels/ILogoutRequiredViewModel.cs
+++ src/GitHub.Exports.Reactive/ViewModels/ILogoutRequiredViewModel.cs
@@ -6,7 +6,6 @@ namespace GitHub.ViewModels
     /// <summary>
     /// Represents a view model for the "Log out Required" dialog..
     /// </summary>
-    [SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces")]
     public interface ILogoutRequiredViewModel : IViewModel
     {
         IReactiveCommand<ProgressState> Logout { get; }
