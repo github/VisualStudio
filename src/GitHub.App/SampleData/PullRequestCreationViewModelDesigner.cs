cb5f3071f80e6fccb67f3bbce190013423e72ec0
Andreia Gaita <shana@spoiledcat.net>
2016-06-08 15:36:04 +0200

    Implement pull request creation and branch data

--- src/GitHub.App/SampleData/PullRequestCreationViewModelDesigner.cs
+++ src/GitHub.App/SampleData/PullRequestCreationViewModelDesigner.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
 using GitHub.ViewModels;
 using System.Collections.Generic;
+using GitHub.Models;
 
 namespace GitHub.SampleData
 {
@@ -9,16 +10,18 @@ namespace GitHub.SampleData
     {
         public PullRequestCreationViewModelDesigner()
         {
-            Branches = new List<string>()
+            Branches = new List<IBranch>
             {
-                "don/stub-ui",
-                "feature/pr/views",
-                "release-1.0.17.0"
-            };
+                new BranchModel { Name = "master" },
+                new BranchModel { Name = "don/stub-ui" },
+                new BranchModel { Name = "feature/pr/views" },
+                new BranchModel { Name = "release-1.0.17.0" }
+            }.AsReadOnly();
 
-            CurrentBranchName = "fix-everything";
+            TargetBranch = new BranchModel { Name = "master" };
+            SourceBranch = Branches[2];
+
             SelectedAssignee = "Haacked (Phil Haack)";
-            TargetBranchName = "master";
             Users = new List<string>()
             {
                 "Haacked (Phil Haack)",
@@ -26,10 +29,11 @@ namespace GitHub.SampleData
             };
         }
 
-        public string CurrentBranchName { get; set; }
+        public IBranch SourceBranch { get; set; }
+        public IBranch TargetBranch { get; set; }
+        public IReadOnlyList<IBranch> Branches { get; set; }
+
         public string SelectedAssignee { get; set; }
-        public string TargetBranchName { get; set; }
-        public List<string> Branches { get; set; }
         public List<string> Users { get; set; }
     }
 }
