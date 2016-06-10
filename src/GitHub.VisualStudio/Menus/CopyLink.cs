ec7c3d91857d11203acea79f5b72a4a3e2a43345
Jasmine <paladique@github.com>
2016-06-10 13:45:04 -0400

    don't return the wait result, return the actual result

--- src/GitHub.VisualStudio/Menus/CopyLink.cs
+++ src/GitHub.VisualStudio/Menus/CopyLink.cs
@@ -48,7 +48,9 @@ namespace GitHub.VisualStudio.Menus
 
         public bool CanShow()
         {
-            return IsGitHubRepo().Wait(250); 
+            Task<bool> githubRepoCheckTask = IsGitHubRepo();
+            githubRepoCheckTask.Wait(250);
+            return githubRepoCheckTask.Result;
         }
 
     }
