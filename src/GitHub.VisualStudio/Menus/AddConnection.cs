04862c5e2a8e4dc7531f0f8edd0c164c56ee3430
Jasmine <paladique@github.com>
2016-06-23 13:19:36 -0400

    remove unrelated methods, restore relevant method

--- src/GitHub.VisualStudio/Menus/AddConnection.cs
+++ src/GitHub.VisualStudio/Menus/AddConnection.cs
@@ -11,7 +11,7 @@ namespace GitHub.VisualStudio.Menus
 {
     [Export(typeof(IMenuHandler))]
     [PartCreationPolicy(CreationPolicy.Shared)]
-    public class AddConnection: MenuBase, IMenuHandler
+    public class AddConnection : MenuBase, IMenuHandler
     {
         [ImportingConstructor]
         public AddConnection([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider, ISimpleApiClientFactory apiFactory)
@@ -25,6 +25,12 @@ namespace GitHub.VisualStudio.Menus
         public void Activate()
         {
             StartFlow(UIControllerFlow.Authentication);
+        }
+
+        void StartFlow(UIControllerFlow controllerFlow)
+        {
+            var uiProvider = ServiceProvider.GetExportedValue<IUIProvider>();
+            uiProvider.RunUI(controllerFlow, null);
         }
     }
 }
