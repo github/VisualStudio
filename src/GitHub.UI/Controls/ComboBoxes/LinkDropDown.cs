020b93aa98e8372f9c0ebe94d827d293dbc60003
Steven Kirk <grokys@gmail.com>
2016-07-06 18:16:51 +0200

    Added LinkDropDown control.
    And use it in PullRequestListView. Only used in one place so far, so it can be compared to the existing implementation.

---
+++ src/GitHub.UI/Controls/ComboBoxes/LinkDropDown.cs
@@ -0,0 +1,75 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GitHub.UI
+{
+    /// <summary>
+    /// A ComboBox that displays as a link with a dropdown.
+    /// </summary>
+    public class LinkDropDown : ComboBox
+    {
+        /// <summary>
+        /// Defines the <see cref="LinkText"/> property.
+        /// </summary>
+        static readonly DependencyPropertyKey LinkTextPropertyKey =
+            DependencyProperty.RegisterReadOnly(
+                "LinkText",
+                typeof(string),
+                typeof(LinkDropDown),
+                new FrameworkPropertyMetadata(string.Empty));
+
+        /// <summary>
+        /// Defines the <see cref="Header"/> property.
+        /// </summary>
+        public static readonly DependencyProperty HeaderProperty =
+            HeaderedItemsControl.HeaderProperty.AddOwner(
+                typeof(LinkDropDown),
+                new FrameworkPropertyMetadata(typeof(LinkDropDown), HeaderChanged));
+
+        /// <summary>
+        /// Defines the readonly <see cref="LinkText"/> property.
+        /// </summary>
+        public static readonly DependencyProperty LinkTextProperty =
+            LinkTextPropertyKey.DependencyProperty;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="LinkDropDown"/> class.
+        /// </summary>
+        static LinkDropDown()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(LinkDropDown),
+                new FrameworkPropertyMetadata(typeof(LinkDropDown)));
+        }
+
+        /// <summary>
+        /// Gets or sets a header to use as the link text when no item is selected.
+        /// </summary>
+        public object Header
+        {
+            get { return GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets the text to display in the link.
+        /// </summary>
+        public string LinkText => (string)GetValue(LinkTextProperty);
+
+        protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+        {
+            UpdateLinkText();
+        }
+
+        private static void HeaderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var source = (LinkDropDown)d;
+            source.UpdateLinkText();
+        }
+
+        private void UpdateLinkText()
+        {
+            SetValue(LinkTextPropertyKey, SelectedItem?.ToString() ?? Header?.ToString());
+        }
+    }
+}
