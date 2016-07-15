94171b3bcc462165b789863c374a308b09ff7f68
Steven Kirk <grokys@github.com>
2016-07-15 11:34:52 -0700

    Track login count metric.

--- src/GitHub.Exports/Services/IUsageTracker.cs
+++ src/GitHub.Exports/Services/IUsageTracker.cs
@@ -10,5 +10,6 @@
         void IncrementLinkToGitHubCount();
         void IncrementCreateGistCount();
         void IncrementUpstreamPullRequestCount();
+        void IncrementLoginCount();
     }
 }
