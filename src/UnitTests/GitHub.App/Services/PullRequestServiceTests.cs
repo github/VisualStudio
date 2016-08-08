4c6708121af6bdd53bb033d5ae4a9aae7dd5e8d1
Andreia Gaita <shana@spoiledcat.net>
2016-08-08 20:38:06 +0200

    Add tests for PR creation service and view model

---
+++ src/UnitTests/GitHub.App/Services/PullRequestServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Reactive.Linq;
+using System.Threading.Tasks;
+using NSubstitute;
+using Xunit;
+using UnitTests;
+using GitHub.Models;
+using System;
+using GitHub.Services;
+
+public class PullRequestServiceTests : TestBaseClass
+{
+    [Fact]
+    public async Task CreatePullRequestAllArgsMandatory()
+    {
+        var serviceProvider = Substitutes.ServiceProvider;
+        var service = new PullRequestService();
+
+        IRepositoryHost host = null;
+        ISimpleRepositoryModel repository = null;
+        string title = null;
+        string body = null;
+        IBranch source = null;
+        IBranch target = null;
+
+        Assert.Throws<ArgumentNullException>(() => service.CreatePullRequest(host, repository, title, body, source, target));
+
+        host = serviceProvider.GetRepositoryHosts().GitHubHost;
+        Assert.Throws<ArgumentNullException>(() => service.CreatePullRequest(host, repository, title, body, source, target));
+
+        repository = new SimpleRepositoryModel("name", new GitHub.Primitives.UriString("http://github.com/github/stuff"));
+        Assert.Throws<ArgumentNullException>(() => service.CreatePullRequest(host, repository, title, body, source, target));
+
+        title = "a title";
+        Assert.Throws<ArgumentNullException>(() => service.CreatePullRequest(host, repository, title, body, source, target));
+
+        body = "a body";
+        Assert.Throws<ArgumentNullException>(() => service.CreatePullRequest(host, repository, title, body, source, target));
+
+        source = new BranchModel() { Name = "source" };
+        Assert.Throws<ArgumentNullException>(() => service.CreatePullRequest(host, repository, title, body, source, target));
+
+        target = new BranchModel() { Name = "target" };
+        var pr = await service.CreatePullRequest(host, repository, title, body, source, target);
+
+        Assert.NotNull(pr);
+    }
+
+}
---
+++ src/UnitTests/GitHub.App/ViewModels/PullRequestCreationViewModelTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Reactive.Linq;
+using System.Threading.Tasks;
+using NSubstitute;
+using Xunit;
+using UnitTests;
+using GitHub.Models;
+using System;
+using GitHub.Services;
+using GitHub.ViewModels;
+
+public class PullRequestCreationViewModelTests : TempFileBaseClass
+{
+    [Fact]
+    public async Task NullDescriptionBecomesEmptyBody()
+    {
+        var serviceProvider = Substitutes.ServiceProvider;
+        var service = new PullRequestService();
+        var notifications = Substitute.For<INotificationService>();
+
+        var host = serviceProvider.GetRepositoryHosts().GitHubHost;
+        var ms = Substitute.For<IModelService>();
+        host.ModelService.Returns(ms);
+
+        var repository = new SimpleRepositoryModel("name", new GitHub.Primitives.UriString("http://github.com/github/stuff"));
+        var title = "a title";
+
+        var vm = new PullRequestCreationViewModel(host, repository, service, notifications);
+        vm.SourceBranch = new BranchModel() { Name = "source" };
+        vm.TargetBranch = new BranchModel() { Name = "target" };
+        vm.PRTitle = title;
+
+        await vm.CreatePullRequest.ExecuteAsync();
+        ms.Received().CreatePullRequest(repository, vm.PRTitle, String.Empty, vm.SourceBranch, vm.TargetBranch);
+    }
+
+}
