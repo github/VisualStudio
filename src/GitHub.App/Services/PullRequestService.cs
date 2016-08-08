9c6eb80f8073b55935aedb49c5ea27d8698b988f
Andreia Gaita <shana@spoiledcat.net>
2016-08-08 20:37:37 +0200

    NullGuard asserts are useless when testing in debug mode

--- src/GitHub.App/Services/PullRequestService.cs
+++ src/GitHub.App/Services/PullRequestService.cs
@@ -4,12 +4,20 @@ using GitHub.Models;
 
 namespace GitHub.Services
 {
+    [NullGuard.NullGuard(NullGuard.ValidationFlags.None)]
     [Export(typeof(IPullRequestService))]
     [PartCreationPolicy(CreationPolicy.Shared)]
     public class PullRequestService : IPullRequestService
     {
         public IObservable<IPullRequestModel> CreatePullRequest(IRepositoryHost host, ISimpleRepositoryModel repository, string title, string body, IBranch source, IBranch target)
         {
+            Extensions.Guard.ArgumentNotNull(host, nameof(host));
+            Extensions.Guard.ArgumentNotNull(repository, nameof(repository));
+            Extensions.Guard.ArgumentNotNull(title, nameof(title));
+            Extensions.Guard.ArgumentNotNull(body, nameof(body));
+            Extensions.Guard.ArgumentNotNull(source, nameof(source));
+            Extensions.Guard.ArgumentNotNull(target, nameof(target));
+
             return host.ModelService.CreatePullRequest(repository, title, body, source, target);
         }
     }
