ff356993563317a290ea69a701c5e9f7837eb19d
Andreia Gaita <shana@spoiledcat.net>
2016-08-22 16:42:42 +0200

    Grab VS version from registry

--- src/GitHub.Exports/Services/IVSServices.cs
+++ src/GitHub.Exports/Services/IVSServices.cs
@@ -5,6 +5,8 @@ namespace GitHub.Services
 {
     public interface IVSServices
     {
+        string VSVersion { get; }
+
         void ActivityLogMessage(string message);
         void ActivityLogWarning(string message);
         void ActivityLogError(string message);
--- src/GitHub.Exports/Services/VSServices.cs
+++ src/GitHub.Exports/Services/VSServices.cs
@@ -6,6 +6,7 @@ using System.Globalization;
 using GitHub.VisualStudio;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell.Interop;
+using System.Linq;
 
 namespace GitHub.Services
 {
@@ -19,6 +20,17 @@ namespace GitHub.Services
         public VSServices(IUIProvider serviceProvider)
         {
             this.serviceProvider = serviceProvider;
+        }
+
+        string vsVersion;
+        public string VSVersion
+        {
+            get
+            {
+                if (vsVersion == null)
+                    vsVersion = GetVSVersion();
+                return vsVersion;
+            }
         }
 
 
@@ -54,6 +66,31 @@ namespace GitHub.Services
                             Info.ApplicationInfo.ApplicationSafeName, message)))
                     Console.WriteLine(string.Format(CultureInfo.CurrentCulture, "Could not log warning to activity log: {0}", message));
             }
+        }
+
+        const string RegistryRootKey = @"Software\Microsoft\VisualStudio";
+        const string EnvVersionKey = "EnvVersion";
+        string GetVSVersion()
+        {
+            var version = VisualStudio.Services.Dte.Version;
+            var keyPath = String.Format(CultureInfo.InvariantCulture, "{0}\\{1}_Config\\SplashInfo", RegistryRootKey, version);
+            try
+            {
+                using (var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(keyPath))
+                {
+                    var value = (string)key.GetValue(EnvVersionKey, String.Empty);
+                    if (!String.IsNullOrEmpty(value))
+                        return value;
+                }
+                var asm = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.FullName.StartsWith("Microsoft.VisualStudio.CommonIDE", StringComparison.OrdinalIgnoreCase));
+                if (asm != null)
+                    return asm.GetName().Version.ToString();
+            }
+            catch(Exception ex)
+            {
+                VsOutputLogger.WriteLine(string.Format(CultureInfo.CurrentCulture, "Error getting the Visual Studio version '{0}'", ex));
+            }
+            return version;
         }
     }
 }
