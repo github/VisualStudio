d6178964e7201cbb0f0f566a68f884eeaa36755c
Dan Walmsley <dan@walms.co.uk>
2016-09-20 22:33:54 +0100

    Corrected logic for filterTextEnabled observable.

--- src/GitHub.App/ViewModels/RepositoryCloneViewModel.cs
+++ src/GitHub.App/ViewModels/RepositoryCloneViewModel.cs
@@ -75,13 +75,12 @@ namespace GitHub.ViewModels
             repositories.Filter = FilterRepository;
             repositories.NewerComparer = OrderedComparer<IRemoteRepositoryModel>.OrderByDescending(x => x.UpdatedAt).Compare;
 
-            filterTextIsEnabled = this.WhenAny(x => x.IsLoading, x => x.LoadingFailed, 
-                (loading, loadingFailed) => !loadingFailed.Value)
-                .Select(x => x || repositories.UnfilteredCount > 0)
-                .ToProperty(this, x => x.FilterTextIsEnabled, initialValue: true);
+            filterTextIsEnabled = this.WhenAny(x => x.IsLoading,
+                (loading) => loading.Value || repositories.UnfilteredCount > 0 && !LoadingFailed)
+                .ToProperty(this, x => x.FilterTextIsEnabled);
 
-            this.WhenAny(x => x.FilterTextIsEnabled, x => x.IsLoading, x => x.LoadingFailed,
-                (any, loading, failed) => !loading.Value && !failed.Value && repositories.UnfilteredCount == 0)
+            this.WhenAny(x => x.IsLoading, x => x.LoadingFailed,
+                (loading, failed) => !loading.Value && !failed.Value && repositories.UnfilteredCount == 0)
                 .Subscribe(x => NoRepositoriesFound = x);
 
             this.WhenAny(x => x.FilterText, x => x.Value)
