4724715b83528adfb546728b0f59ed1a4439079a
Stanley Goldman <Stanley.Goldman@gmail.com>
2016-10-15 08:44:18 -0400

    Improving the unit test

--- src/UnitTests/GitHub.App/Models/AccountModelTests.cs
+++ src/UnitTests/GitHub.App/Models/AccountModelTests.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading;
 using System.Windows.Media.Imaging;
 using GitHub.Collections;
@@ -17,78 +18,79 @@ namespace UnitTests.GitHub.App.Models
         [Fact]
         public void CopyFromDoesNotLoseAvatar()
         {
-            //A function that will return this image in an observable after X seconds
             var userImage = AvatarProvider.CreateBitmapImage("pack://application:,,,/GitHub.App;component/Images/default_user_avatar.png");
             var orgImage = AvatarProvider.CreateBitmapImage("pack://application:,,,/GitHub.App;component/Images/default_org_avatar.png");
 
-            Func<int, BitmapImage, IObservable<BitmapImage>> generateObservable = (seconds, image) =>
-            {
-                return Observable.Generate(
-                    initialState: 0, 
-                    condition: i => i < 1, 
-                    iterate: state => state + 1,
-                    resultSelector: i => image, 
-                    timeSelector: i => TimeSpan.FromSeconds(seconds));
-            };
+            var bitmapImageObservable = new Subject<BitmapImage>();
 
-            var evt = new ManualResetEvent(false);
+            var collectionEvent = new ManualResetEvent(false);
+            var avatarPropertyEvent = new ManualResetEvent(false);
 
             //Creating an initial account with an observable that returns immediately
             const string login = "foo";
             const int initialOwnedPrivateRepositoryCount = 1;
 
-            var initialAccount = new Account(login, true, false, initialOwnedPrivateRepositoryCount, 0, generateObservable(0, userImage));
+            var initialAccount = new Account(login, true, false, initialOwnedPrivateRepositoryCount, 0, bitmapImageObservable);
 
             //Creating the test collection
             var col = new TrackingCollection<IAccount>(Observable.Empty<IAccount>(), OrderedComparer<IAccount>.OrderByDescending(x => x.Login).Compare);
             col.Subscribe(account =>
             {
-                evt.Set();
+                collectionEvent.Set();
             }, () => { });
 
             //Adding that account to the TrackingCollection
             col.AddItem(initialAccount);
 
             //Waiting for the collection add the item
-            evt.WaitOne();
-            evt.Reset();
+            collectionEvent.WaitOne();
+            collectionEvent.Reset();
 
             //Checking some initial properties
             Assert.Equal(login, col[0].Login);
             Assert.Equal(initialOwnedPrivateRepositoryCount, col[0].OwnedPrivateRepos);
 
-            //Demonstrating that the avatar is present
-            Assert.NotNull(col[0].Avatar);
-            Assert.True(BitmapSourcesAreEqual(col[0].Avatar, userImage));
-            Assert.False(BitmapSourcesAreEqual(col[0].Avatar, orgImage));
-
-            //Creating an observable that will return in one second
-            var updatedBitmapSourceObservable = generateObservable(1, orgImage);
-
-            //Creating an account update with an observable
-            const int updatedOwnedPrivateRepositoryCount = 2;
-
-            var updatedAccount = new Account(login, true, false, updatedOwnedPrivateRepositoryCount, 0, updatedBitmapSourceObservable);
+            //Demonstrating that the avatar is not yet present
+            Assert.Null(col[0].Avatar);
 
             //Adding a listener to check for the changing of the Avatar property
             initialAccount.Changed.Subscribe(args =>
             {
                 if (args.PropertyName == "Avatar")
                 {
-                    evt.Set();
+                    avatarPropertyEvent.Set();
                 }
             });
 
-            //Updating the accout in the collection
+            //Providing the first avatar
+            bitmapImageObservable.OnNext(userImage);
+
+            //Waiting for the avatar to be added
+            avatarPropertyEvent.WaitOne();
+            avatarPropertyEvent.Reset();
+
+            //Demonstrating that the avatar is present
+            Assert.NotNull(col[0].Avatar);
+            Assert.True(BitmapSourcesAreEqual(col[0].Avatar, userImage));
+            Assert.False(BitmapSourcesAreEqual(col[0].Avatar, orgImage));
+
+            //Creating an account update
+            const int updatedOwnedPrivateRepositoryCount = 2;
+            var updatedAccount = new Account(login, true, false, updatedOwnedPrivateRepositoryCount, 0, bitmapImageObservable);
+
+            //Updating the account in the collection
             col.AddItem(updatedAccount);
 
             //Waiting for the collection to process the update
-            evt.WaitOne();
-            evt.Reset();
+            collectionEvent.WaitOne();
+            collectionEvent.Reset();
+
+            //Providing the second avatar
+            bitmapImageObservable.OnNext(orgImage);
 
             //Waiting for the delayed bitmap image observable
-            evt.WaitOne();
-            evt.Reset();
+            avatarPropertyEvent.WaitOne();
+            avatarPropertyEvent.Reset();
 
             //Login is the id, so that should be the same
             Assert.Equal(login, col[0].Login);
