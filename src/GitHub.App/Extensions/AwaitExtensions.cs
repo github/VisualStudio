603ebf6a2bc80c347a9605ed46629dc868b57fd0
Andreia Gaita <shana@spoiledcat.net>
2016-10-26 00:35:29 +0200

    Add a helper awaiter for fast thread switching
    This awaiter extension adds a `GetAwaiter` method to `TaskScheduler` so
    we can do `await TaskScheduler.Default` and it will switch to a
    background thread if it's not on one yet (otherwise continue on the same
    thread).

---
+++ src/GitHub.App/Extensions/AwaitExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GitHub.Extensions
+{
+    [NullGuard.NullGuard(NullGuard.ValidationFlags.None)]
+    public static class AwaitExtensions
+    {
+        public static TaskSchedulerAwaiter GetAwaiter(this TaskScheduler scheduler)
+        {
+            Guard.ArgumentNotNull(scheduler, nameof(scheduler));
+            return new TaskSchedulerAwaiter(scheduler);
+        }
+
+        public struct TaskSchedulerAwaiter : INotifyCompletion
+        {
+            readonly TaskScheduler scheduler;
+
+            public bool IsCompleted
+            {
+                get
+                {
+                    bool isThreadPoolThread = Thread.CurrentThread.IsThreadPoolThread;
+                    return (scheduler == TaskScheduler.Default & isThreadPoolThread) || (scheduler == TaskScheduler.Current && TaskScheduler.Current != TaskScheduler.Default);
+                }
+            }
+
+            public TaskSchedulerAwaiter(TaskScheduler scheduler)
+            {
+                Guard.ArgumentNotNull(scheduler, nameof(scheduler));
+                this.scheduler = scheduler;
+            }
+
+            public void OnCompleted(Action continuation)
+            {
+                Task.Factory.StartNew(continuation, CancellationToken.None, TaskCreationOptions.None, scheduler);
+            }
+
+            public void GetResult()
+            {
+            }
+        }
+    }
+}
