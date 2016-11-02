83629b9a994c00efa0f681a2cbf4bcee1e26216f
Jakub Swietek <jakub.swietek@gmail.com>
2016-11-02 10:55:44 +0100

    Refactored indentation to spaces
    Fixed indentation by changing tabs to spaces

--- src/GitHub.App/ViewModels/PullRequestCreationViewModel.cs
+++ src/GitHub.App/ViewModels/PullRequestCreationViewModel.cs
@@ -108,12 +108,11 @@ namespace GitHub.ViewModels
                         notifications.ShowError(error?.Message ?? ex.Message);
                         return Observable.Empty<IPullRequestModel>();
                     }))
-
             .OnExecuteCompleted(pr =>
             {
-				notifications.ShowMessage(String.Format(CultureInfo.CurrentCulture, Resources.PRCreatedUpstream, TargetBranch.Repository.Owner + "/" + TargetBranch.Repository.Name,
-					TargetBranch.Repository.CloneUrl.ToRepositoryUrl().Append("pull/" + pr.Number)));
-			});
+                notifications.ShowMessage(String.Format(CultureInfo.CurrentCulture, Resources.PRCreatedUpstream, TargetBranch.Repository.Owner + "/" + TargetBranch.Repository.Name,
+                    TargetBranch.Repository.CloneUrl.ToRepositoryUrl().Append("pull/" + pr.Number)));
+            });
 
             isExecuting = CreatePullRequest.IsExecuting.ToProperty(this, x => x.IsExecuting);
 
