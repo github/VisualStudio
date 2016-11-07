1c561dd7d8954aadba43d62907fb26291eec9d75
Andreia Gaita <shana@spoiledcat.net>
2016-11-07 14:32:17 +0100

    Add missing files

---
+++ src/GitHub.Exports.Reactive/ViewModels/IBaseCloneViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Reactive;
+using GitHub.Models;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+
+namespace GitHub.ViewModels
+{
+    /// <summary>
+    /// ViewModel for the the Clone Repository dialog
+    /// </summary>
+    public interface IBaseCloneViewModel : IViewModel, IRepositoryCreationTarget
+    {
+        /// <summary>
+        /// Command to clone the currently selected repository.
+        /// </summary>
+        IReactiveCommand<Unit> CloneCommand { get; }
+
+        ISimpleRepositoryModel SelectedRepository { get; set; }
+    }
+}
