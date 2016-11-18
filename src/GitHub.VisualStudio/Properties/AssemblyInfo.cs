753dafbc71fd378405fd60d3a4e83196e820ca71
Steven Kirk <grokys@gmail.com>
2016-11-18 11:42:14 +0100

    Added commented-out [ProvideCodeBase] attrs.

--- src/GitHub.VisualStudio/Properties/AssemblyInfo.cs
+++ src/GitHub.VisualStudio/Properties/AssemblyInfo.cs
@@ -1,6 +1,20 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
 
 [assembly: AssemblyTitle("GitHub.VisualStudio")]
 [assembly: AssemblyDescription("GitHub for Visual Studio VSPackage")]
 [assembly: Guid("fad77eaa-3fe1-4c4b-88dc-3753b6263cd7")]
+
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.Api.dll")]
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.App.dll")]
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.CredentialManagement.dll")]
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.Exports.dll")]
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.Exports.Reactive.dll")]
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.Extensions.dll")]
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.Extensions.Reactive.dll")]
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.UI.dll")]
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.UI.Reactive.dll")]
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.TeamFoundation.14.dll")]
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.TeamFoundation.15.dll")]
+//[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\GitHub.VisualStudio.UI.dll")]
