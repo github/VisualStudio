77addfac64da91f6c3b9751644378f219a30d0e2
Jamie Cansdale <jcansdale@github.com>
2017-02-03 19:32:41 +0000

    Split <summary> and <remarks> of TryOpenRepository

--- src/GitHub.Exports/Services/VSServices.cs
+++ src/GitHub.Exports/Services/VSServices.cs
@@ -73,12 +73,13 @@ namespace GitHub.Services
             }
         }
 
-        /// <summary>
+        /// <summary>Open a repository in Team Explorer</summary>
+        /// <remarks>
         /// There doesn't appear to be a command that directly opens a target repo.
         /// Our workaround is to create, open and delete a solution in the repo directory.
         /// This triggers an event that causes the target repo to open. ;)
-        /// </summary>
-        /// <param name="repoPath">The target repo directory.</param>
+        /// </remarks>
+        /// <param name="repoPath">The path to the repository to open</param>
         /// <returns>True if a transient solution was successfully created in target directory (which should trigger opening of repository).</returns>
         public bool TryOpenRepository(string repoPath)
         {
