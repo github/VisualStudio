d73b4b7980f4e5b65fa2e118cfdf354ba3ee397a
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-02-08 10:19:26 -0500

    Adding Assert methods; Adding additional methods to Log; Code Cleanup

--- src/GitHub.App/Infrastructure/LogManager.cs
+++ src/GitHub.App/Infrastructure/LogManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using GitHub.Extensions;
 using Serilog;
@@ -13,10 +14,11 @@ namespace GitHub.Infrastructure
             var environment = new Rothko.Environment();
             var logPath = Path.Combine(environment.GetLocalGitHubApplicationDataPath(), "extension.log");
 
-            const string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff}|{Level}|Thread:{ThreadId}|{SourceContext}|{Message}{NewLine}{Exception}";
+            const string outputTemplate =
+                "{Timestamp:yyyy-MM-dd HH:mm:ss.fff}|{Level}|Thread:{ThreadId}|{SourceContext}|{Message}{NewLine}{Exception}";
 
             //2MBs
-            const long fileSizeLimitBytes = 2L*1024L*1024L;
+            const long fileSizeLimitBytes = 2L * 1024L * 1024L;
 
             return new LoggerConfiguration()
                 .Enrich.WithThreadId()
@@ -29,9 +31,9 @@ namespace GitHub.Infrastructure
         static Lazy<Logger> Logger { get; } = new Lazy<Logger>(CreateLogger);
 
         //Violates CA1004 - Generic methods should provide type parameter
-        #pragma warning disable CA1004
+#pragma warning disable CA1004
         public static ILogger ForContext<T>() => Logger.Value.ForContext<T>();
-        #pragma warning restore CA1004
+#pragma warning restore CA1004
 
         public static ILogger ForContext(Type type) => Logger.Value.ForContext(type);
     }
@@ -40,17 +42,101 @@ namespace GitHub.Infrastructure
     {
         private static Lazy<ILogger> Logger { get; } = new Lazy<ILogger>(() => LogManager.ForContext(typeof(Log)));
 
-        public static void Information(Exception exception, string messageTemplate) => Logger.Value.Information(exception, messageTemplate);
-        public static void Information(Exception exception, string messageTemplate, params object[] propertyValues) => Logger.Value.Information(exception, messageTemplate, propertyValues);
-        public static void Debug(Exception exception, string messageTemplate) => Logger.Value.Debug(exception, messageTemplate);
-        public static void Debug(Exception exception, string messageTemplate, params object[] propertyValues) => Logger.Value.Debug(exception, messageTemplate, propertyValues);
-        public static void Error(Exception exception, string messageTemplate) => Logger.Value.Error(exception, messageTemplate);
-        public static void Error(Exception exception, string messageTemplate, params object[] propertyValues) => Logger.Value.Error(exception, messageTemplate, propertyValues);
-        public static void Fatal(Exception exception, string messageTemplate) => Logger.Value.Fatal(exception, messageTemplate);
-        public static void Fatal(Exception exception, string messageTemplate, params object[] propertyValues) => Logger.Value.Fatal(exception, messageTemplate, propertyValues);
-        public static void Verbose(Exception exception, string messageTemplate) => Logger.Value.Verbose(exception, messageTemplate);
-        public static void Verbose(Exception exception, string messageTemplate, params object[] propertyValues) => Logger.Value.Verbose(exception, messageTemplate, propertyValues);
-        public static void Warning(Exception exception, string messageTemplate) => Logger.Value.Warning(exception, messageTemplate);
-        public static void Warning(Exception exception, string messageTemplate, params object[] propertyValues) => Logger.Value.Warning(exception, messageTemplate, propertyValues);
+        public static void Information(string messageTemplate)
+            => Logger.Value.Information(messageTemplate);
+
+        public static void Information(string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Information(messageTemplate, propertyValues);
+
+        public static void Information(Exception exception, string messageTemplate)
+            => Logger.Value.Information(exception, messageTemplate);
+
+        public static void Information(Exception exception, string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Information(exception, messageTemplate, propertyValues);
+
+        public static void Debug(string messageTemplate)
+            => Logger.Value.Debug(messageTemplate);
+
+        public static void Debug(string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Debug(messageTemplate, propertyValues);
+
+        public static void Debug(Exception exception, string messageTemplate)
+            => Logger.Value.Debug(exception, messageTemplate);
+
+        public static void Debug(Exception exception, string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Debug(exception, messageTemplate, propertyValues);
+
+        public static void Error(string messageTemplate)
+            => Logger.Value.Error(messageTemplate);
+
+        public static void Error(string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Error(messageTemplate, propertyValues);
+
+        public static void Error(Exception exception, string messageTemplate)
+            => Logger.Value.Error(exception, messageTemplate);
+
+        public static void Error(Exception exception, string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Error(exception, messageTemplate, propertyValues);
+
+        public static void Fatal(Exception exception, string messageTemplate)
+            => Logger.Value.Fatal(exception, messageTemplate);
+
+        public static void Fatal(Exception exception, string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Fatal(exception, messageTemplate, propertyValues);
+
+        public static void Fatal(string messageTemplate)
+            => Logger.Value.Fatal(messageTemplate);
+
+        public static void Fatal(string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Fatal(messageTemplate, propertyValues);
+
+        public static void Verbose(string messageTemplate)
+            => Logger.Value.Verbose(messageTemplate);
+
+        public static void Verbose(string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Verbose(messageTemplate, propertyValues);
+
+        public static void Verbose(Exception exception, string messageTemplate)
+            => Logger.Value.Verbose(exception, messageTemplate);
+
+        public static void Verbose(Exception exception, string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Verbose(exception, messageTemplate, propertyValues);
+
+        public static void Warning(string messageTemplate)
+            => Logger.Value.Warning(messageTemplate);
+
+        public static void Warning(string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Warning(messageTemplate, propertyValues);
+
+        public static void Warning(Exception exception, string messageTemplate)
+            => Logger.Value.Warning(exception, messageTemplate);
+
+        public static void Warning(Exception exception, string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Warning(exception, messageTemplate, propertyValues);
+
+        public static void Assert(bool condition, string messageTemplate)
+            => Logger.Value.Assert(condition, messageTemplate);
+
+        public static void Assert(bool condition, string messageTemplate, params object[] propertyValues)
+            => Logger.Value.Assert(condition, messageTemplate, propertyValues);
+    }
+
+    public static class ILoggerExtensions
+    {
+        public static void Assert(this ILogger logger, bool condition, string messageTemplate)
+        {
+            if (!condition)
+            {
+                logger.Warning($"Assertion Failed: {messageTemplate}");
+            }
+        }
+
+        public static void Assert(this ILogger logger, bool condition, string messageTemplate, params object[] propertyValues)
+        {
+            if (!condition)
+            {
+                logger.Warning($"Assertion Failed: {messageTemplate}", propertyValues);
+            }
+        }
     }
 }
