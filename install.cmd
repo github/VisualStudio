1d87ff07bc29affda149ce1ac7e586a6e71db963
Jamie Cansdale <jcansdale@gmail.com>
2017-02-23 20:32:09 +0000

    Add update script to reinstall extension
    Updated script uses `vsixutil.exe` which is like a scriptable version
    of `vsixinstaller.exe`. Extension will be installed for all versions of
    Visual Studio. It will only be installed for one VS 2017 SKU
    (prioritizing Enterprise, Professional then Community).

--- install.cmd
+++ install.cmd
@@ -1,3 +1,6 @@
-@set PATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE;%PATH%
-vsixinstaller /q /u:"c3d3dc68-c977-411f-b3e8-03b0dccf7dfc"
-vsixinstaller "%cd%\build\Debug\GitHub.VisualStudio.vsix"
+@if "%config%" == "" set config=Debug
+
+@set path=%cd%\tools\VsixUtil;%path%
+
+@echo Installing `%config%` build of GitHub for Vidual Studio
+vsixutil /install "%cd%\build\%config%\GitHub.VisualStudio.vsix" /s Enterprise;Professional;Community
