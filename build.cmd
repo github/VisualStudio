b37d84ac25847655515fcc3026d990215cc976a7
Jamie Cansdale <jcansdale@gmail.com>
2017-02-28 14:35:09 +0000

    Change `build.cmd` to build Experimental version
    The `Debug` configuration will be build with AllUsers=false and
    Experimental=true. This configuration can be installed without needing
    to uninstall the `AllUsers` version.
    Changed .cmd so that it works without the private `script` repo.

--- build.cmd
+++ build.cmd
@@ -1,1 +1,4 @@
-powershell.exe .\script\cibuild.ps1
+@if "%config%" == "" set config=Debug
+
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+msbuild GitHubVS.sln /p:Configuration=%Config%
