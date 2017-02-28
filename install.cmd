ae32e3f53f194da08fe8c579fe06ec0fe8d43b38
Jasmine Greenaway <jasminegweb@gmail.com>
2017-02-28 10:55:37 -0500

    fix typo in echo command

--- install.cmd
+++ install.cmd
@@ -2,5 +2,5 @@
 
 @set path=%cd%\tools\VsixUtil;%path%
 
-@echo Installing `%config%` build of GitHub for Vidual Studio
+@echo Installing `%config%` build of GitHub for Visual Studio
 vsixutil /install "%cd%\build\%config%\GitHub.VisualStudio.vsix" /s Enterprise;Professional;Community
