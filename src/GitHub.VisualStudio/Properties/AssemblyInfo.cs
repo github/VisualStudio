30aac34376ed90fa818bcec51c7bee53c66a110a
Jamie Cansdale <jcansdale@gmail.com>
2017-03-07 13:27:45 +0000

    ProvideCodeBase for resource assemblies
    Assemblies that contain resources (and their dependencies) need to be
    discoverable by their full name.

--- src/GitHub.VisualStudio/Properties/AssemblyInfo.cs
+++ src/GitHub.VisualStudio/Properties/AssemblyInfo.cs
@@ -1,6 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
 
 [assembly: AssemblyTitle("GitHub.VisualStudio")]
 [assembly: AssemblyDescription("GitHub for Visual Studio VSPackage")]
 [assembly: Guid("fad77eaa-3fe1-4c4b-88dc-3753b6263cd7")]
+
+[assembly: ProvideCodeBase(AssemblyName = "GitHub.UI", CodeBase = @"$PackageFolder$\GitHub.UI.dll")]
+[assembly: ProvideCodeBase(AssemblyName = "GitHub.VisualStudio.UI", CodeBase = @"$PackageFolder$\GitHub.VisualStudio.UI.dll")]
+[assembly: ProvideCodeBase(AssemblyName = "GitHub.Exports", CodeBase = @"$PackageFolder$\GitHub.Exports.dll")]
+[assembly: ProvideCodeBase(AssemblyName = "GitHub.Extensions", CodeBase = @"$PackageFolder$\GitHub.Extensions.dll")]
+[assembly: ProvideCodeBase(AssemblyName = "Octokit", CodeBase = @"$PackageFolder$\Octokit.dll")]
+[assembly: ProvideCodeBase(AssemblyName = "LibGit2Sharp", CodeBase = @"$PackageFolder$\LibGit2Sharp.dll")]
+[assembly: ProvideCodeBase(AssemblyName = "Splat", CodeBase = @"$PackageFolder$\Splat.dll")]
+[assembly: ProvideCodeBase(AssemblyName = "Rothko", CodeBase = @"$PackageFolder$\Rothko.dll")]
+[assembly: ProvideCodeBase(AssemblyName = "GitHub.VisualStudio.TestAutomation.AutomationIDs",
+    CodeBase = @"$PackageFolder$\GitHub.VisualStudio.TestAutomation.AutomationIDs.dll")]
