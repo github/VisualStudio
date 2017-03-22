fed05bb7bd11b4ea9c81a28d2ea56902bc88a303
Steven Kirk <grokys@gmail.com>
2017-03-22 16:52:43 +0100

    Renamed ICanLoad -> ICanNavigate.
    The term "load" is rather um, overloaded, so try using something else.

---
+++ src/GitHub.Exports/ViewModels/ICanNavigate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GitHub.UI;
+
+namespace GitHub.ViewModels
+{
+    /// <summary>
+    /// Interface for view models in a navigable panel such as the GitHub pane which can signal
+    /// to navigate to another page.
+    /// </summary>
+    public interface ICanNavigate
+    {
+        /// <summary>
+        /// Gets an observable which is signalled with the page to navigate to.
+        /// </summary>
+        IObservable<ViewWithData> Navigate { get; }
+    }
+}
