8a47e987ecf27e82acb3378ea8c26a18dc2bfa8f
Jamie Cansdale <jcansdale@gmail.com>
2017-04-01 11:28:45 +0100

    Mega refactoring of SharedDictionaryManager
    Move all caching and loading responsibilities into SharedDictionaryManagerBase.
    Move theme support into ThemeDictionaryManager.
    Always add ResourceDictionary to MergedDictionaries (not just when there is a cache hit).
    Allow `Source` to be set multiple times to swap out ResourceDictionary (used when changing theme).

--- src/GitHub.UI/Helpers/SharedDictionaryManager.cs
+++ src/GitHub.UI/Helpers/SharedDictionaryManager.cs
@@ -1,31 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Windows;
-using GitHub.Helpers;
+﻿using GitHub.Helpers;
 
 namespace GitHub.UI.Helpers
 {
     public class SharedDictionaryManager : SharedDictionaryManagerBase
     {
-        static readonly Dictionary<Uri, ResourceDictionary> resourceDicts = new Dictionary<Uri, ResourceDictionary>();
-
-        Uri sourceUri;
-        public new Uri Source
-        {
-            get { return sourceUri; }
-            set
-            {
-                sourceUri = value;
-                ResourceDictionary ret;
-                if (resourceDicts.TryGetValue(value, out ret))
-                {
-                    MergedDictionaries.Add(ret);
-                    return;
-                }
-                base.Source = value;
-                resourceDicts.Add(value, this);
-            }
-        }
     }
 }
---
+++ src/GitHub.VisualStudio.UI/Helpers/ThemeDictionaryManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+using System.ComponentModel;
+using Microsoft.VisualStudio.PlatformUI;
+using GitHub.VisualStudio.Helpers;
+using GitHub.Helpers;
+
+namespace GitHub.VisualStudio.UI.Helpers
+{
+    public class ThemeDictionaryManager : SharedDictionaryManagerBase, IDisposable
+    {
+        static bool isInDesignMode;
+        Uri baseThemeUri;
+
+        static ThemeDictionaryManager()
+        {
+            isInDesignMode = DesignerProperties.GetIsInDesignMode(new DependencyObject());
+        }
+
+        public override Uri Source
+        {
+            get { return base.Source; }
+            set
+            {
+                InitTheme(value);
+                base.Source = GetCurrentThemeUri();
+            }
+        }
+
+        public void Dispose()
+        {
+            if(baseThemeUri != null)
+            {
+                baseThemeUri = null;
+                if (!isInDesignMode)
+                {
+                    VSColorTheme.ThemeChanged -= OnThemeChange;
+                }
+            }
+        }
+
+        void InitTheme(Uri themeUri)
+        {
+            if (baseThemeUri == null)
+            {
+                baseThemeUri = themeUri;
+                if (!isInDesignMode)
+                {
+                    VSColorTheme.ThemeChanged += OnThemeChange;
+                }
+            }
+        }
+
+        void OnThemeChange(ThemeChangedEventArgs e)
+        {
+            base.Source = GetCurrentThemeUri();
+        }
+
+        Uri GetCurrentThemeUri()
+        {
+            if (isInDesignMode)
+            {
+                return baseThemeUri;
+            }
+
+            var currentTheme = Colors.DetectTheme();
+            return new Uri(baseThemeUri, $"Theme{currentTheme}.xaml");
+        }
+    }
+}
