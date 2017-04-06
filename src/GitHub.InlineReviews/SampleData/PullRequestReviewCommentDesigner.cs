f92377745a82ed6fb238e91b8330b7226962f6fd
Steven Kirk <grokys@gmail.com>
2017-04-06 18:24:40 +0200

    Make CommentView look a bit nicer.

---
+++ src/GitHub.InlineReviews/SampleData/PullRequestReviewCommentDesigner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GitHub.Models;
+using GitHub.SampleData;
+
+namespace GitHub.InlineReviews.SampleData
+{
+    public class PullRequestReviewCommentDesigner : IPullRequestReviewCommentModel
+    {
+        public string Path => string.Empty;
+        public int? Position => 1;
+        public DateTimeOffset UpdatedAt => DateTime.Now.Subtract(TimeSpan.FromDays(3));
+        public IAccount User => new AccountDesigner { Login = "shana", IsUser = true };
+        public string Body => @"You can use a `CompositeDisposable` type here, it's designed to handle disposables in an optimal way (you can just call `Dispose()` on it and it will handle disposing everything it holds).";
+    }
+}
