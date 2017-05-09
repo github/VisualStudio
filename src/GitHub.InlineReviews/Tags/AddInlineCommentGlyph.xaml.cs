82480b7ab7b8b1a5515fa85dd5c68d18cdbe0dd3
Jamie Cansdale <jcansdale@gmail.com>
2017-05-09 21:08:14 +0100

    Show and hide Glyph using events not overrides

--- src/GitHub.InlineReviews/Tags/AddInlineCommentGlyph.xaml.cs
+++ src/GitHub.InlineReviews/Tags/AddInlineCommentGlyph.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace GitHub.InlineReviews.Tags
 {
@@ -9,19 +8,10 @@ namespace GitHub.InlineReviews.Tags
         public AddInlineCommentGlyph()
         {
             InitializeComponent();
-            Visibility = System.Windows.Visibility.Hidden;
-        }
 
-        protected override void OnMouseEnter(MouseEventArgs e)
-        {
-            Visibility = System.Windows.Visibility.Visible;
-            base.OnMouseEnter(e);
-        }
-
-        protected override void OnMouseLeave(MouseEventArgs e)
-        {
             Visibility = System.Windows.Visibility.Hidden;
-            base.OnMouseLeave(e);
+            MouseEnter += (s, e) => Visibility = System.Windows.Visibility.Visible;
+            MouseLeave += (s, e) => Visibility = System.Windows.Visibility.Hidden;
         }
     }
 }
