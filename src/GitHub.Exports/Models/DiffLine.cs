dfa066dca9be9f0797de42f459293c67c612fb1f
Jamie Cansdale <jcansdale@gmail.com>
2017-06-02 15:06:42 +0100

    Add doc comments for `DiffLine`

--- src/GitHub.Exports/Models/DiffLine.cs
+++ src/GitHub.Exports/Models/DiffLine.cs
@@ -4,10 +4,29 @@ namespace GitHub.Models
 {
     public class DiffLine
     {
+        /// <summary>
+        /// Was the line added, deleted or unchanged.
+        /// </summary>
         public DiffChangeType Type { get; set; }
+
+        /// <summary>
+        /// Gets the old 1-based line number.
+        /// </summary>
         public int OldLineNumber { get; set; } = -1;
+
+        /// <summary>
+        /// Gets the new 1-based line number.
+        /// </summary>
         public int NewLineNumber { get; set; } = -1;
+
+        /// <summary>
+        /// Gets the unified diff line number where the first chunk header is line 0.
+        /// </summary>
         public int DiffLineNumber { get; set; } = -1;
+
+        /// <summary>
+        /// Gets the content of the diff line (including +, - or space).
+        /// </summary>
         public string Content { get; set; }
 
         public override string ToString() => Content;
