e403eea8a999dcba4821296dbdd0ebeb1f99c53a
Steven Kirk <grokys@gmail.com>
2017-06-20 12:13:35 +0200

    Added Markdown unit tests.

---
+++ test/Markdown.Xaml.Tests/MarkdownTests.cs
@@ -0,0 +1,99 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using System.IO;
+using System.Reflection;
+using System.Windows.Markup;
+using System.Xml;
+
+namespace Markdown.Xaml.Tests
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    public class MarkdownTests
+    {
+        [Test]
+        public void Transform_givenTest1_generatesExpectedResult()
+        {
+            var text = LoadText("Test1.md");
+            var markdown = new GitHub.UI.Markdown();
+            var result = markdown.Transform(text);
+            Approvals.Verify(AsXaml(result));
+        }
+
+        [Test]
+        [RequiresSTA]
+        public void Transform_givenLists_generatesExpectedResult()
+        {
+            var text = LoadText("Lists.md");
+            var markdown = new GitHub.UI.Markdown();
+            var result = markdown.Transform(text);
+            Approvals.Verify(AsXaml(result));
+        }
+
+        [Test]
+        [RequiresSTA]
+        public void Transform_givenHorizontalRules_generatesExpectedResult()
+        {
+            var text = LoadText("HorizontalRules.md");
+            var markdown = new GitHub.UI.Markdown();
+            var result = markdown.Transform(text);
+            Approvals.Verify(AsXaml(result));
+        }
+
+        [Test]
+        [RequiresSTA]
+        public void Transform_givenLinksInline_generatesExpectedResult()
+        {
+            var text = LoadText("Links_inline_style.md");
+            var markdown = new GitHub.UI.Markdown();
+            var result = markdown.Transform(text);
+            Approvals.Verify(AsXaml(result));
+        }
+
+        [Test]
+        [RequiresSTA]
+        public void Transform_givenTextStyles_generatesExpectedResult()
+        {
+            var text = LoadText("Text_style.md");
+            var markdown = new GitHub.UI.Markdown();
+            var result = markdown.Transform(text);
+            Approvals.Verify(AsXaml(result));
+        }
+
+        [Test]
+        [RequiresSTA]
+        public void Transform_givenImages_generatesExpectedResult()
+        {
+            var text = LoadText("Images.md");
+            var markdown = new GitHub.UI.Markdown();
+            var result = markdown.Transform(text);
+            Approvals.Verify(AsXaml(result));
+        }
+
+        private string LoadText(string name)
+        {
+            using (Stream stream = Assembly.GetExecutingAssembly()
+                               .GetManifestResourceStream("Markdown.Xaml.Tests." + name))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private string AsXaml(object instance)
+        {
+            using (var writer = new StringWriter())
+            {
+                var settings = new XmlWriterSettings { Indent = true };
+                using (var xmlWriter = XmlWriter.Create(writer, settings))
+                {
+                    XamlWriter.Save(instance, xmlWriter);
+                }
+
+                writer.WriteLine();
+                return writer.ToString();
+            }
+        }
+    }
+}
---
+++ test/Markdown.Xaml.Tests/Test1.md
@@ -0,0 +1,37 @@
+﻿Header 1
+========
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis orci erat, 
+hendrerit et interdum at, feugiat non diam. Vestibulum id odio non elit 
+consectetur faucibus quis a urna. 
+
+Header 2
+-----------
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis orci erat, 
+hendrerit et interdum at, feugiat non diam. Vestibulum id odio non elit 
+consectetur faucibus quis a urna. 
+
+# Header Level 1
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis orci erat, 
+hendrerit et interdum at, feugiat non diam. Vestibulum id odio non elit 
+consectetur faucibus quis a urna. 
+
+## Header Level 2
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis orci erat, 
+hendrerit et interdum at, feugiat non diam. Vestibulum id odio non elit 
+consectetur faucibus quis a urna. 
+
+### Header Level 3
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis orci erat, 
+hendrerit et interdum at, feugiat non diam. Vestibulum id odio non elit 
+consectetur faucibus quis a urna. 
+
+#### Header Level 4
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis orci erat, 
+hendrerit et interdum at, feugiat non diam. Vestibulum id odio non elit 
+consectetur faucibus quis a urna. 
