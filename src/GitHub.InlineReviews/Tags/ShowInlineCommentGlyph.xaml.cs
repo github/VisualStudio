72ab864cdc2445d1d478ca342d53b97b83bfa0fc
Jamie Cansdale <jcansdale@gmail.com>
2017-06-27 14:59:06 +0100

    Spike to show first comment as tooltip
    I'm pushing this now so we can see the functionality. I'll implement it properly in future commits.

--- src/GitHub.InlineReviews/Tags/ShowInlineCommentGlyph.xaml.cs
+++ src/GitHub.InlineReviews/Tags/ShowInlineCommentGlyph.xaml.cs
@@ -1,14 +1,38 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Controls;
+using GitHub.InlineReviews.Views;
+using GitHub.InlineReviews.ViewModels;
 
 namespace GitHub.InlineReviews.Tags
 {
     public partial class ShowInlineCommentGlyph : UserControl
     {
+        readonly CommentView tooltipCommentView;
+
         public ShowInlineCommentGlyph()
         {
             InitializeComponent();
+
+            tooltipCommentView = new CommentView();
+            ToolTip = tooltipCommentView;
+            ToolTipOpening += ShowInlineCommentGlyph_ToolTipOpening;
         }
 
+        private void ShowInlineCommentGlyph_ToolTipOpening(object sender, ToolTipEventArgs e)
+        {
+            var tag = Tag as ShowInlineCommentTag;
+            var comment = tag?.Thread.Comments.FirstOrDefault();
+            if (comment == null)
+            {
+                return;
+            }
+
+            var viewModel = new SampleData.CommentViewModelDesigner();
+            viewModel.EditState = CommentEditState.None;
+            viewModel.Body = comment.Body;
+            viewModel.User = comment.User;
+            tooltipCommentView.DataContext = viewModel;
+        }
     }
 }
