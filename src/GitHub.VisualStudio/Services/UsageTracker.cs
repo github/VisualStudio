b419c1cfee75cde6c79d1923e3c3decf20f40ee2
Steven Kirk <grokys@gmail.com>
2017-08-10 15:51:42 +0200

    Added UsageTracker unit tests.
    - Separated IO and date/time comparisons into a separate `UsageService` to allow for easier unit testing
    - Added some unit tests.

--- src/GitHub.VisualStudio/Services/UsageTracker.cs
+++ src/GitHub.VisualStudio/Services/UsageTracker.cs
@@ -1,71 +1,39 @@
 ﻿using System;
 using System.ComponentModel.Composition;
-using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Windows.Threading;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using GitHub.Extensions;
+using GitHub.Helpers;
 using GitHub.Models;
 using GitHub.Settings;
 using Task = System.Threading.Tasks.Task;
-using GitHub.Extensions;
-using System.Threading.Tasks;
-using GitHub.Helpers;
-using System.Threading;
 
 namespace GitHub.Services
 {
     public sealed class UsageTracker : IUsageTracker, IDisposable
     {
-        const string StoreFileName = "ghfvs.usage";
-        static readonly Calendar cal = CultureInfo.InvariantCulture.Calendar;
-
         readonly IGitHubServiceProvider gitHubServiceProvider;
+        bool initialized;
         IMetricsService client;
+        IUsageService service;
         IConnectionManager connectionManager;
         IPackageSettings userSettings;
         IVSServices vsservices;
-        Timer timer;
-        string storePath;
-        bool firstRun = true;
-
-        Func<string, bool> fileExists;
-        Func<string, Encoding, string> readAllText;
-        Action<string, string, Encoding> writeAllText;
-        Action<string> dirCreate;
+        IDisposable timer;
+        bool firstTick = true;
 
         [ImportingConstructor]
-        public UsageTracker(IGitHubServiceProvider gitHubServiceProvider)
+        public UsageTracker(
+            IGitHubServiceProvider gitHubServiceProvider,
+            IUsageService service)
         {
             this.gitHubServiceProvider = gitHubServiceProvider;
-
-            fileExists = (path) => System.IO.File.Exists(path);
-            readAllText = (path, encoding) =>
-            {
-                try
-                {
-                    return System.IO.File.ReadAllText(path, encoding);
-                }
-                catch
-                {
-                    return null;
-                }
-            };
-            writeAllText = (path, content, encoding) =>
-            {
-                try
-                {
-                    System.IO.File.WriteAllText(path, content, encoding);
-                }
-                catch {}
-            };
-            dirCreate = (path) => System.IO.Directory.CreateDirectory(path);
-
-            this.timer = new Timer(
-                TimerTick,
-                null,
-                TimeSpan.FromMinutes(3),
-                TimeSpan.FromHours(8));
+            this.service = service;
+            timer = StartTimer();            
         }
 
         public void Dispose()
@@ -79,63 +47,63 @@ namespace GitHub.Services
             ++usage.Model.NumberOfStartups;
             ++usage.Model.NumberOfStartupsWeek;
             ++usage.Model.NumberOfStartupsMonth;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementCloneCount()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfClones;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementCreateCount()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfReposCreated;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementPublishCount()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfReposPublished;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementOpenInGitHubCount()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfOpenInGitHub;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementLinkToGitHubCount()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfLinkToGitHub;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementCreateGistCount()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfGists;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementUpstreamPullRequestCount()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfUpstreamPullRequests;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementLoginCount()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfLogins;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementPullRequestCheckOutCount(bool fork)
@@ -147,7 +115,7 @@ namespace GitHub.Services
             else
                 ++usage.Model.NumberOfLocalPullRequestsCheckedOut;
 
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementPullRequestPushCount(bool fork)
@@ -159,7 +127,7 @@ namespace GitHub.Services
             else
                 ++usage.Model.NumberOfLocalPullRequestPushes;
 
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementPullRequestPullCount(bool fork)
@@ -171,84 +139,89 @@ namespace GitHub.Services
             else
                 ++usage.Model.NumberOfLocalPullRequestPulls;
 
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementWelcomeDocsClicks()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfWelcomeDocsClicks;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementWelcomeTrainingClicks()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfWelcomeTrainingClicks;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementGitHubPaneHelpClicks()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfGitHubPaneHelpClicks;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementPullRequestOpened()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfPullRequestsOpened;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementPRDetailsViewChanges()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfPRDetailsViewChanges;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementPRDetailsViewFile()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfPRDetailsViewFile;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementPRDetailsCompareWithSolution()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfPRDetailsCompareWithSolution;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementPRDetailsOpenFileInSolution()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfPRDetailsOpenFileInSolution;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementPRReviewDiffViewInlineCommentOpen()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfPRReviewDiffViewInlineCommentOpen;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
         }
 
         public async Task IncrementPRReviewDiffViewInlineCommentPost()
         {
             var usage = await LoadUsage();
             ++usage.Model.NumberOfPRReviewDiffViewInlineCommentPost;
-            SaveUsage(usage);
+            await service.WriteLocalData(usage);
+        }
+
+        IDisposable StartTimer()
+        {
+            return service.StartTimer(TimerTick, TimeSpan.FromMinutes(3), TimeSpan.FromHours(8));
         }
 
         async Task Initialize()
         {
             // The services needed by the usage tracker are loaded when they are first needed to
             // improve the startup time of the extension.
-            if (userSettings == null)
+            if (!initialized)
             {
                 await ThreadingHelper.SwitchToMainThreadAsync();
 
@@ -256,64 +229,29 @@ namespace GitHub.Services
                 connectionManager = gitHubServiceProvider.GetService<IConnectionManager>();
                 userSettings = gitHubServiceProvider.GetService<IPackageSettings>();
                 vsservices = gitHubServiceProvider.GetService<IVSServices>();
-
-                var program = gitHubServiceProvider.GetService<IProgram>();
-                storePath = System.IO.Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                    program.ApplicationName,
-                    StoreFileName);
+                initialized = true;
             }
         }
 
-        async Task<UsageStore> LoadUsage()
+        async Task<UsageData> LoadUsage()
         {
             await Initialize();
 
-            var json = fileExists(storePath) ? readAllText(storePath, Encoding.UTF8) : null;
-            UsageStore result = null;
-            try
-            {
-                result = json != null ?
-                    SimpleJson.DeserializeObject<UsageStore>(json) :
-                    new UsageStore { Model = new UsageModel() };
-            }
-            catch
-            {
-                result = new UsageStore { Model = new UsageModel() };
-            }
-
+            var result = await service.ReadLocalData();
             result.Model.Lang = CultureInfo.InstalledUICulture.IetfLanguageTag;
             result.Model.AppVersion = AssemblyVersionInformation.Version;
             result.Model.VSVersion = vsservices.VSVersion;
-
             return result;
-        }
-
-        void SaveUsage(UsageStore store)
-        {
-            dirCreate(System.IO.Path.GetDirectoryName(storePath));
-            var json = SimpleJson.SerializeObject(store);
-            writeAllText(storePath, json, Encoding.UTF8);
-        }
-
-        void TimerTick(object state)
-        {
-            TimerTick()
-                .Catch(ex =>
-                {
-                    //log.Warn("Failed submitting usage data", ex);
-                })
-                .Forget();
         }
 
         async Task TimerTick()
         {
             await Initialize();
 
-            if (firstRun)
+            if (firstTick)
             {
                 await IncrementLaunchCount();
-                firstRun = false;
+                firstTick = false;
             }
 
             if (client == null || !userSettings.CollectMetrics)
@@ -330,16 +268,16 @@ namespace GitHub.Services
             var usage = await LoadUsage();
             var lastDate = usage.LastUpdated;
             var currentDate = DateTimeOffset.Now;
-            var includeWeekly = GetIso8601WeekOfYear(lastDate) != GetIso8601WeekOfYear(currentDate);
-            var includeMonthly = lastDate.Month != currentDate.Month;
+            var includeWeekly = !service.IsSameWeek(usage.LastUpdated);
+            var includeMonthly = !service.IsSameMonth(usage.LastUpdated);
 
             // Only send stats once a day.
-            if (lastDate.Date != currentDate.Date)
+            if (!service.IsSameDay(usage.LastUpdated))
             {
                 await SendUsage(usage.Model, includeWeekly, includeMonthly);
                 ClearCounters(usage.Model, includeWeekly, includeMonthly);
                 usage.LastUpdated = DateTimeOffset.Now.UtcDateTime;
-                SaveUsage(usage);
+                await service.WriteLocalData(usage);
             }
         }
 
@@ -364,61 +302,24 @@ namespace GitHub.Services
             await client.PostUsage(model);
         }
 
-        // http://blogs.msdn.com/b/shawnste/archive/2006/01/24/iso-8601-week-of-year-format-in-microsoft-net.aspx
-        static int GetIso8601WeekOfYear(DateTimeOffset time)
-        {
-            // Seriously cheat.  If its Monday, Tuesday or Wednesday, then it'll
-            // be the same week# as whatever Thursday, Friday or Saturday are,
-            // and we always get those right
-            DayOfWeek day = cal.GetDayOfWeek(time.UtcDateTime);
-            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
-            {
-                time = time.AddDays(3);
-            }
-
-            // Return the week of our adjusted day
-            return cal.GetWeekOfYear(time.UtcDateTime, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-        }
-
         static void ClearCounters(UsageModel usage, bool weekly, bool monthly)
         {
-            usage.NumberOfStartups = 0;
-            usage.NumberOfClones = 0;
-            usage.NumberOfReposCreated = 0;
-            usage.NumberOfReposPublished = 0;
-            usage.NumberOfGists = 0;
-            usage.NumberOfOpenInGitHub = 0;
-            usage.NumberOfLinkToGitHub = 0;
-            usage.NumberOfLogins = 0;
-            usage.NumberOfUpstreamPullRequests = 0;
-            usage.NumberOfPullRequestsOpened = 0;
-            usage.NumberOfLocalPullRequestsCheckedOut = 0;
-            usage.NumberOfLocalPullRequestPulls = 0;
-            usage.NumberOfLocalPullRequestPushes = 0;
-            usage.NumberOfForkPullRequestsCheckedOut = 0;
-            usage.NumberOfForkPullRequestPulls = 0;
-            usage.NumberOfForkPullRequestPushes = 0;
-            usage.NumberOfGitHubPaneHelpClicks = 0;
-            usage.NumberOfWelcomeTrainingClicks = 0;
-            usage.NumberOfWelcomeDocsClicks = 0;
-            usage.NumberOfPRDetailsViewChanges = 0;
-            usage.NumberOfPRDetailsViewFile = 0;
-            usage.NumberOfPRDetailsCompareWithSolution = 0;
-            usage.NumberOfPRDetailsOpenFileInSolution = 0;
-            usage.NumberOfPRReviewDiffViewInlineCommentOpen = 0;
-            usage.NumberOfPRReviewDiffViewInlineCommentPost = 0;
-    
-            if (weekly)
-                usage.NumberOfStartupsWeek = 0;
+            var properties = usage.GetType().GetRuntimeProperties();
 
-            if (monthly)
-                usage.NumberOfStartupsMonth = 0;
-        }
+            foreach (var property in properties)
+            {
+                var setValue = property.PropertyType == typeof(int);
 
-        class UsageStore
-        {
-            public DateTimeOffset LastUpdated { get; set; }
-            public UsageModel Model { get; set; }
+                if (property.Name == nameof(usage.NumberOfStartupsWeek))
+                    setValue = weekly;
+                else if (property.Name == nameof(usage.NumberOfStartupsMonth))
+                    setValue = monthly;
+
+                if (setValue)
+                {
+                    property.SetValue(usage, 0);
+                }
+            }
         }
     }
 }
