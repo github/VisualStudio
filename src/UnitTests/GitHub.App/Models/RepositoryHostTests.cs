bcfbd798781b31df16f110dd2c90a68dec8d07ea
Steven Kirk <grokys@gmail.com>
2017-09-07 15:12:07 +0200

    Fix broken merge.

--- src/UnitTests/GitHub.App/Models/RepositoryHostTests.cs
+++ src/UnitTests/GitHub.App/Models/RepositoryHostTests.cs
@@ -26,11 +26,11 @@ public class RepositoryHostTests
             apiClient.GetOrCreateApplicationAuthenticationCode(
                 Args.TwoFactorChallengCallback, Args.String, Args.Boolean)
                 .Returns(Observable.Return(new ApplicationAuthorization("S3CR3TS")));
-            apiClient.GetUser().Returns(Observable.Return(CreateUserAndScopes("baymax")));
+            apiClient.GetUser().Returns(Observable.Return(CreateOctokitUser("baymax")));
             var hostCache = new InMemoryBlobCache();
             var modelService = new ModelService(apiClient, hostCache, Substitute.For<IAvatarProvider>());
             var loginManager = Substitute.For<ILoginManager>();
-            loginManager.Login(HostAddress.GitHubDotComHostAddress, Arg.Any<IGitHubClient>(), "baymax", "aPassword").Returns(CreateUserAndScopes("baymax").User);
+            loginManager.Login(HostAddress.GitHubDotComHostAddress, Arg.Any<IGitHubClient>(), "baymax", "aPassword").Returns(CreateOctokitUser("baymax"));
             var keychain = Substitute.For<IKeychain>();
             var usage = Substitute.For<IUsageTracker>();
             var host = new RepositoryHost(apiClient, modelService, loginManager, keychain, usage);
@@ -51,11 +51,11 @@ public class RepositoryHostTests
             apiClient.GetOrCreateApplicationAuthenticationCode(
                 Args.TwoFactorChallengCallback, Args.String, Args.Boolean)
                 .Returns(Observable.Return(new ApplicationAuthorization("S3CR3TS")));
-            apiClient.GetUser().Returns(Observable.Return(CreateUserAndScopes("baymax")));
+            apiClient.GetUser().Returns(Observable.Return(CreateOctokitUser("baymax")));
             var hostCache = new InMemoryBlobCache();
             var modelService = Substitute.For<IModelService>();
             var loginManager = Substitute.For<ILoginManager>();
-            loginManager.Login(HostAddress.GitHubDotComHostAddress, Arg.Any<IGitHubClient>(), "baymax", "aPassword").Returns(CreateUserAndScopes("baymax").User);
+            loginManager.Login(HostAddress.GitHubDotComHostAddress, Arg.Any<IGitHubClient>(), "baymax", "aPassword").Returns(CreateOctokitUser("baymax"));
             var keychain = Substitute.For<IKeychain>();
             var usage = Substitute.For<IUsageTracker>();
             var host = new RepositoryHost(apiClient, modelService, loginManager, keychain, usage);
@@ -96,7 +96,7 @@ public class RepositoryHostTests
             var hostCache = new InMemoryBlobCache();
             var modelService = new ModelService(apiClient, hostCache, Substitute.For<IAvatarProvider>());
             var loginManager = Substitute.For<ILoginManager>();
-            loginManager.LoginFromCache(HostAddress.GitHubDotComHostAddress, Arg.Any<IGitHubClient>()).Returns(CreateUserAndScopes("baymax").User);
+            loginManager.LoginFromCache(HostAddress.GitHubDotComHostAddress, Arg.Any<IGitHubClient>()).Returns(CreateOctokitUser("baymax"));
             var keychain = Substitute.For<IKeychain>();
             var usage = Substitute.For<IUsageTracker>();
             var host = new RepositoryHost(apiClient, modelService, loginManager, keychain, usage);
@@ -117,7 +117,7 @@ public class RepositoryHostTests
             var hostCache = new InMemoryBlobCache();
             var modelService = Substitute.For<IModelService>();
             var loginManager = Substitute.For<ILoginManager>();
-            loginManager.LoginFromCache(HostAddress.GitHubDotComHostAddress, Arg.Any<IGitHubClient>()).Returns(CreateUserAndScopes("baymax").User);
+            loginManager.LoginFromCache(HostAddress.GitHubDotComHostAddress, Arg.Any<IGitHubClient>()).Returns(CreateOctokitUser("baymax"));
             var keychain = Substitute.For<IKeychain>();
             var usage = Substitute.For<IUsageTracker>();
             var host = new RepositoryHost(apiClient, modelService, loginManager, keychain, usage);
