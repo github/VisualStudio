43dc30595a98f68a717f87f5b717f91d459d9ea6
Jamie Cansdale <jcansdale@gmail.com>
2017-09-21 14:03:34 +0100

    Resolve assemblies using ProvideBindingPath attribute

--- src/GitHub.VisualStudio/AssemblyResolverPackage.cs
+++ src/GitHub.VisualStudio/AssemblyResolverPackage.cs
@@ -1,124 +1,12 @@
 ﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Globalization;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
-using NLog;
 
 namespace GitHub.VisualStudio
 {
-    // This fires before ShellInitialized and SolutionExists.
-    [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string)]
+    [ProvideBindingPath]
     [Guid(Guids.guidAssemblyResolverPkgString)]
     public class AssemblyResolverPackage : Package
     {
-        // list of assemblies that should be considered when resolving
-        IEnumerable<ProvideDependentAssemblyAttribute> dependentAssemblies;
-        string packageFolder;
-
-        IDictionary<string, Assembly> resolvingAssemblies;
-        IDictionary<string, Exception> resolvingExceptions;
-
-        public AssemblyResolverPackage()
-        {
-            var asm = Assembly.GetExecutingAssembly();
-            packageFolder = Path.GetDirectoryName(asm.Location);
-            dependentAssemblies = asm.GetCustomAttributes<ProvideDependentAssemblyAttribute>();
-            resolvingAssemblies = new Dictionary<string, Assembly>();
-            resolvingExceptions = new Dictionary<string, Exception>();
-            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssemblyFromPackageFolder;
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            AppDomain.CurrentDomain.AssemblyResolve -= ResolveAssemblyFromPackageFolder;
-
-            if (resolvingAssemblies.Count > 0 || resolvingExceptions.Count > 0)
-            {
-                // Avoid executing any logging code unless there is something to log.
-                WriteToLog();
-            }
-
-            base.Dispose(disposing);
-        }
-
-        [SuppressMessage("Microsoft.Reliability", "CA2001:AvoidCallingProblematicMethods")]
-        public static Assembly ResolveDependentAssembly(
-            ProvideDependentAssemblyAttribute dependentAssembly, string packageFolder, AssemblyName resolveAssemblyName)
-        {
-            if (dependentAssembly.AssemblyName == resolveAssemblyName.Name)
-            {
-                var file = dependentAssembly.CodeBase.Replace("$PackageFolder$", packageFolder);
-                if (File.Exists(file))
-                {
-                    var targetAssemblyName = AssemblyName.GetAssemblyName(file);
-
-                    var codeBase = dependentAssembly as ProvideCodeBaseAttribute;
-                    if (codeBase != null)
-                    {
-                        if (resolveAssemblyName.FullName == targetAssemblyName.FullName)
-                        {
-                            return Assembly.LoadFrom(file);
-                        }
-                    }
-
-                    var bindingRedirection = dependentAssembly as ProvideBindingRedirectionAttribute;
-                    if (bindingRedirection != null)
-                    {
-                        if (resolveAssemblyName.Version >= new Version(bindingRedirection.OldVersionLowerBound) &&
-                            resolveAssemblyName.Version <= new Version(bindingRedirection.OldVersionUpperBound))
-                        {
-                            resolveAssemblyName.Version = targetAssemblyName.Version;
-                            if (resolveAssemblyName.FullName == targetAssemblyName.FullName)
-                            {
-                                return Assembly.LoadFrom(file);
-                            }
-                        }
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        Assembly ResolveAssemblyFromPackageFolder(object sender, ResolveEventArgs e)
-        {
-            try
-            {
-                var resolveAssemblyName = new AssemblyName(e.Name);
-                foreach (var dependentAssembly in dependentAssemblies)
-                {
-                    var resolvedAssembly = ResolveDependentAssembly(dependentAssembly, packageFolder, resolveAssemblyName);
-                    if (resolvedAssembly != null)
-                    {
-                        return resolvingAssemblies[e.Name] = resolvedAssembly;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                resolvingExceptions[e.Name] = ex;
-            }
-
-            return null;
-        }
-
-        void WriteToLog()
-        {
-            var log = LogManager.GetCurrentClassLogger();
-            foreach (var resolvedAssembly in resolvingAssemblies)
-            {
-                log.Info(CultureInfo.InvariantCulture, "Resolved '{0}' to '{1}'.", resolvedAssembly.Key, resolvedAssembly.Value.Location);
-            }
-
-            foreach (var resolvingException in resolvingExceptions)
-            {
-                log.Error(CultureInfo.InvariantCulture, "Error occurred loading '{0}' from '{1}'.\n{2}", resolvingException.Key, packageFolder, resolvingException.Value);
-            }
-        }
     }
 }
