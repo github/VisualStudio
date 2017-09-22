30fa76cac0b1d382bb76d487e00726507d919ce1
Jamie Cansdale <jcansdale@github.com>
2017-09-22 11:43:13 +0100

    Add comment about ProvideBindingPath

--- src/GitHub.VisualStudio/AssemblyResolverPackage.cs
+++ src/GitHub.VisualStudio/AssemblyResolverPackage.cs
@@ -4,7 +4,11 @@ using Microsoft.VisualStudio.Shell;
 
 namespace GitHub.VisualStudio
 {
+    // Allow assemblies in the extension directory to be resolved by their full or partial name.
+    // This is required for GitHub.VisualStudio.imagemanifest, XAML and when using unsigned assemblies.
+    // See: https://github.com/github/VisualStudio/pull/1236/
     [ProvideBindingPath]
+    
     [Guid(Guids.guidAssemblyResolverPkgString)]
     public class AssemblyResolverPackage : Package
     {
