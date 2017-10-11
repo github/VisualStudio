5ab9b66ecff6b885fe2901404671ebcadec17a46
ci <ci@github.com>
2017-10-11 11:07:31 +0000

    Bump version to 2.3.4.43

--- src/common/SolutionInfo.cs
+++ src/common/SolutionInfo.cs
@@ -3,8 +3,8 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("GitHub Extension for Visual Studio")]
-[assembly: AssemblyVersion("2.3.4.42")]
-[assembly: AssemblyFileVersion("2.3.4.42")]
+[assembly: AssemblyVersion("2.3.4.43")]
+[assembly: AssemblyFileVersion("2.3.4.43")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyCompany("GitHub, Inc.")]
 [assembly: AssemblyCopyright("Copyright © GitHub, Inc. 2014-2016")]
@@ -16,6 +16,6 @@ using System.Runtime.InteropServices;
 namespace System
 {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.3.4.42";
+        internal const string Version = "2.3.4.43";
     }
 }
