4880b8803fa95a53372f18b8a4d1ab394ca131ca
Steven Kirk <grokys@gmail.com>
2017-12-07 17:24:19 +0100

    Don't use C#7 features.

--- src/GitHub.App/Infrastructure/ExportWrappers.cs
+++ src/GitHub.App/Infrastructure/ExportWrappers.cs
@@ -27,17 +27,17 @@ namespace GitHub.Infrastructure
     public class ExportedHttpListener : IHttpListener
     {
         readonly IHttpListener inner = new HttpListenerWrapper(new HttpListener());
-        public AuthenticationSchemes AuthenticationSchemes { get => inner.AuthenticationSchemes; set => inner.AuthenticationSchemes = value; }
-        public AuthenticationSchemeSelector AuthenticationSchemeSelectorDelegate { get => inner.AuthenticationSchemeSelectorDelegate; set => inner.AuthenticationSchemeSelectorDelegate = value; }
+        public AuthenticationSchemes AuthenticationSchemes { get { return inner.AuthenticationSchemes; } set { inner.AuthenticationSchemes = value; } }
+        public AuthenticationSchemeSelector AuthenticationSchemeSelectorDelegate { get { return inner.AuthenticationSchemeSelectorDelegate; } set { inner.AuthenticationSchemeSelectorDelegate = value; } }
         public ServiceNameCollection DefaultServiceNames => inner.DefaultServiceNames;
-        public ExtendedProtectionPolicy ExtendedProtectionPolicy { get => inner.ExtendedProtectionPolicy; set => inner.ExtendedProtectionPolicy = value; }
-        public HttpListener.ExtendedProtectionSelector ExtendedProtectionSelectorDelegate { get => inner.ExtendedProtectionSelectorDelegate; set => inner.ExtendedProtectionSelectorDelegate = value; }
-        public bool IgnoreWriteExceptions { get => inner.IgnoreWriteExceptions; set => inner.IgnoreWriteExceptions = value; }
+        public ExtendedProtectionPolicy ExtendedProtectionPolicy { get { return inner.ExtendedProtectionPolicy; } set { inner.ExtendedProtectionPolicy = value; } }
+        public HttpListener.ExtendedProtectionSelector ExtendedProtectionSelectorDelegate { get { return inner.ExtendedProtectionSelectorDelegate; } set { inner.ExtendedProtectionSelectorDelegate = value; } }
+        public bool IgnoreWriteExceptions { get { return inner.IgnoreWriteExceptions; } set { inner.IgnoreWriteExceptions = value; } }
         public bool IsListening => inner.IsListening;
         public ICollection<string> Prefixes => inner.Prefixes;
-        public string Realm { get => inner.Realm; set => inner.Realm = value; }
+        public string Realm { get { return inner.Realm; } set { inner.Realm = value; } }
         public HttpListenerTimeoutManager TimeoutManager => inner.TimeoutManager;
-        public bool UnsafeConnectionNtlmAuthentication { get => inner.UnsafeConnectionNtlmAuthentication; set => inner.UnsafeConnectionNtlmAuthentication = value; }
+        public bool UnsafeConnectionNtlmAuthentication { get { return inner.UnsafeConnectionNtlmAuthentication; } set { inner.UnsafeConnectionNtlmAuthentication = value; } }
         public void Abort() => inner.Abort();
         public IAsyncResult BeginGetContext(AsyncCallback callback, object state) => inner.BeginGetContext(callback, state);
         public void Close() => inner.Close();
