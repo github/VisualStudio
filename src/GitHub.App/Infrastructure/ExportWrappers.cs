bcef0654bdf2ec9578fe52a9e739eadee0eb4d73
Steven Kirk <grokys@gmail.com>
2017-12-07 16:03:18 +0100

    Remove Rothko submodule.
    Usean updated Rothko nuget package. Also had to add an export wrapper as I prefer to define the export in `github/VisualStudio` rather than in our Rothko fork.

--- src/GitHub.App/Infrastructure/ExportWrappers.cs
+++ src/GitHub.App/Infrastructure/ExportWrappers.cs
@@ -1,7 +1,13 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Net;
+using System.Security.Authentication.ExtendedProtection;
+using System.Threading.Tasks;
 using GitHub.Models;
 using Octokit;
 using Octokit.Internal;
+using Rothko;
 
 namespace GitHub.Infrastructure
 {
@@ -15,6 +21,31 @@ namespace GitHub.Infrastructure
         public ExportedHttpClient() :
             base(HttpMessageHandlerFactory.CreateDefault)
         {}
+    }
+
+    [Export(typeof(IHttpListener))]
+    public class ExportedHttpListener : IHttpListener
+    {
+        readonly IHttpListener inner = new HttpListenerWrapper(new HttpListener());
+        public AuthenticationSchemes AuthenticationSchemes { get => inner.AuthenticationSchemes; set => inner.AuthenticationSchemes = value; }
+        public AuthenticationSchemeSelector AuthenticationSchemeSelectorDelegate { get => inner.AuthenticationSchemeSelectorDelegate; set => inner.AuthenticationSchemeSelectorDelegate = value; }
+        public ServiceNameCollection DefaultServiceNames => inner.DefaultServiceNames;
+        public ExtendedProtectionPolicy ExtendedProtectionPolicy { get => inner.ExtendedProtectionPolicy; set => inner.ExtendedProtectionPolicy = value; }
+        public HttpListener.ExtendedProtectionSelector ExtendedProtectionSelectorDelegate { get => inner.ExtendedProtectionSelectorDelegate; set => inner.ExtendedProtectionSelectorDelegate = value; }
+        public bool IgnoreWriteExceptions { get => inner.IgnoreWriteExceptions; set => inner.IgnoreWriteExceptions = value; }
+        public bool IsListening => inner.IsListening;
+        public ICollection<string> Prefixes => inner.Prefixes;
+        public string Realm { get => inner.Realm; set => inner.Realm = value; }
+        public HttpListenerTimeoutManager TimeoutManager => inner.TimeoutManager;
+        public bool UnsafeConnectionNtlmAuthentication { get => inner.UnsafeConnectionNtlmAuthentication; set => inner.UnsafeConnectionNtlmAuthentication = value; }
+        public void Abort() => inner.Abort();
+        public IAsyncResult BeginGetContext(AsyncCallback callback, object state) => inner.BeginGetContext(callback, state);
+        public void Close() => inner.Close();
+        public IHttpListenerContext EndGetContext(IAsyncResult asyncResult) => inner.EndGetContext(asyncResult);
+        public IHttpListenerContext GetContext() => inner.GetContext();
+        public Task<IHttpListenerContext> GetContextAsync() => inner.GetContextAsync();
+        public void Start() => inner.Start();
+        public void Stop() => inner.Stop();
     }
 
     [Export(typeof(IEnterpriseProbe))]
