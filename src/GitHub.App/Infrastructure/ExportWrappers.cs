6f4f34a482598a2c68c43dd968002ff835496c1a
Steven Kirk <grokys@gmail.com>
2017-12-11 10:07:56 +0100

    Update rotko.
    `HttpListenerWrapper` is now non-sealed so we don't need to wrap it to export it.

--- src/GitHub.App/Infrastructure/ExportWrappers.cs
+++ src/GitHub.App/Infrastructure/ExportWrappers.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Net;
-using System.Security.Authentication.ExtendedProtection;
-using System.Threading.Tasks;
 using GitHub.Models;
 using Octokit;
 using Octokit.Internal;
@@ -24,28 +21,12 @@ namespace GitHub.Infrastructure
     }
 
     [Export(typeof(IHttpListener))]
-    public class ExportedHttpListener : IHttpListener
+    public class ExportedHttpListener : HttpListenerWrapper
     {
-        readonly IHttpListener inner = new HttpListenerWrapper(new HttpListener());
-        public AuthenticationSchemes AuthenticationSchemes { get { return inner.AuthenticationSchemes; } set { inner.AuthenticationSchemes = value; } }
-        public AuthenticationSchemeSelector AuthenticationSchemeSelectorDelegate { get { return inner.AuthenticationSchemeSelectorDelegate; } set { inner.AuthenticationSchemeSelectorDelegate = value; } }
-        public ServiceNameCollection DefaultServiceNames => inner.DefaultServiceNames;
-        public ExtendedProtectionPolicy ExtendedProtectionPolicy { get { return inner.ExtendedProtectionPolicy; } set { inner.ExtendedProtectionPolicy = value; } }
-        public HttpListener.ExtendedProtectionSelector ExtendedProtectionSelectorDelegate { get { return inner.ExtendedProtectionSelectorDelegate; } set { inner.ExtendedProtectionSelectorDelegate = value; } }
-        public bool IgnoreWriteExceptions { get { return inner.IgnoreWriteExceptions; } set { inner.IgnoreWriteExceptions = value; } }
-        public bool IsListening => inner.IsListening;
-        public ICollection<string> Prefixes => inner.Prefixes;
-        public string Realm { get { return inner.Realm; } set { inner.Realm = value; } }
-        public HttpListenerTimeoutManager TimeoutManager => inner.TimeoutManager;
-        public bool UnsafeConnectionNtlmAuthentication { get { return inner.UnsafeConnectionNtlmAuthentication; } set { inner.UnsafeConnectionNtlmAuthentication = value; } }
-        public void Abort() => inner.Abort();
-        public IAsyncResult BeginGetContext(AsyncCallback callback, object state) => inner.BeginGetContext(callback, state);
-        public void Close() => inner.Close();
-        public IHttpListenerContext EndGetContext(IAsyncResult asyncResult) => inner.EndGetContext(asyncResult);
-        public IHttpListenerContext GetContext() => inner.GetContext();
-        public Task<IHttpListenerContext> GetContextAsync() => inner.GetContextAsync();
-        public void Start() => inner.Start();
-        public void Stop() => inner.Stop();
+        public ExportedHttpListener()
+            : base(new HttpListener())
+        {
+        }
     }
 
     [Export(typeof(IEnterpriseProbe))]
