24e3697f6537646da077e7ddff574bf3bf519258
Steven Kirk <grokys@gmail.com>
2017-12-14 10:35:59 +0100

    Fix typo.

--- src/GitHub.Api/ApiClientConfiguration.cs
+++ src/GitHub.Api/ApiClientConfiguration.cs
@@ -63,13 +63,13 @@ namespace GitHub.Api
             try
             {
                 // adapted from http://stackoverflow.com/a/1561067
-                var fastedValidNetworkInterface = NetworkInterface.GetAllNetworkInterfaces()
+                var fastestValidNetworkInterface = NetworkInterface.GetAllNetworkInterfaces()
                     .OrderByDescending(nic => nic.Speed)
                     .Where(nic => nic.OperationalStatus == OperationalStatus.Up)
                     .Select(nic => nic.GetPhysicalAddress().ToString())
                     .FirstOrDefault(address => address.Length >= 12);
 
-                return fastedValidNetworkInterface ?? GetMachineNameSafe();
+                return fastestValidNetworkInterface ?? GetMachineNameSafe();
             }
             catch (Exception)
             {
