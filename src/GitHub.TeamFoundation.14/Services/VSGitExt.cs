c17c75614a220499ad984769dbcdbb4d82184320
Jamie Cansdale <jcansdale@github.com>
2018-02-06 12:13:33 +0000

    Move VSUIContext in with VSGitExt
    This class is an internal helper class for VSGitExt and doesn't need to be on its own.

--- src/GitHub.TeamFoundation.14/Services/VSGitExt.cs
+++ src/GitHub.TeamFoundation.14/Services/VSGitExt.cs
@@ -6,9 +6,9 @@ using GitHub.Models;
 using GitHub.Services;
 using GitHub.Logging;
 using Serilog;
+using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.TeamFoundation.Git.Extensibility;
 using Task = System.Threading.Tasks.Task;
-using GitHub.TeamFoundation.Services;
 
 namespace GitHub.VisualStudio.Base
 {
@@ -154,5 +154,41 @@ namespace GitHub.VisualStudio.Base
         public event Action ActiveRepositoriesChanged;
 
         public Task InitializeTask { get; private set; }
+    }
+
+    class VSUIContext : IVSUIContext
+    {
+        readonly UIContext context;
+        readonly Dictionary<EventHandler<VSUIContextChangedEventArgs>, EventHandler<UIContextChangedEventArgs>> handlers =
+            new Dictionary<EventHandler<VSUIContextChangedEventArgs>, EventHandler<UIContextChangedEventArgs>>();
+        public VSUIContext(Guid contextGuid)
+        {
+            context = UIContext.FromUIContextGuid(contextGuid);
+        }
+
+        public bool IsActive { get { return context.IsActive; } }
+
+        public event EventHandler<VSUIContextChangedEventArgs> UIContextChanged
+        {
+            add
+            {
+                EventHandler<UIContextChangedEventArgs> handler = null;
+                if (!handlers.TryGetValue(value, out handler))
+                {
+                    handler = (s, e) => value.Invoke(s, new VSUIContextChangedEventArgs(e.Activated));
+                    handlers.Add(value, handler);
+                }
+                context.UIContextChanged += handler;
+            }
+            remove
+            {
+                EventHandler<UIContextChangedEventArgs> handler = null;
+                if (handlers.TryGetValue(value, out handler))
+                {
+                    handlers.Remove(value);
+                    context.UIContextChanged -= handler;
+                }
+            }
+        }
     }
 }
