8c79dc2a8c7c32b281176279f6932579affe035f
Jamie Cansdale <jcansdale@github.com>
2018-02-07 09:43:58 +0000

    Unload solution while checking out PR
    This is an attempt to avoid hangs that can happen when project files are touched.

--- src/GitHub.App/Services/PullRequestService.cs
+++ src/GitHub.App/Services/PullRequestService.cs
@@ -40,14 +40,17 @@ namespace GitHub.Services
         readonly IGitService gitService;
         readonly IOperatingSystem os;
         readonly IUsageTracker usageTracker;
+        readonly EnvDTE.DTE dte;
 
         [ImportingConstructor]
-        public PullRequestService(IGitClient gitClient, IGitService gitService, IOperatingSystem os, IUsageTracker usageTracker)
+        public PullRequestService(IGitClient gitClient, IGitService gitService, IOperatingSystem os, IUsageTracker usageTracker,
+            IGitHubServiceProvider serviceProvider = null)
         {
             this.gitClient = gitClient;
             this.gitService = gitService;
             this.os = os;
             this.usageTracker = usageTracker;
+            dte = serviceProvider?.GetService<EnvDTE.DTE>();
         }
 
         public IObservable<IPullRequestModel> CreatePullRequest(IModelService modelService,
@@ -296,6 +299,9 @@ namespace GitHub.Services
         {
             return Observable.Defer(async () =>
             {
+                var solutionFile = dte.Solution.FullName;
+                dte.Solution.Close(true);
+
                 using (var repo = gitService.GetRepository(repository.LocalPath))
                 {
                     var branchName = GetLocalBranchesInternal(repository, repo, pullRequest).FirstOrDefault();
@@ -329,6 +335,8 @@ namespace GitHub.Services
                         await MarkBranchAsPullRequest(repo, branchName, pullRequest);
                     }
                 }
+
+                dte.Solution.Open(solutionFile);
 
                 return Observable.Return(Unit.Default);
             });
