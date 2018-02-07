33e5edf084e4adc729ee20e2d228868441982347
Andreia Gaita <shana@spoiledcat.net>
2018-02-07 16:30:44 +0100

    Add metrics tests that validate all the data
    - Add test to validate that the UsageService writes all the data
    - Add test to validate that the MetricsService sends all the data
    - Move the usage tracker tests into a decent namespace and one single fixture

--- test/UnitTests/GitHub.VisualStudio/Services/UsageTrackerTests.cs
+++ test/UnitTests/GitHub.VisualStudio/Services/UsageTrackerTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using System.Reactive.Disposables;
 using System.Threading.Tasks;
 using GitHub.Extensions;
@@ -8,209 +7,267 @@ using GitHub.Services;
 using GitHub.Settings;
 using NSubstitute;
 using NUnit.Framework;
+using System.Reflection;
+using System.Linq;
 
-
-namespace UnitTests.GitHub.VisualStudio.Services
+namespace MetricsTests
 {
-    public class UsageTrackerTests
+    public class UsageTrackerTests : TestBaseClass
     {
-        public class TheTimer : TestBaseClass
+        [Test]
+        public void ShouldStartTimer()
         {
-            [Test]
-            public void ShouldStartTimer()
-            {
-                var service = Substitute.For<IUsageService>();
-                var target = new UsageTracker(CreateServiceProvider(), service);
+            var service = Substitute.For<IUsageService>();
+            var target = new UsageTracker(CreateServiceProvider(), service);
 
-                service.Received(1).StartTimer(Arg.Any<Func<Task>>(), TimeSpan.FromMinutes(3), TimeSpan.FromHours(8));
-            }
-
-            [Test]
-            public async Task FirstTickShouldIncrementLaunchCount()
-            {
-                var service = CreateUsageService();
-                var targetAndTick = CreateTargetAndGetTick(CreateServiceProvider(), service);
-
-                await targetAndTick.Item2();
-
-                await service.Received(1).WriteLocalData(
-                    Arg.Is<UsageData>(x => 
-                        x.Model.NumberOfStartups == 1 &&
-                        x.Model.NumberOfStartupsWeek == 1 &&
-                        x.Model.NumberOfStartupsMonth == 1));
-            }
-
-            [Test]
-            public async Task SubsequentTickShouldNotIncrementLaunchCount()
-            {
-                var service = CreateUsageService();
-                var targetAndTick = CreateTargetAndGetTick(CreateServiceProvider(), service);
-
-                await targetAndTick.Item2();
-                service.ClearReceivedCalls();
-                await targetAndTick.Item2();
-
-                await service.DidNotReceiveWithAnyArgs().WriteLocalData(null);
-            }
-
-            [Test]
-            public async Task ShouldDisposeTimerIfMetricsServiceNotFound()
-            {
-                var service = CreateUsageService();
-                var disposed = false;
-                var disposable = Disposable.Create(() => disposed = true);
-                service.StartTimer(null, new TimeSpan(), new TimeSpan()).ReturnsForAnyArgs(disposable);
-
-                var targetAndTick = CreateTargetAndGetTick(
-                    CreateServiceProvider(hasMetricsService: false),
-                    service);
-
-                await targetAndTick.Item2();
-
-                Assert.True(disposed);
-            }
-
-            [Test]
-            public async Task TickShouldNotSendDataIfSameDay()
-            {
-                var serviceProvider = CreateServiceProvider();
-                var targetAndTick = CreateTargetAndGetTick(
-                    serviceProvider,
-                    CreateUsageService());
-
-                await targetAndTick.Item2();
-
-                var metricsService = serviceProvider.TryGetService<IMetricsService>();
-                await metricsService.DidNotReceive().PostUsage(Arg.Any<UsageModel>());
-            }
-
-            [Test]
-            public async Task TickShouldSendDataIfDifferentDay()
-            {
-                var serviceProvider = CreateServiceProvider();
-                var targetAndTick = CreateTargetAndGetTick(
-                    serviceProvider,
-                    CreateUsageService(sameDay: false));
-
-                await targetAndTick.Item2();
-
-                var metricsService = serviceProvider.TryGetService<IMetricsService>();
-                await metricsService.Received(1).PostUsage(Arg.Any<UsageModel>());
-            }
-
-            [Test]
-            public async Task NonWeeklyOrMonthlyCountersShouldBeZeroed()
-            {
-                var service = CreateUsageService(new UsageModel
-                {
-                    NumberOfStartups = 1,
-                    NumberOfStartupsWeek = 1,
-                    NumberOfStartupsMonth = 1,
-                    NumberOfClones = 1,
-                }, sameDay: false);
-                Func<Task> tick = null;
-
-                service.WhenForAnyArgs(x => x.StartTimer(null, new TimeSpan(), new TimeSpan()))
-                    .Do(x => tick = x.ArgAt<Func<Task>>(0));
-
-                var serviceProvider = CreateServiceProvider();
-                var target = new UsageTracker(serviceProvider, service);
-
-                await tick();
-
-                await service.Received().WriteLocalData(
-                    Arg.Is<UsageData>(x =>
-                        x.Model.NumberOfStartups == 0 &&
-                        x.Model.NumberOfStartupsWeek == 2 &&
-                        x.Model.NumberOfStartupsMonth == 2 &&
-                        x.Model.NumberOfClones == 0));
-            }
-
-            [Test]
-            public async Task NonMonthlyCountersShouldBeZeroed()
-            {
-                var service = CreateUsageService(new UsageModel
-                {
-                    NumberOfStartups = 1,
-                    NumberOfStartupsWeek = 1,
-                    NumberOfStartupsMonth = 1,
-                    NumberOfClones = 1,
-                }, sameDay: false, sameWeek: false);
-                Func<Task> tick = null;
-
-                service.WhenForAnyArgs(x => x.StartTimer(null, new TimeSpan(), new TimeSpan()))
-                    .Do(x => tick = x.ArgAt<Func<Task>>(0));
-
-                var serviceProvider = CreateServiceProvider();
-                var target = new UsageTracker(serviceProvider, service);
-
-                await tick();
-
-                await service.Received().WriteLocalData(
-                    Arg.Is<UsageData>(x =>
-                        x.Model.NumberOfStartups == 0 &&
-                        x.Model.NumberOfStartupsWeek == 0 &&
-                        x.Model.NumberOfStartupsMonth == 2 &&
-                        x.Model.NumberOfClones == 0));
-            }
-
-            [Test]
-            public async Task AllCountersShouldBeZeroed()
-            {
-                var service = CreateUsageService(new UsageModel
-                {
-                    NumberOfStartups = 1,
-                    NumberOfStartupsWeek = 1,
-                    NumberOfStartupsMonth = 1,
-                    NumberOfClones = 1,
-                }, sameDay: false, sameWeek: false, sameMonth: false);
-                Func<Task> tick = null;
-
-                service.WhenForAnyArgs(x => x.StartTimer(null, new TimeSpan(), new TimeSpan()))
-                    .Do(x => tick = x.ArgAt<Func<Task>>(0));
-
-                var serviceProvider = CreateServiceProvider();
-                var target = new UsageTracker(serviceProvider, service);
-
-                await tick();
-
-                await service.Received().WriteLocalData(
-                    Arg.Is<UsageData>(x =>
-                        x.Model.NumberOfStartups == 0 &&
-                        x.Model.NumberOfStartupsWeek == 0 &&
-                        x.Model.NumberOfStartupsMonth == 0 &&
-                        x.Model.NumberOfClones == 0));
-            }
+            service.Received(1).StartTimer(Arg.Any<Func<Task>>(), TimeSpan.FromMinutes(3), TimeSpan.FromHours(8));
         }
 
-        public class TheIncrementCounterMethod : TestBaseClass
+        [Test]
+        public async Task FirstTickShouldIncrementLaunchCount()
         {
-            [Test]
-            public async Task ShouldIncrementCounter()
+            var service = CreateUsageService();
+            var targetAndTick = CreateTargetAndGetTick(CreateServiceProvider(), service);
+
+            await targetAndTick.Item2();
+
+            await service.Received(1).WriteLocalData(
+                Arg.Is<UsageData>(x =>
+                    x.Model.NumberOfStartups == 1 &&
+                    x.Model.NumberOfStartupsWeek == 1 &&
+                    x.Model.NumberOfStartupsMonth == 1));
+        }
+
+        [Test]
+        public async Task SubsequentTickShouldNotIncrementLaunchCount()
+        {
+            var service = CreateUsageService();
+            var targetAndTick = CreateTargetAndGetTick(CreateServiceProvider(), service);
+
+            await targetAndTick.Item2();
+            service.ClearReceivedCalls();
+            await targetAndTick.Item2();
+
+            await service.DidNotReceiveWithAnyArgs().WriteLocalData(null);
+        }
+
+        [Test]
+        public async Task ShouldDisposeTimerIfMetricsServiceNotFound()
+        {
+            var service = CreateUsageService();
+            var disposed = false;
+            var disposable = Disposable.Create(() => disposed = true);
+            service.StartTimer(null, new TimeSpan(), new TimeSpan()).ReturnsForAnyArgs(disposable);
+
+            var targetAndTick = CreateTargetAndGetTick(
+                CreateServiceProvider(hasMetricsService: false),
+                service);
+
+            await targetAndTick.Item2();
+
+            Assert.True(disposed);
+        }
+
+        [Test]
+        public async Task TickShouldNotSendDataIfSameDay()
+        {
+            var serviceProvider = CreateServiceProvider();
+            var targetAndTick = CreateTargetAndGetTick(
+                serviceProvider,
+                CreateUsageService());
+
+            await targetAndTick.Item2();
+
+            var metricsService = serviceProvider.TryGetService<IMetricsService>();
+            await metricsService.DidNotReceive().PostUsage(Arg.Any<UsageModel>());
+        }
+
+        [Test]
+        public async Task TickShouldSendDataIfDifferentDay()
+        {
+            var serviceProvider = CreateServiceProvider();
+            var targetAndTick = CreateTargetAndGetTick(
+                serviceProvider,
+                CreateUsageService(sameDay: false));
+
+            await targetAndTick.Item2();
+
+            var metricsService = serviceProvider.TryGetService<IMetricsService>();
+            await metricsService.Received(1).PostUsage(Arg.Any<UsageModel>());
+        }
+
+        [Test]
+        public async Task NonWeeklyOrMonthlyCountersShouldBeZeroed()
+        {
+            var service = CreateUsageService(new UsageModel
             {
-                var model = new UsageModel { NumberOfClones = 4 };
-                var target = new UsageTracker(
-                    CreateServiceProvider(),
-                    CreateUsageService(model));
+                NumberOfStartups = 1,
+                NumberOfStartupsWeek = 1,
+                NumberOfStartupsMonth = 1,
+                NumberOfClones = 1,
+            }, sameDay: false);
+            Func<Task> tick = null;
 
-                await target.IncrementCounter(x => x.NumberOfClones);
+            service.WhenForAnyArgs(x => x.StartTimer(null, new TimeSpan(), new TimeSpan()))
+                .Do(x => tick = x.ArgAt<Func<Task>>(0));
 
-                Assert.That(5, Is.EqualTo(model.NumberOfClones));
+            var serviceProvider = CreateServiceProvider();
+            var target = new UsageTracker(serviceProvider, service);
+
+            await tick();
+
+            await service.Received().WriteLocalData(
+                Arg.Is<UsageData>(x =>
+                    x.Model.NumberOfStartups == 0 &&
+                    x.Model.NumberOfStartupsWeek == 2 &&
+                    x.Model.NumberOfStartupsMonth == 2 &&
+                    x.Model.NumberOfClones == 0));
+        }
+
+        [Test]
+        public async Task NonMonthlyCountersShouldBeZeroed()
+        {
+            var service = CreateUsageService(new UsageModel
+            {
+                NumberOfStartups = 1,
+                NumberOfStartupsWeek = 1,
+                NumberOfStartupsMonth = 1,
+                NumberOfClones = 1,
+            }, sameDay: false, sameWeek: false);
+            Func<Task> tick = null;
+
+            service.WhenForAnyArgs(x => x.StartTimer(null, new TimeSpan(), new TimeSpan()))
+                .Do(x => tick = x.ArgAt<Func<Task>>(0));
+
+            var serviceProvider = CreateServiceProvider();
+            var target = new UsageTracker(serviceProvider, service);
+
+            await tick();
+
+            await service.Received().WriteLocalData(
+                Arg.Is<UsageData>(x =>
+                    x.Model.NumberOfStartups == 0 &&
+                    x.Model.NumberOfStartupsWeek == 0 &&
+                    x.Model.NumberOfStartupsMonth == 2 &&
+                    x.Model.NumberOfClones == 0));
+        }
+
+        [Test]
+        public async Task AllCountersShouldBeZeroed()
+        {
+            var service = CreateUsageService(new UsageModel
+            {
+                NumberOfStartups = 1,
+                NumberOfStartupsWeek = 1,
+                NumberOfStartupsMonth = 1,
+                NumberOfClones = 1,
+            }, sameDay: false, sameWeek: false, sameMonth: false);
+            Func<Task> tick = null;
+
+            service.WhenForAnyArgs(x => x.StartTimer(null, new TimeSpan(), new TimeSpan()))
+                .Do(x => tick = x.ArgAt<Func<Task>>(0));
+
+            var serviceProvider = CreateServiceProvider();
+            var target = new UsageTracker(serviceProvider, service);
+
+            await tick();
+
+            await service.Received().WriteLocalData(
+                Arg.Is<UsageData>(x =>
+                    x.Model.NumberOfStartups == 0 &&
+                    x.Model.NumberOfStartupsWeek == 0 &&
+                    x.Model.NumberOfStartupsMonth == 0 &&
+                    x.Model.NumberOfClones == 0));
+        }
+
+        [Test]
+        public async Task ShouldIncrementCounter()
+        {
+            var model = new UsageModel { NumberOfClones = 4 };
+            var target = new UsageTracker(
+                CreateServiceProvider(),
+                CreateUsageService(model));
+
+            await target.IncrementCounter(x => x.NumberOfClones);
+
+            Assert.That(5, Is.EqualTo(model.NumberOfClones));
+        }
+
+        [Test]
+        public async Task ShouldWriteUpdatedData()
+        {
+            var data = new UsageData { Model = new UsageModel() };
+            var service = CreateUsageService(data);
+            var target = new UsageTracker(
+                CreateServiceProvider(),
+                service);
+
+            await target.IncrementCounter(x => x.NumberOfClones);
+
+            await service.Received(1).WriteLocalData(data);
+        }
+
+        [Test]
+        public async Task UsageServiceWritesAllTheDataCorrectly()
+        {
+            var model = CreateUsageModel();
+            var serviceProvider = CreateServiceProvider();
+            var usageService = CreateUsageService(model, sameDay: false);
+            var targetAndTick = CreateTargetAndGetTick(serviceProvider, usageService);
+
+            await targetAndTick.Item2();
+
+            var metricsService = serviceProvider.TryGetService<IMetricsService>();
+
+            UsageData result = usageService.ReceivedCalls().First(x => x.GetMethodInfo().Name == "WriteLocalData").GetArguments()[0] as UsageData;
+            CollectionAssert.AreEquivalent(
+                       model.GetType().GetRuntimeProperties().Select(x => new { x.Name, Value = x.GetValue(model) }),
+                result.Model.GetType().GetRuntimeProperties().Select(x => new { x.Name, Value = x.GetValue(model) }));
+        }
+
+                [Test]
+        public async Task MetricserviceSendsAllTheDataCorrectly()
+        {
+            var model = CreateUsageModel();
+            var serviceProvider = CreateServiceProvider();
+            var targetAndTick = CreateTargetAndGetTick(
+                serviceProvider,
+                CreateUsageService(model, sameDay: false));
+
+            await targetAndTick.Item2();
+
+            var metricsService = serviceProvider.TryGetService<IMetricsService>();
+
+            UsageData result = metricsService.ReceivedCalls().First(x => x.GetMethodInfo().Name == "PostUsage").GetArguments()[0] as UsageData;
+            CollectionAssert.AreEquivalent(
+                       model.GetType().GetRuntimeProperties().Select(x => new { x.Name, Value = x.GetValue(model) }),
+                result.Model.GetType().GetRuntimeProperties().Select(x => new { x.Name, Value = x.GetValue(model) }));
+        }
+
+        private static UsageModel CreateUsageModel()
+        {
+            var count = 1;
+            var model = new UsageModel();
+            var properties = model.GetType().GetRuntimeProperties();
+            foreach (var property in properties)
+            {
+                if (property.PropertyType == typeof(int))
+                {
+                    property.SetValue(model, count++);
+                }
+                else if (property.PropertyType == typeof(string))
+                {
+                    property.SetValue(model, $"string {count++}");
+                }
+                else if (property.PropertyType == typeof(bool))
+                {
+                    property.SetValue(model, true);
+                }
+                else
+                    Assert.Fail("Unknown field type in UsageModel. Fix this test to support it");
             }
 
-            [Test]
-            public async Task ShouldWriteUpdatedData()
-            {
-                var data = new UsageData { Model = new UsageModel() };
-                var service = CreateUsageService(data);
-                var target = new UsageTracker(
-                    CreateServiceProvider(),
-                    service);
-
-                await target.IncrementCounter(x => x.NumberOfClones);
-
-                await service.Received(1).WriteLocalData(data);
-            }
+            return model;
         }
 
         static Tuple<UsageTracker, Func<Task>> CreateTargetAndGetTick(
