d28c8644935d6a887776979d77195f4c34c0877d
Andreia Gaita <shana@spoiledcat.net>
2018-02-07 18:01:13 +0100

    This really needs to be a struct

--- src/GitHub.Exports/Models/UsageModel.cs
+++ src/GitHub.Exports/Models/UsageModel.cs
@@ -3,64 +3,67 @@ using System.Reflection;
 
 namespace GitHub.Models
 {
-    public class UsageModel
+    public struct UsageModel
     {
-        public bool IsGitHubUser { get; set; }
-        public bool IsEnterpriseUser { get; set; }
-        public string AppVersion { get; set; }
-        public string VSVersion { get; set; }
-        public string Lang { get; set; }
-        public int NumberOfStartups { get; set; }
-        public int NumberOfStartupsWeek { get; set; }
-        public int NumberOfStartupsMonth { get; set; }
-        public int NumberOfUpstreamPullRequests { get; set; }
-        public int NumberOfClones { get; set; }
-        public int NumberOfReposCreated { get; set; }
-        public int NumberOfReposPublished { get; set; }
-        public int NumberOfGists { get; set; }
-        public int NumberOfOpenInGitHub { get; set; }
-        public int NumberOfLinkToGitHub { get; set; }
-        public int NumberOfLogins { get; set; }
-        public int NumberOfOAuthLogins { get; set; }
-        public int NumberOfTokenLogins { get; set; }
-        public int NumberOfPullRequestsOpened { get; set; }
-        public int NumberOfLocalPullRequestsCheckedOut { get; set; }
-        public int NumberOfLocalPullRequestPulls { get; set; }
-        public int NumberOfLocalPullRequestPushes { get; set; }
-        public int NumberOfForkPullRequestsCheckedOut { get; set; }
-        public int NumberOfForkPullRequestPulls { get; set; }
-        public int NumberOfForkPullRequestPushes { get; set; }
-        public int NumberOfWelcomeDocsClicks { get; set; }
-        public int NumberOfWelcomeTrainingClicks { get; set; }
-        public int NumberOfGitHubPaneHelpClicks { get; set; }
-        public int NumberOfPRDetailsViewChanges { get; set; }
-        public int NumberOfPRDetailsViewFile { get; set; }
-        public int NumberOfPRDetailsCompareWithSolution { get; set; }
-        public int NumberOfPRDetailsOpenFileInSolution { get; set; }
-        public int NumberOfPRReviewDiffViewInlineCommentOpen { get; set; }
-        public int NumberOfPRReviewDiffViewInlineCommentPost { get; set; }
+        public bool IsGitHubUser;
+        public bool IsEnterpriseUser;
+        public string AppVersion;
+        public string VSVersion;
+        public string Lang;
+        public int NumberOfStartups;
+        public int NumberOfStartupsWeek;
+        public int NumberOfStartupsMonth;
+        public int NumberOfUpstreamPullRequests;
+        public int NumberOfClones;
+        public int NumberOfReposCreated;
+        public int NumberOfReposPublished;
+        public int NumberOfGists;
+        public int NumberOfOpenInGitHub;
+        public int NumberOfLinkToGitHub;
+        public int NumberOfLogins;
+        public int NumberOfOAuthLogins;
+        public int NumberOfTokenLogins;
+        public int NumberOfPullRequestsOpened;
+        public int NumberOfLocalPullRequestsCheckedOut;
+        public int NumberOfLocalPullRequestPulls;
+        public int NumberOfLocalPullRequestPushes;
+        public int NumberOfForkPullRequestsCheckedOut;
+        public int NumberOfForkPullRequestPulls;
+        public int NumberOfForkPullRequestPushes;
+        public int NumberOfWelcomeDocsClicks;
+        public int NumberOfWelcomeTrainingClicks;
+        public int NumberOfGitHubPaneHelpClicks;
+        public int NumberOfPRDetailsViewChanges;
+        public int NumberOfPRDetailsViewFile;
+        public int NumberOfPRDetailsCompareWithSolution;
+        public int NumberOfPRDetailsOpenFileInSolution;
+        public int NumberOfPRDetailsNavigateToEditor;
+        public int NumberOfPRReviewDiffViewInlineCommentOpen;
+        public int NumberOfPRReviewDiffViewInlineCommentPost;
 
         public UsageModel Clone(bool includeWeekly, bool includeMonthly)
         {
+            var result = this;
+            if (!includeWeekly)
+                result.NumberOfStartupsWeek = 0;
+            if (!includeMonthly)
+                result.NumberOfStartupsMonth = 0;
+            return result;
+        }
+
+        public UsageModel ClearCounters(bool clearWeekly, bool clearMonthly)
+        {
             var result = new UsageModel();
-            var properties = result.GetType().GetRuntimeProperties();
+            if (!clearWeekly)
+                result.NumberOfStartupsWeek = NumberOfStartupsWeek;
+            if (!clearMonthly)
+                result.NumberOfStartupsMonth = NumberOfStartupsMonth;
 
-            foreach (var property in properties)
-            {
-                var cloneValue = property.PropertyType == typeof(int);
-
-                if (property.Name == nameof(result.NumberOfStartupsWeek))
-                    cloneValue = includeWeekly;
-                else if (property.Name == nameof(result.NumberOfStartupsMonth))
-                    cloneValue = includeMonthly;
-
-                if (cloneValue)
-                {
-                    var value = property.GetValue(this);
-                    property.SetValue(result, value);
-                }
-            }
-
+            result.IsGitHubUser = IsGitHubUser;
+            result.IsEnterpriseUser = IsEnterpriseUser;
+            result.AppVersion = AppVersion;
+            result.VSVersion = VSVersion;
+            result.Lang = Lang;
             return result;
         }
     }
