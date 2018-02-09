e9f0c17c47d8ddc5425f300349ae66d17713f974
Jamie Cansdale <jcansdale@github.com>
2018-02-09 15:05:17 +0000

    Convert PullRequestStatusPackage to an AsyncPackage

--- src/GitHub.InlineReviews/PullRequestStatusPackage.cs
+++ src/GitHub.InlineReviews/PullRequestStatusPackage.cs
@@ -1,20 +1,22 @@
 ﻿using System;
+using System.Threading;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.ComponentModelHost;
 using GitHub.VisualStudio;
 using GitHub.InlineReviews.Services;
+using Task = System.Threading.Tasks.Task;
 
 namespace GitHub.InlineReviews
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [Guid(Guids.PullRequestStatusPackageId)]
     [ProvideAutoLoad(Guids.GitSccProviderId)]
-    public class PullRequestStatusPackage : Package
+    public class PullRequestStatusPackage : AsyncPackage
     {
-        protected override void Initialize()
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            var componentModel = (IComponentModel)GetService(typeof(SComponentModel));
+            var componentModel = (IComponentModel)await GetServiceAsync(typeof(SComponentModel));
             var exportProvider = componentModel.DefaultExportProvider;
             var pullRequestStatusManager = exportProvider.GetExportedValue<IPullRequestStatusBarManager>();
             pullRequestStatusManager.Initialize();
