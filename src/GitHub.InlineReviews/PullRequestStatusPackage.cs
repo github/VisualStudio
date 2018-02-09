4503596cc478345f10a60528fa6b118d27d6e9f1
Jamie Cansdale <jcansdale@github.com>
2018-02-09 12:03:53 +0000

    Make PR status visible when GitSccProvider is loaded

--- src/GitHub.InlineReviews/PullRequestStatusPackage.cs
+++ src/GitHub.InlineReviews/PullRequestStatusPackage.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.ComponentModelHost;
 using GitHub.VisualStudio;
 using GitHub.InlineReviews.Services;
@@ -10,7 +9,7 @@ namespace GitHub.InlineReviews
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [Guid(Guids.PullRequestStatusPackageId)]
-    [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
+    [ProvideAutoLoad(Guids.GitSccProviderId)]
     public class PullRequestStatusPackage : Package
     {
         protected override void Initialize()
