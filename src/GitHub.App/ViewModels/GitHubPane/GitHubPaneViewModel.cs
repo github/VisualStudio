87dd7ab0aef2cc3661b356ea426af1939a4daed3
Steven Kirk <grokys@gmail.com>
2018-03-19 12:34:50 +0100

    Fix GitHubPane route regex.
    Need to make sure we match the start and end of the string.

--- src/GitHub.App/ViewModels/GitHubPane/GitHubPaneViewModel.cs
+++ src/GitHub.App/ViewModels/GitHubPane/GitHubPaneViewModel.cs
@@ -408,7 +408,7 @@ namespace GitHub.ViewModels.GitHubPane
         static Regex CreateRoute(string route)
         {
             // Build RegEx from route (:foo to named group (?<foo>[\w_.-]+)).
-            var routeFormat = new Regex("(:([a-z]+))\\b").Replace(route, @"(?<$2>[\w_.-]+)");
+            var routeFormat = "^" + new Regex("(:([a-z]+))\\b").Replace(route, @"(?<$2>[\w_.-]+)") + "$";
             return new Regex(routeFormat, RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase);
         }
     }
