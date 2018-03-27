bff29ef31555e3f4d1c398dec0f91ecdc820dda6
Jamie Cansdale <jcansdale@github.com>
2018-03-27 15:10:09 +0100

    Add enable inline comments margin functionality

--- src/GitHub.InlineReviews/Commands/EnableInlineCommentsCommand.cs
+++ src/GitHub.InlineReviews/Commands/EnableInlineCommentsCommand.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
+using GitHub.Services;
 using GitHub.VisualStudio;
 using GitHub.Services.Vssdk.Commands;
 using System.ComponentModel.Composition;
+using EnvDTE;
+using GitHub.Settings;
 
 namespace GitHub.InlineReviews.Commands
 {
@@ -19,14 +22,60 @@ namespace GitHub.InlineReviews.Commands
         /// </summary>
         public const int CommandId = PkgCmdIDList.EnableInlineCommentsId;
 
-        public EnableInlineCommentsCommand() : base(CommandSet, CommandId)
+        readonly IGitHubServiceProvider gitHubServiceProvider;
+        readonly IPullRequestEditorService pullRequestEditorService;
+        readonly IPackageSettings packageSettings;
+
+        [ImportingConstructor]
+        public EnableInlineCommentsCommand(IGitHubServiceProvider gitHubServiceProvider, IPullRequestEditorService pullRequestEditorService,
+            IPackageSettings packageSettings)
+            : base(CommandSet, CommandId)
         {
+            this.pullRequestEditorService = pullRequestEditorService;
+            this.gitHubServiceProvider = gitHubServiceProvider;
+            this.packageSettings = packageSettings;
         }
 
         public override Task Execute()
         {
-            System.Diagnostics.Trace.WriteLine("EnableInlineComments");
+            var view = pullRequestEditorService.FindActiveView();
+            string file = FindActiveFile();
+
+            using (EnableEditorComments())
+            {
+                pullRequestEditorService.NavigateToEquivalentPosition(view, file);
+            }
+
             return Task.CompletedTask;
+        }
+
+        string FindActiveFile()
+        {
+            var dte = gitHubServiceProvider.GetService<DTE>();
+            var file = dte.ActiveDocument.FullName;
+            return file;
+        }
+
+        IDisposable EnableEditorComments() => new EnableEditorCommentsContext(packageSettings);
+
+        // Editor comments will be enabled when a file is opened from inside this context.
+        class EnableEditorCommentsContext : IDisposable
+        {
+            readonly IPackageSettings settings;
+            bool editorComments;
+
+            internal EnableEditorCommentsContext(IPackageSettings settings)
+            {
+                this.settings = settings;
+
+                editorComments = settings.EditorComments;
+                settings.EditorComments = true;
+            }
+
+            public void Dispose()
+            {
+                settings.EditorComments = editorComments;
+            }
         }
     }
 }
