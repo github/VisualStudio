acb8a945d0533df69e0c0e122ab31e9ce51bef8f
Jamie Cansdale <jcansdale@github.com>
2018-04-11 10:53:04 +0100

    Prototype toggle margin using GlyphMargin
    This toggles the GlyphMargin on and off.
    It will be changed to toggle the comments margin.

--- src/GitHub.InlineReviews/Commands/EnableInlineCommentsCommand.cs
+++ src/GitHub.InlineReviews/Commands/EnableInlineCommentsCommand.cs
@@ -3,6 +3,10 @@ using System.Threading.Tasks;
 using GitHub.VisualStudio;
 using GitHub.Services.Vssdk.Commands;
 using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Editor;
+using GitHub.Services;
 
 namespace GitHub.InlineReviews.Commands
 {
@@ -19,13 +23,31 @@ namespace GitHub.InlineReviews.Commands
         /// </summary>
         public const int CommandId = PkgCmdIDList.EnableInlineCommentsId;
 
-        public EnableInlineCommentsCommand() : base(CommandSet, CommandId)
+        const string optionName = "TextViewHost/GlyphMargin";
+
+        readonly Lazy<IVsTextManager> textManager;
+        readonly Lazy<IVsEditorAdaptersFactoryService> editorAdapter;
+
+        [ImportingConstructor]
+        public EnableInlineCommentsCommand(
+            IGitHubServiceProvider serviceProvider,
+            Lazy<IVsEditorAdaptersFactoryService> editorAdapter) : base(CommandSet, CommandId)
         {
+            textManager = new Lazy<IVsTextManager>(() => serviceProvider.GetService<SVsTextManager, IVsTextManager>());
+            this.editorAdapter = editorAdapter;
         }
 
         public override Task Execute()
         {
-            System.Diagnostics.Trace.WriteLine("EnableInlineComments");
+            IVsTextView activeView = null;
+            if (textManager.Value.GetActiveView(1, null, out activeView) == VSConstants.S_OK)
+            {
+                var wpfTextView = editorAdapter.Value.GetWpfTextView(activeView);
+                var options = wpfTextView.Options;
+                var enabled = options.GetOptionValue<bool>(optionName);
+                options.SetOptionValue(optionName, !enabled);
+            }
+
             return Task.CompletedTask;
         }
     }
