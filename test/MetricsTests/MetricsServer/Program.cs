abcbda90df8eab34d3711210b9812446de733485
Andreia Gaita <shana@spoiledcat.net>
2018-04-16 10:53:30 +0200

    Metrics tests

---
+++ test/MetricsTests/MetricsServer/Program.cs
@@ -0,0 +1,74 @@
+﻿using GitHub.Models;
+using Nancy;
+using Nancy.Hosting.Self;
+using Nancy.ModelBinding;
+using Nancy.Responses.Negotiation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using Nancy.Extensions;
+
+namespace MetricsServer
+{
+    public class Server
+    {
+        readonly string host;
+        readonly int port;
+        NancyHost server;
+        public Server(string host, int port)
+        {
+            this.host = host;
+            this.port = port;
+        }
+
+        public void Start()
+        {
+            var conf = new HostConfiguration { RewriteLocalhost = false };
+            server = new NancyHost(conf, new Uri($"http://{host}:{port}"));
+            server.Start();
+        }
+
+        public void Stop()
+        {
+            server.Stop();
+        }
+    }
+
+    public class UsageModule : NancyModule
+    {
+        public UsageModule()
+        {
+            Post["/api/usage/visualstudio"] = p =>
+            {
+                Console.WriteLine(Request.Body.AsString());
+                var errors = new List<string>();
+                var usage = this.Bind<UsageModel>();
+                if (String.IsNullOrEmpty(usage.AppVersion))
+                    errors.Add("Empty appVersion");
+                if (String.IsNullOrEmpty(usage.Lang))
+                    errors.Add("Empty lang");
+                if (String.IsNullOrEmpty(usage.VSVersion))
+                    errors.Add("Empty vSVersion");
+                if (usage.NumberOfStartups == 0)
+                    errors.Add("Startups is 0");
+                if (errors.Count > 0)
+                {
+                    return Negotiate
+                        .WithStatusCode(HttpStatusCode.BadRequest)
+                        .WithAllowedMediaRange(MediaRange.FromString("application/json"))
+                        .WithMediaRangeModel(
+                              MediaRange.FromString("application/json"),
+                              new { result = errors }); // Model for 'application/json';
+                }
+
+                return Negotiate
+                    .WithAllowedMediaRange(MediaRange.FromString("application/json"))
+                    .WithMediaRangeModel(
+                            MediaRange.FromString("application/json"),
+                            new { result = "Cool usage" }); // Model for 'application/json';
+            };
+        }
+    }
+}
---
+++ test/MetricsTests/MetricsServerApp/Program.cs
@@ -0,0 +1,117 @@
+﻿using GitHub.Models;
+using Octokit.Internal;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricsServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Run();
+            Console.Read();
+        }
+
+        void Run()
+        {
+            var uri = new Uri("http://localhost:40000");
+            var server = new MetricsServer.Server(uri.Host, uri.Port);
+
+            server.Start();
+        }
+
+        async Task Send()
+        {
+            var uri = new Uri("http://localhost:40000");
+            var server = new MetricsServer.Server(uri.Host, uri.Port);
+
+            server.Start();
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders
+                  .Accept
+                  .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var request = new HttpRequestMessage(HttpMethod.Post, new Uri(uri, "/api/usage/visualstudio"));
+
+            var data = new UsageData();
+            var model = data.Model;
+            model.AppVersion = "9.9.9";
+            model.Lang = "en-us";
+            model.VSVersion = "14";
+            model.NumberOfStartups = 1;
+
+            request.Content = SerializeRequest(model);
+
+            HttpResponseMessage response = null;
+            try
+            {
+                response = await client.SendAsync(request);
+
+            }
+            catch (Exception ex)
+            {
+                Debugger.Break();
+            }
+            var ret = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(response.ToString());
+            Console.WriteLine(ret);
+
+            server.Stop();
+        }
+
+        static StringContent SerializeRequest(UsageModel model)
+        {
+            var serializer = new SimpleJsonSerializer();
+            var dictionary = ToModelDictionary(model);
+            return new StringContent(serializer.Serialize(dictionary), Encoding.UTF8, "application/json");
+        }
+
+        static Dictionary<string, object> ToModelDictionary(object model)
+        {
+            var dict = new Dictionary<string, object>();
+            var type = model.GetType();
+
+            foreach (var prop in type.GetProperties())
+            {
+                if (prop.PropertyType.IsValueType || prop.PropertyType == typeof(string))
+                {
+                    dict.Add(ToJsonPropertyName(prop.Name), prop.GetValue(model));
+                }
+                else
+                {
+                    var value = prop.GetValue(model);
+
+                    if (value == null)
+                    {
+                        dict.Add(ToJsonPropertyName(prop.Name), value);
+                    }
+                    else
+                    {
+                        dict.Add(ToJsonPropertyName(prop.Name), ToModelDictionary(value));
+                    }
+                }
+            }
+
+            return dict;
+        }
+
+        static string ToJsonPropertyName(string propertyName)
+        {
+            if (propertyName.Length < 2)
+            {
+                return propertyName.ToLowerInvariant();
+            }
+
+            return propertyName.Substring(0, 1).ToLowerInvariant() + propertyName.Substring(1);
+        }
+    }
+}
