56844b16d019029f6028a07ae62bab61086daa59
Jamie Cansdale <jcansdale@github.com>
2018-04-17 16:13:16 +0100

    Add NumberOfOpenPullRequests counter to UsageModel
    Track usage of the PR status bar commands explicitly.

--- src/GitHub.InlineReviews/Services/PullRequestStatusBarManager.cs
+++ src/GitHub.InlineReviews/Services/PullRequestStatusBarManager.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.ComponentModel.Composition;
 using System.Reactive.Linq;
+using System.Linq.Expressions;
 using GitHub.Commands;
 using GitHub.InlineReviews.Views;
 using GitHub.InlineReviews.ViewModels;
 using GitHub.Services;
 using GitHub.Models;
 using GitHub.Logging;
+using GitHub.Extensions;
 using Serilog;
 using ReactiveUI;
 
@@ -24,8 +27,9 @@ namespace GitHub.InlineReviews.Services
         static readonly ILogger log = LogManager.ForContext<PullRequestStatusBarManager>();
         const string StatusBarPartName = "PART_SccStatusBarHost";
 
-        readonly IOpenPullRequestsCommand openPullRequestsCommand;
-        readonly IShowCurrentPullRequestCommand showCurrentPullRequestCommand;
+        readonly IUsageTracker usageTracker;
+        readonly ICommand openPullRequestsCommand;
+        readonly ICommand showCurrentPullRequestCommand;
 
         // At the moment these must be constructed on the main thread.
         // TeamExplorerContext needs to retrieve DTE using GetService.
@@ -36,13 +40,19 @@ namespace GitHub.InlineReviews.Services
 
         [ImportingConstructor]
         public PullRequestStatusBarManager(
+            IUsageTracker usageTracker,
             IOpenPullRequestsCommand openPullRequestsCommand,
             IShowCurrentPullRequestCommand showCurrentPullRequestCommand,
             Lazy<IPullRequestSessionManager> pullRequestSessionManager,
             Lazy<ITeamExplorerContext> teamExplorerContext)
         {
-            this.openPullRequestsCommand = openPullRequestsCommand;
-            this.showCurrentPullRequestCommand = showCurrentPullRequestCommand;
+            this.usageTracker = usageTracker;
+
+            this.openPullRequestsCommand = new UsageTrackingCommand(openPullRequestsCommand,
+                usageTracker, x => x.NumberOfOpenPullRequests);
+            this.showCurrentPullRequestCommand = new UsageTrackingCommand(showCurrentPullRequestCommand,
+                usageTracker, x => x.NumberOfShowCurrentPullRequest);
+
             this.pullRequestSessionManager = pullRequestSessionManager;
             this.teamExplorerContext = teamExplorerContext;
         }
@@ -135,6 +145,38 @@ namespace GitHub.InlineReviews.Services
         {
             var contentControl = mainWindow?.Template?.FindName(StatusBarPartName, mainWindow) as ContentControl;
             return contentControl?.Content as StatusBar;
+        }
+
+        class UsageTrackingCommand : ICommand
+        {
+            readonly ICommand command;
+            readonly IUsageTracker usageTracker;
+            readonly Expression<Func<UsageModel.MeasuresModel, int>> counter;
+
+            internal UsageTrackingCommand(ICommand command, IUsageTracker usageTracker,
+                Expression<Func<UsageModel.MeasuresModel, int>> counter)
+            {
+                this.command = command;
+                this.usageTracker = usageTracker;
+                this.counter = counter;
+            }
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { command.CanExecuteChanged += value; }
+                remove { command.CanExecuteChanged -= value; }
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return command.CanExecute(parameter);
+            }
+
+            public void Execute(object parameter)
+            {
+                command.Execute(parameter);
+                usageTracker.IncrementCounter(counter).Forget();
+            }
         }
     }
 }
