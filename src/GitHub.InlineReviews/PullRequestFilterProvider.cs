79a8aea0ad5145b567a1cd1f58a8cbecfbbcf9dd
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2018-05-16 08:19:07 +0200

    Add Pull Request filter button to Visual Studio solution explorer

---
+++ src/GitHub.InlineReviews/PullRequestFilterProvider.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Threading.Tasks;
+using GitHub.Services;
+using GitHub.VisualStudio;
+using Microsoft.Internal.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
+
+namespace GitHub.InlineReviews
+{
+    public class PullRequestFilterPackageGuids
+    {
+        public const string GuidPullRequestFilterPackageCmdSetString = "7cde2dfc-43c9-41ff-bf2e-bef41cd99e09";
+        public const int PullRequestFilterId = 0x0100;
+    }
+
+    [SolutionTreeFilterProvider(PullRequestFilterPackageGuids.GuidPullRequestFilterPackageCmdSetString, PullRequestFilterPackageGuids.PullRequestFilterId)]
+    [Export]
+    public class PullRequestFilterProvider : HierarchyTreeFilterProvider
+    {
+        private readonly IVsHierarchyItemCollectionProvider hierarchyCollectionProvider;
+        private readonly IGitHubServiceProvider githubServiceProvider;
+
+        [ImportingConstructor]
+        public PullRequestFilterProvider(IVsHierarchyItemCollectionProvider hierarchyCollectionProvider, IGitHubServiceProvider githubServiceProvider)
+        {
+            this.hierarchyCollectionProvider = hierarchyCollectionProvider;
+            this.githubServiceProvider = githubServiceProvider;
+        }
+
+        protected override HierarchyTreeFilter CreateFilter()
+        {
+            return new PullRequestFilter(hierarchyCollectionProvider, githubServiceProvider);
+        }
+
+        private sealed class PullRequestFilter : HierarchyTreeFilter
+        {
+            private readonly IVsHierarchyItemCollectionProvider hierarchyCollectionProvider;
+            private readonly IGitHubServiceProvider githubServiceProvider;
+            private IPullRequestSessionManager sessionManager;
+            private HashSet<string> pullRequestSessionFiles;
+
+            public PullRequestFilter(IVsHierarchyItemCollectionProvider hierarchyCollectionProvider, IGitHubServiceProvider githubServiceProvider)
+            {
+                this.hierarchyCollectionProvider = hierarchyCollectionProvider;
+                this.githubServiceProvider = githubServiceProvider;
+            }
+
+            IPullRequestSessionManager SessionManager
+            {
+                get
+                {
+                    // Lazily load the pull request session manager to prevent all of our assemblies
+                    // being loaded on VS startup.
+                    if (sessionManager == null)
+                    {
+                        sessionManager = githubServiceProvider.GetService<IPullRequestSessionManager>();
+                    }
+
+                    return sessionManager;
+                }
+            }
+
+            // Gets the items to be included from this filter provider.
+            // rootItems is a collection that contains the root of your solution
+            // Returns a collection of items to be included as part of the filter
+            protected override async Task<IReadOnlyObservableSet> GetIncludedItemsAsync(IEnumerable<IVsHierarchyItem> rootItems)
+            {
+                var root = HierarchyUtilities.FindCommonAncestor(rootItems);
+                var sourceItems = await hierarchyCollectionProvider.GetDescendantsAsync(root.HierarchyIdentity.NestedHierarchy, CancellationToken);
+
+                var vsSolution = githubServiceProvider.GetSolution();
+                vsSolution.GetSolutionInfo(out var solutionDirectory, out _, out _);
+
+                this.pullRequestSessionFiles = new HashSet<string>();
+                if (SessionManager.CurrentSession != null)
+                {
+                    var requestSessionFiles = await SessionManager.CurrentSession.GetAllFiles();
+                    requestSessionFiles.ForEach(file => this.pullRequestSessionFiles.Add(BuildAbsolutePath(solutionDirectory, file.RelativePath)));
+                }
+
+                return await hierarchyCollectionProvider.GetFilteredHierarchyItemsAsync(sourceItems, ShouldIncludeInFilter, CancellationToken);
+            }
+
+            // Returns true if filters hierarchy item name for given filter; otherwise, false</returns>
+            private bool ShouldIncludeInFilter(IVsHierarchyItem hierarchyItem)
+            {
+                return hierarchyItem?.CanonicalName != null && pullRequestSessionFiles.Contains(hierarchyItem.CanonicalName);
+            }
+
+            private static string BuildAbsolutePath(string solutionDirectory, string fileRelativePath)
+            {
+                return Path.Combine(solutionDirectory, fileRelativePath.Replace("/", @"\")).ToLower();
+            }
+        }
+    }
+}
