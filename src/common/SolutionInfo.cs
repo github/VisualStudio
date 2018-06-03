dfb94c3804bc1b495b555cecc518a7abbdc21bc0
MysticBoy <mysticboy@live.com>
2018-06-04 00:33:59 +0800

    Chinese interface shows normal, preliminary translation complete.

--- src/common/SolutionInfo.cs
+++ src/common/SolutionInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en-US")]
+
 
 namespace System
 {
