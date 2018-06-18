b2945cb3952be224f5c04cbd3092b1dd9f57d4e9
Šimon Koníček <neme112@outlook.com>
2018-06-18 13:27:05 +0200

    Fixing crash in WPF designer

--- src/GitHub.VisualStudio/Views/GitHubPane/FileNameToImageMonikerConverter.cs
+++ src/GitHub.VisualStudio/Views/GitHubPane/FileNameToImageMonikerConverter.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
+using Microsoft.VisualStudio.Imaging.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 
@@ -19,6 +22,10 @@ namespace GitHub.VisualStudio.Views.GitHubPane
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            // In design mode, imageService will be null
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+                return default(ImageMoniker);
+
             return imageService.GetImageMonikerForFile((string)value);
         }
 
