deaad941a5d16c5a1a60805ae4a9af62c4a57c1d
Steven Kirk <grokys@users.noreply.github.com>
2018-06-25 11:51:48 +0200

    Explicitly include resx file in GitHub.App.
    Had to also remove the `<RootNamespace>` as it was causing the resources to get generated in the wrong place.

--- src/GitHub.App/Resources.Designer.cs
+++ src/GitHub.App/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace GitHub.App {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -210,7 +210,7 @@ namespace GitHub.App {
         /// <summary>
         ///   Looks up a localized string similar to Fork Repository.
         /// </summary>
-        internal static string ForkRepositoryTitle {
+        public static string ForkRepositoryTitle {
             get {
                 return ResourceManager.GetString("ForkRepositoryTitle", resourceCulture);
             }
@@ -588,7 +588,7 @@ namespace GitHub.App {
         /// <summary>
         ///   Looks up a localized string similar to Switch Origin.
         /// </summary>
-        internal static string SwitchOriginTitle {
+        public static string SwitchOriginTitle {
             get {
                 return ResourceManager.GetString("SwitchOriginTitle", resourceCulture);
             }
