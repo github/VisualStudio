bbbe93094af65115c9d2e47689b20e2f7dc2af11
Jamie Cansdale <jcansdale@github.com>
2018-07-25 13:13:59 +0100

    Load GitContextPackage in UICONTEXT.RepositoryOpen
    Only load the GitContextPackage  when there is an open repository.

--- src/GitHub.VisualStudio/GitContextPackage.cs
+++ src/GitHub.VisualStudio/GitContextPackage.cs
@@ -5,17 +5,18 @@ using GitHub.Exports;
 using GitHub.Services;
 using Microsoft.VisualStudio.Shell;
 using Task = System.Threading.Tasks.Task;
+using static Microsoft.VisualStudio.VSConstants;
 
 namespace GitHub.VisualStudio
 {
     /// <summary>
     /// This package creates a custom UIContext <see cref="Guids.UIContext_Git"/> that is activated when a
-    /// repository is active in <see cref="IVSGitExt"/>.
+    /// repository is active in <see cref="IVSGitExt"/> and the current process is Visual Studio (not Blend).
     /// </summary>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(Guids.UIContext_Git)]
-    // this is the Git service GUID, so we load whenever it loads
-    [ProvideAutoLoad(Guids.GitSccProviderId, PackageAutoLoadFlags.BackgroundLoad)]
+    // Initialize when we enter the context of a Git repository
+    [ProvideAutoLoad(UICONTEXT.RepositoryOpen_string, PackageAutoLoadFlags.BackgroundLoad)]
     public class GitContextPackage : AsyncPackage
     {
         protected async override Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
