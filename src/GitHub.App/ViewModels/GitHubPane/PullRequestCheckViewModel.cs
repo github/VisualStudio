93975e7ac7a37ca5115a9276dcd2705832a1f1c9
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-08-20 15:04:19 -0400

    DetailsUrl can be null

--- src/GitHub.App/ViewModels/GitHubPane/PullRequestCheckViewModel.cs
+++ src/GitHub.App/ViewModels/GitHubPane/PullRequestCheckViewModel.cs
@@ -48,7 +48,7 @@ namespace GitHub.ViewModels.GitHubPane
                 pullRequestCheckViewModel.Title = model.Context;
                 pullRequestCheckViewModel.Description = model.Description;
                 pullRequestCheckViewModel.Status = checkStatus;
-                pullRequestCheckViewModel.DetailsUrl = new Uri(model.TargetUrl);
+                pullRequestCheckViewModel.DetailsUrl = model.TargetUrl != null ? new Uri(model.TargetUrl) : null;
                 pullRequestCheckViewModel.AvatarUrl = model.AvatarUrl ?? DefaultAvatar;
                 pullRequestCheckViewModel.Avatar = model.AvatarUrl != null
                     ? new BitmapImage(new Uri(model.AvatarUrl))
