a6a408bfa1e971a8e793b6a62e594c977ebba2cf
Jamie Cansdale <jcansdale@github.com>
2018-08-29 16:13:23 +0100

    Add support for Visual Studio 2019
    This commit targets the Microsoft.TeamFoundation.* assemblies from
    Visual Studio 2017 with their version numbers changed to 16.0. This
    will hopefully be compatible with Visual Studio 2019.

---
+++ src/GitHub.TeamFoundation.16/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GitHub.TeamFoundation.16")]
+[assembly: AssemblyDescription("GitHub TeamFoundation")]
+[assembly: Guid("b5f72f45-adf1-4630-b066-eebf99f945a3")]
--- src/GitHub.VisualStudio/Services/VSGitExtFactory.cs
+++ src/GitHub.VisualStudio/Services/VSGitExtFactory.cs
@@ -1,5 +1,6 @@
 ﻿extern alias TF14;
 extern alias TF15;
+extern alias TF16;
 
 using System;
 using GitHub.Logging;
@@ -7,6 +8,7 @@ using Serilog;
 using Microsoft.VisualStudio.Shell;
 using VSGitExt14 = TF14.GitHub.VisualStudio.Base.VSGitExt;
 using VSGitExt15 = TF15.GitHub.VisualStudio.Base.VSGitExt;
+using VSGitExt16 = TF16.GitHub.VisualStudio.Base.VSGitExt;
 
 namespace GitHub.Services
 {
@@ -31,13 +33,15 @@ namespace GitHub.Services
                     return Create(() => new VSGitExt14(asyncServiceProvider));
                 case 15:
                     return Create(() => new VSGitExt15(asyncServiceProvider));
+                case 16:
+                    return Create(() => new VSGitExt16(asyncServiceProvider));
                 default:
                     log.Error("There is no IVSGitExt implementation for DTE version {Version}", vsVersion);
                     return null;
             }
         }
 
-        // NOTE: We're being careful to only reference VSGitExt14 and VSGitExt15 from inside a lambda expression.
+        // NOTE: We're being careful to only reference VSGitExt14/VSGitExt15/VSGitExt16 from inside a lambda expression.
         // This ensures that only the type that's compatible with the running DTE version is loaded.
         static IVSGitExt Create(Func<IVSGitExt> factory) => factory.Invoke();
     }
