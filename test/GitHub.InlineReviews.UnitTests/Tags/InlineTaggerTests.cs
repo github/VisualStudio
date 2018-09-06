db07173a39db6b6ce4172b0119b58e8102edef17
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-09-06 14:39:07 -0400

    Rename class

--- test/GitHub.InlineReviews.UnitTests/Tags/InlineTaggerTests.cs
+++ test/GitHub.InlineReviews.UnitTests/Tags/InlineTaggerTests.cs
@@ -14,7 +14,7 @@ using GitHub.InlineReviews.Margins;
 
 namespace GitHub.InlineReviews.UnitTests.Tags
 {
-    public class InlineCommentTaggerTests
+    public class InlineTaggerTests
     {
         public class WithTextBufferInfo
         {
