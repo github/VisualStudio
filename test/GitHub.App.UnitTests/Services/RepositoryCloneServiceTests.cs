451a5b03036c0b51acb13ee5ff3814db346c77f9
Jamie Cansdale <jcansdale@github.com>
2018-09-25 14:43:04 +0100

    Only increment NumberOfClones during clone
    It appears that CloneRepository is also called when a new repository is
    created. Only increment the new counters during CloneOrOpenRepository.

--- test/GitHub.App.UnitTests/Services/RepositoryCloneServiceTests.cs
+++ test/GitHub.App.UnitTests/Services/RepositoryCloneServiceTests.cs
@@ -28,10 +28,10 @@ public class RepositoryCloneServiceTests
         }
 
         [TestCase("https://github.com/foo/bar", 1, nameof(UsageModel.MeasuresModel.NumberOfClones))]
-        [TestCase("https://github.com/foo/bar", 1, nameof(UsageModel.MeasuresModel.NumberOfGitHubClones))]
+        [TestCase("https://github.com/foo/bar", 0, nameof(UsageModel.MeasuresModel.NumberOfGitHubClones))]
         [TestCase("https://github.com/foo/bar", 0, nameof(UsageModel.MeasuresModel.NumberOfEnterpriseClones))]
         [TestCase("https://enterprise.com/foo/bar", 1, nameof(UsageModel.MeasuresModel.NumberOfClones))]
-        [TestCase("https://enterprise.com/foo/bar", 1, nameof(UsageModel.MeasuresModel.NumberOfEnterpriseClones))]
+        [TestCase("https://enterprise.com/foo/bar", 0, nameof(UsageModel.MeasuresModel.NumberOfEnterpriseClones))]
         [TestCase("https://enterprise.com/foo/bar", 0, nameof(UsageModel.MeasuresModel.NumberOfGitHubClones))]
         public async Task UpdatesMetricsWhenRepositoryClonedAsync(string cloneUrl, int numberOfCalls, string counterName)
         {
@@ -45,6 +45,30 @@ public class RepositoryCloneServiceTests
 
             await cloneService.CloneRepository(cloneUrl, @"c:\dev\bar");
             var model = UsageModel.Create(Guid.NewGuid());
+
+            await usageTracker.Received(numberOfCalls).IncrementCounter(
+                Arg.Is<Expression<Func<UsageModel.MeasuresModel, int>>>(x =>
+                    ((MemberExpression)x.Body).Member.Name == counterName));
+        }
+
+        [TestCase("https://github.com/foo/bar", 1, nameof(UsageModel.MeasuresModel.NumberOfClones))]
+        [TestCase("https://github.com/foo/bar", 1, nameof(UsageModel.MeasuresModel.NumberOfGitHubClones))]
+        [TestCase("https://github.com/foo/bar", 0, nameof(UsageModel.MeasuresModel.NumberOfEnterpriseClones))]
+        [TestCase("https://enterprise.com/foo/bar", 1, nameof(UsageModel.MeasuresModel.NumberOfClones))]
+        [TestCase("https://enterprise.com/foo/bar", 1, nameof(UsageModel.MeasuresModel.NumberOfEnterpriseClones))]
+        [TestCase("https://enterprise.com/foo/bar", 0, nameof(UsageModel.MeasuresModel.NumberOfGitHubClones))]
+        public async Task UpdatesMetricsWhenCloneOrOpenRepositoryAsync(string cloneUrl, int numberOfCalls, string counterName)
+        {
+            var cloneDialogResult = new CloneDialogResult(@"c:\dev\bar", cloneUrl);
+            var serviceProvider = Substitutes.ServiceProvider;
+            var operatingSystem = serviceProvider.GetOperatingSystem();
+            var vsGitServices = serviceProvider.GetVSGitServices();
+            var teamExplorerServices = Substitute.For<ITeamExplorerServices>();
+            var graphqlFactory = Substitute.For<IGraphQLClientFactory>();
+            var usageTracker = Substitute.For<IUsageTracker>();
+            var cloneService = new RepositoryCloneService(operatingSystem, vsGitServices, teamExplorerServices, graphqlFactory, usageTracker);
+
+            await cloneService.CloneOrOpenRepository(cloneDialogResult);
 
             await usageTracker.Received(numberOfCalls).IncrementCounter(
                 Arg.Is<Expression<Func<UsageModel.MeasuresModel, int>>>(x =>
