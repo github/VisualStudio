aa8d2f7849877494ff0f257d0c81af7285f79c6a
Jamie Cansdale <jcansdale@github.com>
2018-11-16 14:54:02 +0000

    Remove VSServices.TryOpenRepository hack
    It worked but it's a bit of a hack.

--- src/GitHub.Exports/Services/IVSServices.cs
+++ src/GitHub.Exports/Services/IVSServices.cs
@@ -10,16 +10,6 @@ namespace GitHub.Services
         /// </summary>
         string VSVersion { get; }
 
-        /// <summary>Open a repository in Team Explorer</summary>
-        /// <remarks>
-        /// There doesn't appear to be a command that directly opens a target repo.
-        /// Our workaround is to create, open and delete a solution in the repo directory.
-        /// This triggers an event that causes the target repo to open. ;)
-        /// </remarks>
-        /// <param name="directory">The path to the repository to open</param>
-        /// <returns>True if a transient solution was successfully created in target directory (which should trigger opening of repository).</returns>
-        bool TryOpenRepository(string directory);
-
         /// <summary>
         /// Displays a message box with the specified message.
         /// </summary>
--- src/GitHub.Exports/Services/VSServices.cs
+++ src/GitHub.Exports/Services/VSServices.cs
@@ -9,7 +9,6 @@ using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Rothko;
 using Serilog;
-using EnvDTE;
 
 namespace GitHub.Services
 {
@@ -17,22 +16,13 @@ namespace GitHub.Services
     [PartCreationPolicy(CreationPolicy.Shared)]
     public class VSServices : IVSServices
     {
-        readonly ILogger log;
+        readonly static ILogger log = LogManager.ForContext<VSServices>();
         readonly IGitHubServiceProvider serviceProvider;
 
-        // Use a prefix (~$) that is defined in the default VS gitignore.
-        public const string TempSolutionName = "~$GitHubVSTemp$~";
-
         [ImportingConstructor]
-        public VSServices(IGitHubServiceProvider serviceProvider) :
-            this(serviceProvider, LogManager.ForContext<VSServices>())
-        {
-        }
-
-        public VSServices(IGitHubServiceProvider serviceProvider, ILogger log)
+        public VSServices(IGitHubServiceProvider serviceProvider)
         {
             this.serviceProvider = serviceProvider;
-            this.log = log;
         }
 
         string vsVersion;
@@ -52,56 +42,6 @@ namespace GitHub.Services
         {
             return (VSConstants.MessageBoxResult)VsShellUtilities.ShowMessageBox(serviceProvider, message, null,
                 OLEMSGICON.OLEMSGICON_INFO, OLEMSGBUTTON.OLEMSGBUTTON_OK, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-        }
-
-        /// <summary>Open a repository in Team Explorer</summary>
-        /// <remarks>
-        /// There doesn't appear to be a command that directly opens a target repo.
-        /// Our workaround is to create, open and delete a solution in the repo directory.
-        /// This triggers an event that causes the target repo to open. ;)
-        /// </remarks>
-        /// <param name="repoPath">The path to the repository to open</param>
-        /// <returns>True if a transient solution was successfully created in target directory (which should trigger opening of repository).</returns>
-        public bool TryOpenRepository(string repoPath)
-        {
-            var os = serviceProvider.TryGetService<IOperatingSystem>();
-            if (os == null)
-            {
-                log.Error("TryOpenRepository couldn't find IOperatingSystem service");
-                return false;
-            }
-
-            var dte = serviceProvider.TryGetService<DTE>();
-            if (dte == null)
-            {
-                log.Error("TryOpenRepository couldn't find DTE service");
-                return false;
-            }
-
-            var gitPath = Path.Combine(repoPath, ".git");
-            var gitDir = os.Directory.GetDirectory(gitPath);
-            if (!gitDir.Exists)
-            {
-                return false;
-            }
-
-            bool solutionCreated = false;
-            try
-            {
-                dte.Solution.Create(repoPath, TempSolutionName);
-                solutionCreated = true;
-
-                dte.Solution.Close(false); // Don't create a .sln file when we close.
-            }
-            catch (Exception e)
-            {
-                log.Error(e, "Error opening repository");
-            }
-            finally
-            {
-                TryCleanupSolutionUserFiles(os, repoPath, TempSolutionName);
-            }
-            return solutionCreated;
         }
 
         void TryCleanupSolutionUserFiles(IOperatingSystem os, string repoPath, string slnName)
