387f2e34746d719b0150ae381d6c25ddadb22d00
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-11-26 14:03:19 -0500

    Revert "Using only one view"
    This reverts commit 271c2cd7ef7e426ae701e1a50c4c6a5d7a3be291.

---
+++ src/GitHub.InlineReviews/Tags/ShowInlineAnnotationGlyph.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace GitHub.InlineReviews.Tags
+{
+    public partial class ShowInlineAnnotationGlyph : UserControl
+    {
+        public ShowInlineAnnotationGlyph()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
