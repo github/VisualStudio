ea9fb70714b4fc2b4c13e5f777d9e7d21c14b720
Myoxocephalus <cj.adolfsson@gmail.com>
2019-01-01 13:45:16 +0100

    Added test to DiffService.

--- test/GitHub.InlineReviews.UnitTests/Services/DiffServiceTests.cs
+++ test/GitHub.InlineReviews.UnitTests/Services/DiffServiceTests.cs
@@ -1,19 +1,42 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.Threading.Tasks;
 using GitHub.InlineReviews.Services;
-using GitHub.InlineReviews.UnitTests.Properties;
 using GitHub.Services;
-using GitHub.Models;
 using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 using NUnit.Framework;
 
 namespace GitHub.InlineReviews.UnitTests.Services
 {
     public class DiffServiceTests
     {
-        public class TheParseFragmentMethod
+        public class TheDiffMethod
         {
+            [Test]
+            public async Task DiffReturnsEmptyList()
+            {
+                var gitClient = Substitute.For<IGitClient>();
+                gitClient.Compare(null, null, null, null).ReturnsNull();
+                var target = new DiffService(gitClient);
+
+                var result = await target.Diff(null, null, null, null);
+
+                Assert.That(result, Is.Empty);
+            }
+        }
+
+        public class TheDiffOverloadMethod
+        {
+            [Test]
+            public async Task DiffReturnsEmptyList()
+            {
+                var gitClient = Substitute.For<IGitClient>();
+                gitClient.CompareWith(null, null, null, null, null).ReturnsNull();
+                var target = new DiffService(gitClient);
+
+                var result = await target.Diff(null, null, null, null, null);
+
+                Assert.That(result, Is.Empty);
+            }
         }
     }
 }
