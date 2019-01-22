1442fefd15cb10fd78127c841b3a3ba8b0c0925a
Jamie Cansdale <jcansdale@github.com>
2019-01-22 12:53:15 +0000

    Check assemblies reference System.Net.Http v4.0
    Check all GitHub.* assemblies reference System.Net.Http v4.0.0.0.

--- test/GitHub.VisualStudio.UnitTests/GitHubAssemblyTests.cs
+++ test/GitHub.VisualStudio.UnitTests/GitHubAssemblyTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
 using NUnit.Framework;
 
@@ -12,6 +13,19 @@ public class GitHubAssemblyTests
         {
             Assert.That(referencedAssembly.Name, Does.Not.EndWith(".DesignTime"),
                 "DesignTime assemblies should be embedded not referenced");
+        }
+    }
+
+    [Theory]
+    public void GitHub_Assembly_Should_Not_Reference_System_Net_Http_Above_4_0(string assemblyFile)
+    {
+        var asm = Assembly.LoadFrom(assemblyFile);
+        foreach (var referencedAssembly in asm.GetReferencedAssemblies())
+        {
+            if (referencedAssembly.Name == "System.Net.Http")
+            {
+                Assert.That(referencedAssembly.Version, Is.EqualTo(new Version("4.0.0.0")));
+            }
         }
     }
 
