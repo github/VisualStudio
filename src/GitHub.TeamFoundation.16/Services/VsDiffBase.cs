1a41c3c4901d63f5e0b68188cb074ceddcef0a4f
Jamie Cansdale <jcansdale@github.com>
2019-02-20 21:37:47 +0000

    Only create IVsDiffBase if IDiffBaseService exists
    Use VsDiffBaseFactory to conditionally create VsDiffBase.

--- src/GitHub.TeamFoundation.16/Services/VsDiffBase.cs
+++ src/GitHub.TeamFoundation.16/Services/VsDiffBase.cs
@@ -1,7 +1,7 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
 using System.Threading;
 using GitHub.Services;
-using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.RichReview.Contracts;
 using Microsoft.VisualStudio.RichReview.UX.Controls;
 using Microsoft.VisualStudio.RichReview.UX.Controls.ViewModel;
@@ -9,31 +9,51 @@ using Microsoft.VisualStudio.Shell;
 
 namespace GitHub.TeamFoundation.Services
 {
-    [Export(typeof(IVsDiffBase))]
+    internal class VsDiffBaseFactory
+    {
+        [ImportingConstructor]
+        public VsDiffBaseFactory(
+            [Import("Microsoft.VisualStudio.RichReview.Contracts.IDiffBaseService", AllowDefault = true)] Lazy<object> diffBaseServiceObject)
+        {
+            if (diffBaseServiceObject != null)
+            {
+                Instance = new VsDiffBase(diffBaseServiceObject);
+            }
+        }
+
+        [Export(typeof(IVsDiffBase))]
+        internal IVsDiffBase Instance { get; }
+    }
+
     internal class VsDiffBase : IVsDiffBase
     {
+        readonly Lazy<object> diffBaseServiceObject;
+
         // store this in a static because this class does not seem to be a singleton
-        private static object ChangesList;
+        static object ChangesList;
+
+        internal VsDiffBase(Lazy<object> diffBaseServiceObject)
+        {
+            this.diffBaseServiceObject = diffBaseServiceObject;
+        }
 
         public void SetDiffBase(string repoPath, string branchName)
         {
             ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
             {
-                // Note: Asking for this up front in the constructor causes MEF cardinality issues much of the time.
-                // Delaying when I ask for it seems to have better results.
-                var componentModel = Package.GetGlobalService(typeof(SComponentModel)) as IComponentModel;
-                var diffBaseService = componentModel.GetService<IDiffBaseService>();
-
-                try
+                if (diffBaseServiceObject.Value is IDiffBaseService diffBaseService)
                 {
-                    string mergeBase = await diffBaseService.DiffBaseInformationProvider.GetMergeBaseAsync(repoPath, branchName, CancellationToken.None);
-                    diffBaseService.DiffBaseInfo = new DiffBaseInfo() { Id = mergeBase, ShortDescription = branchName, LongDescription = branchName };
-                }
-                catch
-                {
-                    // currently hitting errors if the target branch does not exist - will have to fetch from the proper remote. How to be sure what remote?
-                    // for prototyping, let's just eat all exceptions and clear the DiffBase if we can't figure it out.
-                    diffBaseService.DiffBaseInfo = null;
+                    try
+                    {
+                        string mergeBase = await diffBaseService.DiffBaseInformationProvider.GetMergeBaseAsync(repoPath, branchName, CancellationToken.None);
+                        diffBaseService.DiffBaseInfo = new DiffBaseInfo() { Id = mergeBase, ShortDescription = branchName, LongDescription = branchName };
+                    }
+                    catch
+                    {
+                        // currently hitting errors if the target branch does not exist - will have to fetch from the proper remote. How to be sure what remote?
+                        // for prototyping, let's just eat all exceptions and clear the DiffBase if we can't figure it out.
+                        diffBaseService.DiffBaseInfo = null;
+                    }
                 }
             });
         }
