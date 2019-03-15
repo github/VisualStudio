150902004afe6e31b8e989a23f977c0d77b45159
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-03-15 10:39:55 -0700

    Enabling more tests

--- test/GitHub.UI.UnitTests/Controls/AutoCompleteTextInputExtensionsTests.cs
+++ test/GitHub.UI.UnitTests/Controls/AutoCompleteTextInputExtensionsTests.cs
@@ -1,25 +1,29 @@
-﻿using GitHub.UI;
-using GitHub.UI.Controls.AutoCompleteBox;
-using Moq;
-using Xunit;
+﻿using GitHub.UI.Controls.AutoCompleteBox;
+using NSubstitute;
+using NUnit.Framework;
 
-class AutoCompleteTextInputExtensionsTests
+namespace GitHub.UI.UnitTests.Controls
 {
-    public class TheGetExpandedTextMethod
+    class AutoCompleteTextInputExtensionsTests
     {
-        [Theory]
-        [InlineData(":", 1, 0, ":apple: ")]
-        [InlineData(":a", 2, 0, ":apple: ")]
-        [InlineData(":ap", 3, 0, ":apple: ")]
-        [InlineData(":a", 1, 0, ":apple: a")]
-        [InlineData("Test :", 6, 5, "Test :apple: ")]
-        [InlineData("Test :ap", 8, 5, "Test :apple: ")]
-        [InlineData("Test :apother stuff", 8, 5, "Test :apple: other stuff")]
-        public void ReturnsExpandedText(string text, int caretIndex, int completionOffset, string expected)
+        public class TheGetExpandedTextMethod
         {
-            var textInput = Mock.Of<IAutoCompleteTextInput>(t => t.CaretIndex == caretIndex && t.Text == text);
-            var expandedText = textInput.GetExpandedText(":apple:", completionOffset);
-            Assert.Equal(expected, expandedText);
+            [TestCase(":", 1, 0, ":apple: ")]
+            [TestCase(":a", 2, 0, ":apple: ")]
+            [TestCase(":ap", 3, 0, ":apple: ")]
+            [TestCase(":a", 1, 0, ":apple: a")]
+            [TestCase("Test :", 6, 5, "Test :apple: ")]
+            [TestCase("Test :ap", 8, 5, "Test :apple: ")]
+            [TestCase("Test :apother stuff", 8, 5, "Test :apple: other stuff")]
+            public void ReturnsExpandedText(string text, int caretIndex, int completionOffset, string expected)
+            {
+                var textInput = Substitute.For<IAutoCompleteTextInput>();
+                textInput.CaretIndex.Returns(caretIndex);
+                textInput.Text.Returns(text);
+
+                var expandedText = textInput.GetExpandedText(":apple:", completionOffset);
+                Assert.AreEqual(expected, expandedText);
+            }
         }
     }
 }
