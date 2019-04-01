2d348d19975756bfbc1073d2aae949773d8dd2a3
Jamie Cansdale <jcansdale@github.com>
2019-04-01 14:08:20 +0100

    Preview UI with VS themes

---
+++ src/GitHub.VisualStudio.TestApp/ThemeControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ThemedDialog.UI
+{
+    /// <summary>
+    /// Interaction logic for ThemeControl.xaml
+    /// </summary>
+    public partial class ThemeControl : UserControl
+    {
+        public ThemeControl()
+        {
+            InitializeComponent();
+        }
+
+        void ThemeListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ThemeListBox.SelectedItem is Label label)
+            {
+                if (label.Content is string theme)
+                {
+                    if (TryFindResource($"Themes/{theme}") is ResourceDictionary themeResources)
+                    {
+                        Application.Current.Resources.MergedDictionaries.Add(themeResources);
+                    }
+                }
+            }
+        }
+    }
+}
