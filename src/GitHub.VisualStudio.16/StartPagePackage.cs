3d330a8caa9269f8e85cd17a5f7f64b40f8aebb6
Jamie Cansdale <jcansdale@github.com>
2019-05-02 17:04:37 +0100

    Use StartPagePackage as ICodeContainerProvider factory
    Simplify by using the built in CreateCodeContainerProvider method.

--- src/GitHub.VisualStudio.16/StartPagePackage.cs
+++ src/GitHub.VisualStudio.16/StartPagePackage.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using GitHub.VisualStudio;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.CodeContainerManagement;
-using CodeContainer = Microsoft.VisualStudio.Shell.CodeContainerManagement.CodeContainer;
 using ICodeContainerProvider = Microsoft.VisualStudio.Shell.CodeContainerManagement.ICodeContainerProvider;
 
 namespace GitHub.StartPage
@@ -14,47 +10,18 @@ namespace GitHub.StartPage
     // This is required for .imagemanifest files, XAML and when using unsigned assemblies.
     // See: https://github.com/github/VisualStudio/pull/1236/
     [ProvideBindingPath(SubPath = "UI")]
-
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [Guid(Guids.StartPagePackageId)]
-    [ProvideCodeContainerProvider("GitHub Container", Guids.StartPagePackageId, Images.ImageMonikerGuid, Images.Logo, "#110", "#111", typeof(GitHubContainerProvider))]
+    [ProvideCodeContainerProvider("GitHub Container", Guids.StartPagePackageId, Images.ImageMonikerGuid, Images.Logo, "#110", "#111", typeof(InBoxGitHubContainerProvider))]
     public sealed class StartPagePackage : ExtensionPointPackage
     {
-        protected override void Initialize()
-        {
-            base.Initialize();
-            ServiceProvider = this;
-        }
-
-        internal static IServiceProvider ServiceProvider { get; private set; }
-    }
-
-    [Guid(Guids.CodeContainerProviderId)]
-    public class GitHubContainerProvider : ICodeContainerProvider
-    {
-        readonly ICodeContainerProvider provider;
-
-        public GitHubContainerProvider() : this(StartPagePackage.ServiceProvider)
-        {
-        }
-
-        public GitHubContainerProvider(IServiceProvider serviceProvider)
+        protected override ICodeContainerProvider CreateCodeContainerProvider(Guid provider)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            var factory = new ExtensionServicesFactory(serviceProvider);
+            var factory = new ExtensionServicesFactory(this);
             var services = factory.Create();
-            provider = services.GetGitHubContainerProvider();
-        }
-
-        public Task<CodeContainer> AcquireCodeContainerAsync(IProgress<ServiceProgressData> downloadProgress, CancellationToken cancellationToken)
-        {
-            return provider.AcquireCodeContainerAsync(downloadProgress, cancellationToken);
-        }
-
-        public Task<CodeContainer> AcquireCodeContainerAsync(RemoteCodeContainer onlineCodeContainer, IProgress<ServiceProgressData> downloadProgress, CancellationToken cancellationToken)
-        {
-            return provider.AcquireCodeContainerAsync(onlineCodeContainer, downloadProgress, cancellationToken);
+            return services.GetGitHubContainerProvider();
         }
     }
 }
