2e04cf3805669434de0bcf53faf076c0368fb857
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-05-22 15:33:33 -0400

    Functionality to add Publish to GitHub Essentials
    co-authored-by: Jamie Cansdale <jcansdale@gmail.com>

---
+++ src/GitHub.VisualStudio.16/Sync/PublishSection.cs
@@ -0,0 +1,93 @@
+﻿/*
+* Copyright (c) Microsoft Corporation. All rights reserved. This code released
+* under the terms of the Microsoft Limited Public License (MS-LPL).
+*/
+using System;
+using System.ComponentModel;
+using Microsoft.TeamFoundation.Controls;
+
+namespace Microsoft.TeamExplorerSample.Sync
+{
+    /// <summary>
+    /// Publish section.
+    /// </summary>
+    [TeamExplorerSection(SectionId, TeamExplorerPageIds.GitCommits, Priority)]
+    public class PublishSection : TeamExplorerBaseSection
+    {
+        public const string SectionId = "35B18474-005D-4A2A-9CCF-FFFFEB60F1F5";
+        public const int Priority = 4;
+
+        readonly Guid PushToRemoteSectionId = new Guid("99ADF41C-0022-4C03-B3C2-05047A3F6C2C");
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PublishSection()
+            : base()
+        {
+            this.Title = "Publish to GitHub";
+            this.IsExpanded = true;
+            this.IsBusy = false;
+            this.SectionContent = new PublishView();
+            this.View.ParentSection = this;
+        }
+
+        /// <summary>
+        /// Get the view.
+        /// </summary>
+        protected PublishView View
+        {
+            get { return this.SectionContent as PublishView; }
+        }
+
+        /// <summary>
+        /// Initialize override.
+        /// </summary>
+        public override void Initialize(object sender, SectionInitializeEventArgs e)
+        {
+            base.Initialize(sender, e);
+
+            RefreshVisibility();
+
+            if (ServiceProvider.GetService(typeof(ITeamExplorerPage)) is ITeamExplorerPage page)
+            {
+                if (page.GetSection(PushToRemoteSectionId) is ITeamExplorerSection pushToRemoteSection)
+                {
+                    pushToRemoteSection.PropertyChanged += Section_PropertyChanged;
+                }
+            }
+        }
+
+        void Section_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(ITeamExplorerSection.IsVisible):
+                case nameof(ITeamExplorerSection.SectionContent):
+                    RefreshVisibility();
+                    break;
+            }
+        }
+
+        void RefreshVisibility()
+        {
+            var visible = false;
+
+            if (ServiceProvider.GetService(typeof(ITeamExplorerPage)) is ITeamExplorerPage page)
+            {
+                if (page.GetSection(PushToRemoteSectionId) is ITeamExplorerSection pushToRemoteSection)
+                {
+                    if (pushToRemoteSection.SectionContent != null && pushToRemoteSection.IsVisible)
+                    {
+                        visible = true;
+                    }
+                }
+            }
+
+            if (IsVisible != visible)
+            {
+                IsVisible = visible;
+            }
+        }
+    }
+}
