07bbffcf5d4422aa428bbc1a525c78a3f2c693d6
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-05-30 12:05:26 -0400

    Loading more of the Publish functionality
    co-authored-by: Jamie Cansdale <jcansdale@gmail.com>

--- src/GitHub.VisualStudio.16/Sync/PublishSection.cs
+++ src/GitHub.VisualStudio.16/Sync/PublishSection.cs
@@ -4,14 +4,19 @@
 */
 using System;
 using System.ComponentModel;
+using System.Globalization;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using GitHub.Factories;
+using GitHub.Models;
 using GitHub.Primitives;
 using GitHub.ViewModels.TeamExplorer;
 using Microsoft.TeamFoundation.Controls;
 using Microsoft.VisualStudio.ComponentModelHost;
 using GitHub.VisualStudio;
+using GitHub.VisualStudio.UI;
 using Microsoft.VisualStudio.Threading;
+using ReactiveUI;
 using Task = System.Threading.Tasks.Task;
 
 namespace Microsoft.TeamExplorerSample.Sync
@@ -86,16 +91,93 @@ namespace Microsoft.TeamExplorerSample.Sync
 
         void ShowPublishDialog(IComponentModel componentModel)
         {
+            /*
+            var factory = ServiceProvider.GetService<IViewViewModelFactory>();
+            var viewModel = ServiceProvider.GetService<IRepositoryPublishViewModel>();
+            */
+
             var compositionServices = new CompositionServices();
             var compositionContainer = compositionServices.CreateVisualStudioCompositionContainer(componentModel.DefaultExportProvider);
 
             var factory = compositionContainer.GetExportedValue<IViewViewModelFactory>();
             var viewModel = compositionContainer.GetExportedValue<IRepositoryPublishViewModel>();
 
-            var frameworkElement = factory.CreateView<IRepositoryPublishViewModel>();
-            frameworkElement.DataContext = viewModel;
+            var busy = viewModel.WhenAnyValue(x => x.IsBusy).Subscribe(x => IsBusy = x);
 
-            this.SectionContent = frameworkElement;
+            var completed = viewModel.PublishRepository
+                .Where(x => x == ProgressState.Success)
+                .Subscribe(_ =>
+                {
+                    var teamExplorer =
+                        VisualStudio.Shell.ServiceProvider.GlobalProvider.GetService(typeof(ITeamExplorer)) as ITeamExplorer;
+                    teamExplorer?.NavigateToPage(new Guid(TeamExplorerPageIds.Home), null);
+
+                    // HandleCreatedRepo(ActiveRepo);
+                });
+
+            var view = factory.CreateView<IRepositoryPublishViewModel>();
+            view.DataContext = viewModel;
+
+            SectionContent = view;
+
+            /*
+
+            var view = factory.CreateView<IRepositoryPublishViewModel>();
+            view.DataContext = viewModel;
+            SectionContent = view;
+
+            Observable.FromEventPattern<RoutedEventHandler, RoutedEventArgs>(
+                    x => view.Unloaded += x,
+                    x => view.Unloaded -= x)
+                .Take(1)
+                .Subscribe(_ =>
+                {
+                    busy.Dispose();
+                    completed.Dispose();
+                });
+
+            */
+        }
+
+        void HandleCreatedRepo(LocalRepositoryModel newrepo)
+        {
+            var msg = String.Format(CultureInfo.CurrentCulture, Constants.Notification_RepoCreated, newrepo.Name, newrepo.CloneUrl);
+            msg += " " + String.Format(CultureInfo.CurrentCulture, Constants.Notification_CreateNewProject, newrepo.LocalPath);
+            ShowNotification(newrepo, msg);
+        }
+
+        private void ShowNotification(LocalRepositoryModel newrepo, string msg)
+        {
+//            var teServices = ServiceProvider.TryGetService<ITeamExplorerServices>();
+//
+//            teServices.ClearNotifications();
+//            teServices.ShowMessage(
+//                msg,
+//                new RelayCommand(o =>
+//                {
+//                    var str = o.ToString();
+//                    /* the prefix is the action to perform:
+//                     * u: launch browser with url
+//                     * c: launch create new project dialog
+//                     * o: launch open existing project dialog 
+//                    */
+//                    var prefix = str.Substring(0, 2);
+//                    if (prefix == "u:")
+//                        OpenInBrowser(ServiceProvider.TryGetService<IVisualStudioBrowser>(), new Uri(str.Substring(2)));
+//                    else if (prefix == "o:")
+//                    {
+//                        if (ErrorHandler.Succeeded(ServiceProvider.GetSolution().OpenSolutionViaDlg(str.Substring(2), 1)))
+//                            ServiceProvider.TryGetService<ITeamExplorer>()?.NavigateToPage(new Guid(TeamExplorerPageIds.Home), null);
+//                    }
+//                    else if (prefix == "c:")
+//                    {
+//                        var vsGitServices = ServiceProvider.TryGetService<IVSGitServices>();
+//                        vsGitServices.SetDefaultProjectPath(newrepo.LocalPath);
+//                        if (ErrorHandler.Succeeded(ServiceProvider.GetSolution().CreateNewProjectViaDlg(null, null, 0)))
+//                            ServiceProvider.TryGetService<ITeamExplorer>()?.NavigateToPage(new Guid(TeamExplorerPageIds.Home), null);
+//                    }
+//                })
+//            );
         }
 
 
