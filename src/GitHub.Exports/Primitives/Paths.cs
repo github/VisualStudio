e46f894a98e9d6a0f3ee760c91097ff4ef5e37b1
Jamie Cansdale <jcansdale@github.com>
2019-06-13 13:13:03 +0100

    Add xmldoc comments to Paths

--- src/GitHub.Exports/Primitives/Paths.cs
+++ src/GitHub.Exports/Primitives/Paths.cs
@@ -3,18 +3,31 @@ using System.IO;
 
 namespace GitHub.Primitives
 {
+    /// <summary>
+    /// Convert to and from Git paths.
+    /// </summary>
     public static class Paths
     {
         public const char GitDirectorySeparatorChar = '/';
 
+        /// <summary>
+        /// Convert from a relative path to a Git path.
+        /// </summary>
+        /// <param name="relativePath">A relative path.</param>
+        /// <returns>A working directory relative path which uses the '/' directory separator.</returns>
         public static string ToGitPath(string relativePath)
         {
             return relativePath.Replace(Path.DirectorySeparatorChar, GitDirectorySeparatorChar);
         }
 
-        public static string ToRelativePath(string relativePath)
+        /// <summary>
+        /// Convert from a Git path to a relative path.
+        /// </summary>
+        /// <param name="gitPath">A working directory relative path which uses the '/' directory separator.</param>
+        /// <returns>A relative paht that uses the <see cref="Path.DirectorySeparatorChar"/> directory separator.</returns>
+        public static string ToRelativePath(string gitPath)
         {
-            return relativePath.Replace(GitDirectorySeparatorChar, Path.DirectorySeparatorChar);
+            return gitPath.Replace(GitDirectorySeparatorChar, Path.DirectorySeparatorChar);
         }
     }
 }
