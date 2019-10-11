4af790c2805993f69564659465b36ac891a427da
Jamie Cansdale <jcansdale@gmail.com>
2019-10-11 14:39:28 +0100

    Bump the copyright year

--- src/common/SolutionInfo.cs
+++ src/common/SolutionInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("GitHub Extension for Visual Studio")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyCompany("GitHub, Inc.")]
-[assembly: AssemblyCopyright("Copyright © GitHub, Inc. 2014-2016")]
+[assembly: AssemblyCopyright("Copyright © GitHub, Inc. 2014-2019")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
