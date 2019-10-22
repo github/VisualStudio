c9973dee079e05b47f1e84a4fb1346c5de34758c
Jamie Cansdale <jcansdale@gmail.com>
2019-10-22 19:03:46 +0100

    Add tests fro RepositoryLayoutUtilities

---
+++ test/GitHub.App.UnitTests/Services/RepositoryLayoutUtilitiesTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GitHub.Services;
+using NUnit.Framework;
+
+public static class RepositoryLayoutUtilitiesTests
+{
+    public class TheGetDefaultPathAndLayoutMethod
+    {
+        [TestCase(@"c:\source\owner\repositoryName", "https://github.com/owner/repositoryName", @"c:\source", RepositoryLayout.OwnerName)]
+        [TestCase(@"c:\source\owner\differentName", "https://github.com/owner/repositoryName", @"c:\source", RepositoryLayout.OwnerName)]
+        [TestCase(@"c:\source\repositoryName", "https://github.com/owner/repositoryName", @"c:\source", RepositoryLayout.Name)]
+        [TestCase(@"c:\source\differentName", "https://github.com/owner/repositoryName", @"c:\source", RepositoryLayout.Name)]
+        public void GetDefaultPathAndLayout(string repositoryPath, string cloneUrl, string expectPath, RepositoryLayout expectLayout)
+        {
+            var (path, layout) = RepositoryLayoutUtilities.GetDefaultPathAndLayout(repositoryPath, cloneUrl);
+
+            Assert.That((path, layout), Is.EqualTo((expectPath, expectLayout)));
+        }
+    }
+}
